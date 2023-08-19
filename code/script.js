f37f386b1032330b474dbc441dad8d0458f6d203
Hang-Nguyen-Vu <nguyenvuhang@gmail.com>
2023-08-19 22:57:17 +0200

    Add step 1 and 2

--- code/script.js
+++ code/script.js
@@ -6,8 +6,40 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+let name = prompt(`Please enter your name:`);
+
+alert(`Hi ${name}!`);
+
 // Step 2 - Food choice
 // Your code goes here
+let food = parseInt(prompt(`What would you like to order? 
+Enter number 1-3 to choose among the followings:
+1. Pizza
+2. Pasta
+3. Sallad`));
+
+switch (food) {
+  case 1:
+    alert(
+      `Thank you. Let's get you a pizza!`
+    );
+    break;
+  case 2:
+    alert(
+      `Thank you. Let's get you a pasta!`
+    );
+    break;
+  case 3:
+    alert(
+      `Thank you. Let's get you a sallad!`
+    );
+    break;
+  default:
+    alert(
+      `Invalid choice. Please enter number 1-3 to choose among the dishes.`
+    );
+    break;
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
