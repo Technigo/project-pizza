2f44e4ac2dee423ab63f70cbc724a3d4e79172d1
LauraLyckholm <laura.lyckholm@gmail.com>
2023-08-20 20:25:16 +0200

    First commit with first draft of steps 1-3

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Replace this readme with your own information about your project. Start by brief
 
 Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
 
+- promt() to then save the value (saveed function in a variable by adding const and a variable name before the promt-funciton, then tested it by using console.log)
+- wanted a new line in the alert window, googled for answer \n 
+- how to store a value from multiple choices in a prompt, google - switch? The problem was that I didn't know how to write the choices in the prompt-function. I decided to instead make it an if-else statement, and then rain into the problem that my three === compared wrong, because it expected a number, so I had to change it to "3", as a string. Added a variable in each turn of the if-statement stat saved to value.
+
 ## View it live
 
 Have you deployed your project somewhere? Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
--- code/script.js
+++ code/script.js
@@ -1,16 +1,90 @@
-// Start here
-
 // Step 1 - Welcome and introduction
-// Your code goes here
+// Ordering process starts
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+// Asking for the customer name and storing it in a variable
+const customerName = prompt(
+  `First and foremost, what is you name?`
+)
+
 // Step 2 - Food choice
-// Your code goes here
+// The saved customer name is shown along with the three pizzakinds, promting the customer to choose one number
+const foodType = prompt(
+  `Happy to see you here ${customerName}!
+  \nWhat do you feel like eating today? Choose a number.
+  \n1. Pizza
+  \n2. Pasta
+  \n3. Salad`
+)
 
 // Step 3 - Subtype choice
-// Your code goes here
+// Defines a variable namned "choice", which save the chosen foodtype. 
+let choice
+let pizzaType
+let pastaType
+let saladType
+let subtypeChoice
+
+if (foodType == 1) {
+  choice = "Pizza"
+  pizzaType = prompt(`You chose ${choice}, yummy in my tummy!
+  \nWhat kind of pizza do you want? Choose a number.
+  \n1. Quattro Formaggio
+  \n2. Margherita
+  \n3. Swedish Kebab`
+  ); if (pizzaType == 1) {
+    subtypeChoice = "Quattro Formaggio"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  } else if (pizzaType == 2) {
+    subtypeChoice = "Margherita"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  } else if (pizzaType == 3) {
+    subtypeChoice = "Swedish Kebab"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  };
+
+} else if (foodType == 2) {
+  choice = "Pasta"
+  pastaType = prompt(`You chose ${choice}, always a good choice!
+  \nWhat kind of pasta do you want? Choose a number.
+  \n1. Truffle
+  \n2. Bolognese
+  \n3. Cheesey Deluxe`
+  ); if (pastaType == 1) {
+    subtypeChoice = "Truffle"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  } else if (pastaType == 2) {
+    subtypeChoice = "Bolognese"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  } else if (pastaType == 3) {
+    subtypeChoice = "Cheesey Deluxe"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  };
+
+} else if (foodType == 3) {
+  choice = "Salad"
+  saladType = prompt(`You chose ${choice}, let's make it a good day!
+  \nWhat kind of pasta do you want? Choose a number.
+  \n1. Ceasar
+  \n2. Goat Cheese
+  \n3. Vegan`
+  ); if (saladType == 1) {
+    subtypeChoice = "Ceasar"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  } else if (saladType == 2) {
+    subtypeChoice = "Goat Cheese"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  } else if (saladType == 3) {
+    subtypeChoice = "Vegan"
+    alert(`A ${subtypeChoice} ${choice} coming up!`)
+  };
+
+} else {
+  alert("Please try again")
+}
+
 
 // Step 4 - Age
 // Your code goes here
