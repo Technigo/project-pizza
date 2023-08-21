51e0c0facd5e16812967f4f64f57d8f9f6a40cc0
Isa Robertini <robertiniisa@gmail.com>
2023-08-21 16:03:45 +0200

    age

--- code/script.js
+++ code/script.js
@@ -12,13 +12,18 @@ if (person != null) {
 }
 
 // Step 2 - Food choice
+let foodOptionText;
+
 let foodOption = prompt("What would you like to order? \n 1 Pizza \n 2 Pasta \n 3 Salad"); {
 
   if (foodOption == 1) {
+    foodOptionText = "pizza"
     alert("Pizza is a great choice!");
   } else if (foodOption == 2) {
+    foodOptionText = "pasta"
     alert("Amazing! Pasta here is delicious!");
   } else if (foodOption == 3) {
+    foodOptionText = "salad"
     alert("wow, salad, that is a great choice!");
   } else {
     alert("Oh... are you not hungry? Please choose one of the options!");
@@ -28,16 +33,17 @@ let foodOption = prompt("What would you like to order? \n 1 Pizza \n 2 Pasta \n 
 
 if (foodOption == 1) {
   let pizzaOption = prompt("Which Pizza would you like to order? \n 1 Pizza Bianca \n 2 Pizza con pomodorini \n 3 Quattro Stagioni");
-}
-else if (foodOption == 2) {
+} else if (foodOption == 2) {
   let pastaOption = prompt("Which pasta would you like to order? \n 1 Pasta Alio e Olio \n 2 Pasta con Pomodorini \n 3 Pasta allo Scoglio");
 } else if (foodOption == 3) {
   let saladOption = prompt("Which salad would you like to order? \n 1 Tuna Salad \n 2 Broccoli Salad \n 3 Fruit Salad");
-}
 
-// Step 4 - Age
-let ageOption = prompt("Is the food for a child or an adult?"); {
-
+  // Step 4 - Age
+} let ageOption = prompt("How old are you?");
+if (ageOption >= 18) {
+  alert("You have ordered a " + foodOptionText + " from the grown ups menu!");
+} else {
+  alert("you have ordered a " + oodOptionText + "from the childrens menu!");
 }
 // Step 5 - Order confirmation
 // Your code goes here
