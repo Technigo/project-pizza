8118ee648c9223d147fe869dbf609052a56806bc
Isa Robertini <robertiniisa@gmail.com>
2023-08-21 17:00:29 +0200

    small changes

--- code/script.js
+++ code/script.js
@@ -17,13 +17,10 @@ let foodOptionText;
 let foodOption = prompt("What would you like to order? \n 1 Pizza \n 2 Pasta \n 3 Salad"); {
 
   if (foodOption == 1) {
-    foodOptionText = "pizza"
     alert("Pizza is a great choice!");
   } else if (foodOption == 2) {
-    foodOptionText = "pasta"
     alert("Amazing! Pasta here is delicious!");
   } else if (foodOption == 3) {
-    foodOptionText = "salad"
     alert("wow, salad, that is a great choice!");
   } else {
     alert("Oh... are you not hungry? Please choose one of the options!");
@@ -33,11 +30,14 @@ let foodOption = prompt("What would you like to order? \n 1 Pizza \n 2 Pasta \n 
 
 if (foodOption == 1) {
   let pizzaOption = prompt("Which Pizza would you like to order? \n 1 Pizza Bianca \n 2 Pizza con pomodorini \n 3 Quattro Stagioni");
+  foodOptionText = "Pizza Bianca"
   if (pizzaOption == 1) {
     alert("You have choosen Pizza Bianca, amazing!");
   } else if (pizzaOption == 2) {
+    foodOptionText = "Pizza con Pomodorini"
     alert("You have choosen Pizza con Pomodorini, amazing!");
   } else if (pizzaOption == 3) {
+    foodOptionText = "Quattro Stagioni"
     alert("You have choosen Quattro Stagioni, amazing!");
   } else {
     alert("Please choose a number between 1-3");
@@ -45,10 +45,13 @@ if (foodOption == 1) {
 } else if (foodOption == 2) {
   let pastaOption = prompt("Which pasta would you like to order? \n 1 Pasta Alio e Olio \n 2 Pasta con Pomodorini \n 3 Pasta allo Scoglio");
   if (pastaOption == 1) {
-    alert("You have choosen Alio e Olio, amazing!");
+    foodOptionText = "Pasta Alio e Olio"
+    alert("You have choosen Pasta Alio e Olio, amazing!");
   } else if (pastaOption == 2) {
+    foodOptionText = "Pasta con Pomodorini"
     alert("You have choosen Pasta con Pomodorini, amazing!");
   } else if (pastaOption == 1) {
+    foodOptionText = "Pasta allo Scoglio"
     alert("You have choosen Pasta allo Scoglio, amazing!");
   } else {
     alert("Please choose a number between 1-3");
@@ -56,11 +59,14 @@ if (foodOption == 1) {
 
 } else if (foodOption == 3) {
   let saladOption = prompt("Which salad would you like to order? \n 1 Tuna Salad \n 2 Broccoli Salad \n 3 Fruit Salad");
+  foodOptionText = "Tuna Salad"
   if (saladOption == 1) {
     alert("You have choosen Tuna Salad, amazing!");
+    foodOptionText = "Broccoli Salad"
   } if (saladOption == 2) {
     alert("You have choosen Broccoli Salad, amazing!");
   } if (saladOption == 3) {
+    foodOptionText = "Fruit Salad"
     alert("You have choosen Fruit Salad, amazing!");
   } else {
     alert("Please choose a number between 1-3");
@@ -72,9 +78,9 @@ if (foodOption == 1) {
 let ageOption = prompt("How old are you?");
 
 if (ageOption >= 18) {
-  alert("You have ordered a " + foodOption + " from the grown ups menu!");
+  alert("You have ordered a " + foodOptionText + " from the grown ups menu!");
 } else {
-  alert("you have ordered a " + foodOption + "from the childrens menu!");
+  alert("you have ordered a " + foodOptionText + " from the childrens menu!");
 }
 // Step 5 - Order confirmation
 
