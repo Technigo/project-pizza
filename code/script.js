b80888466e3239fb4a0e5d3a2d1f44830348458f
Isa Robertini <robertiniisa@gmail.com>
2023-08-21 16:20:28 +0200

    clean up+added responses

--- code/script.js
+++ code/script.js
@@ -33,13 +33,44 @@ let foodOption = prompt("What would you like to order? \n 1 Pizza \n 2 Pasta \n 
 
 if (foodOption == 1) {
   let pizzaOption = prompt("Which Pizza would you like to order? \n 1 Pizza Bianca \n 2 Pizza con pomodorini \n 3 Quattro Stagioni");
+  if (pizzaOption == 1) {
+    alert("You have choosen Pizza Bianca, amazing!");
+  } else if (pizzaOption == 2) {
+    alert("You have choosen Pizza con Pomodorini, amazing!");
+  } else if (pizzaOption == 3) {
+    alert("You have choosen Quattro Stagioni, amazing!");
+  } else {
+    alert("Please choose a number between 1-3");
+  }
 } else if (foodOption == 2) {
   let pastaOption = prompt("Which pasta would you like to order? \n 1 Pasta Alio e Olio \n 2 Pasta con Pomodorini \n 3 Pasta allo Scoglio");
+  if (pastaOption == 1) {
+    alert("You have choosen Alio e Olio, amazing!");
+  } else if (pastaOption == 2) {
+    alert("You have choosen Pasta con Pomodorini, amazing!");
+  } else if (pastaOption == 1) {
+    alert("You have choosen Pasta allo Scoglio, amazing!");
+  } else {
+    alert("Please choose a number between 1-3");
+  }
+
 } else if (foodOption == 3) {
   let saladOption = prompt("Which salad would you like to order? \n 1 Tuna Salad \n 2 Broccoli Salad \n 3 Fruit Salad");
+  if (saladOption == 1) {
+    alert("You have choosen Tuna Salad, amazing!");
+  } if (saladOption == 2) {
+    alert("You have choosen Broccoli Salad, amazing!");
+  } if (saladOption == 3) {
+    alert("You have choosen Fruit Salad, amazing!");
+  } else {
+    alert("Please choose a number between 1-3");
+  }
+} else {
+  alert("Please choose a valid option!");
+}
+// Step 4 - Age
+let ageOption = prompt("How old are you?");
 
-  // Step 4 - Age
-} let ageOption = prompt("How old are you?");
 if (ageOption >= 18) {
   alert("You have ordered a " + foodOptionText + " from the grown ups menu!");
 } else {
