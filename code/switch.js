bc3b531e0baadeb6edcb3d7c5048f01d6507cd15
Andrej <72137459+andrkavr@users.noreply.github.com>
2023-08-21 12:40:44 +0200

    add validation that exits if wrong answer has been chosen

--- code/switch.js
+++ code/switch.js
@@ -1,5 +1,7 @@
 // Start here
 
+let codeWorking = true;
+
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
@@ -16,7 +18,6 @@ switch (name) {
     break;
   default:
     alert(`Hi ${name}. Welcome to JavaScript Pizzeria!`);
-    break;
 }
 // Step 2 - Food choice
 // Your code goes here
@@ -48,137 +49,299 @@ switch (order) {
   default:
     alert(`Sorry, you either didn't choose a number or that is not something that we have. 
   Please contact us again if you want to order one of our dishes.`);
-    break;
+    codeWorking = false;
 }
+
+console.log(foodtype);
+console.log(codeWorking);
 
 // Step 3 - Subtype choice
 // Your code goes here
 
 let subChoice = "";
+switch (true) {
+  case codeWorking == true:
+    switch (foodtype) {
+      case "Pizza":
+        let subPizza = prompt(`What pizza do you want?
+                  We have the following options:
+                  1. Capricciosa
+                  2. Vesuvio
+                  3. Margherita`);
 
-switch (foodtype) {
-  case "Pizza":
-    let subPizza = prompt(`What pizza do you want?
-        We have the following options:
-        1. Capricciosa
-        2. Vesuvio
-        3. Margherita`);
+        switch (subPizza) {
+          case "1":
+            alert("One Capricciosa, got it.");
+            subChoice = "Capricciosa";
+            break;
+          case "2":
+            alert("One Vesuvio, got it.");
+            subChoice = "Vesuvio";
+            break;
+          case "3":
+            alert("One Margherita, got it.");
+            subChoice = "Margherita";
+            break;
+          default:
+            alert(
+              `Sorry, you either didn't choose a number or that is not something that we have. Please contact us again if you want to order one of our dishes.`
+            );
+            codeWorking = false;
+        }
+        break;
 
-    switch (subPizza) {
-      case "1":
-        alert("One Capricciosa, got it.");
-        subChoice = "Capricciosa";
+      case "Pasta":
+        let subPasta = prompt(`What pasta do you want?
+              We have the following options:
+              1. Pasta Carbonara
+              2. Lasagna
+              3. Pasta Bolognese`);
+
+        switch (subPasta) {
+          case "1":
+            alert("One Pasta Carbonara, got it.");
+            subChoice = "Pasta Carbonara";
+            break;
+          case "2":
+            alert("One Lasagna, got it.");
+            subChoice = "Lasagna";
+            break;
+          case "1":
+            alert("One Pasta Bolognese, got it.");
+            subChoice = "Pasta Bolognese";
+            break;
+          default:
+            alert(
+              `Sorry, you either didn't choose a number or that is not something that we have. Please contact us again if you want to order one of our dishes.`
+            );
+            codeWorking = false;
+        }
         break;
-      case "2":
-        alert("One Vesuvio, got it.");
-        subChoice = "Vesuvio";
-        break;
-      case "3":
-        alert("One Margherita, got it.");
-        subChoice = "Margherita";
+
+      case "Salad":
+        let subSalad = prompt(`What salad do you want?
+              We have the following options:
+              1. Caesar Salad
+              2. Caprese Salad
+              3. Greek Salad`);
+
+        switch (subSalad) {
+          case "1":
+            alert("One Caesar Salad, got it.");
+            subChoice = "Caesar Salad";
+            break;
+          case "2":
+            alert("One Caprese Salad, got it.");
+            subChoice = "Caprese Salad";
+            break;
+          case "3":
+            alert("One Greek Salad, got it.");
+            subChoice = "Greek Salad";
+            break;
+          default:
+            alert(
+              `Sorry, you either didn't choose a number or that is not something that we have. Please contact us again if you want to order one of our dishes.`
+            );
+            codeWorking = false;
+        }
         break;
     }
-    break;
-  case "Pasta":
-    let subPasta = prompt(`What pasta do you want?
-    We have the following options:
-    1. Pasta Carbonara
-    2. Lasagna
-    3. Pasta Bolognese`);
-
-    switch (subPasta) {
-      case "1":
-        alert("One Pasta Carbonara, got it.");
-        subChoice = "Pasta Carbonara";
-        break;
-      case "2":
-        alert("One Lasagna, got it.");
-        subChoice = "Lasagna";
-        break;
-      case "1":
-        alert("One Pasta Bolognese, got it.");
-        subChoice = "Pasta Bolognese";
-        break;
-    }
-    break;
-
-  case "Salad":
-    let subSalad = prompt(`What salad do you want?
-    We have the following options:
-    1. Caesar Salad
-    2. Caprese Salad
-    3. Greek Salad`);
-
-    switch (subSalad) {
-      case "1":
-        alert("One Caesar Salad, got it.");
-        subChoice = "Caesar Salad";
-        break;
-      case "2":
-        alert("One Caprese Salad, got it.");
-        subChoice = "Caprese Salad";
-        break;
-      case "3":
-        alert("One Greek Salad, got it.");
-        subChoice = "Greek Salad";
-        break;
-    }
-    break;
-  default:
-    alert("That is not one of our dishes. Please try again from the start.");
     break;
 }
+// switch (foodtype) {
+//   case "Pizza":
+//     let subPizza = prompt(`What pizza do you want?
+//         We have the following options:
+//         1. Capricciosa
+//         2. Vesuvio
+//         3. Margherita`);
+
+//     switch (subPizza) {
+//       case "1":
+//         alert("One Capricciosa, got it.");
+//         subChoice = "Capricciosa";
+//         break;
+//       case "2":
+//         alert("One Vesuvio, got it.");
+//         subChoice = "Vesuvio";
+//         break;
+//       case "3":
+//         alert("One Margherita, got it.");
+//         subChoice = "Margherita";
+//         break;
+//       default:
+//         alert(`Sorry, you either didn't choose a number or that is not something that we have.
+//               Please contact us again if you want to order one of our dishes.`);
+//     }
+//     break;
+
+//   case "Pasta":
+//     let subPasta = prompt(`What pasta do you want?
+//     We have the following options:
+//     1. Pasta Carbonara
+//     2. Lasagna
+//     3. Pasta Bolognese`);
+
+//     switch (subPasta) {
+//       case "1":
+//         alert("One Pasta Carbonara, got it.");
+//         subChoice = "Pasta Carbonara";
+//         break;
+//       case "2":
+//         alert("One Lasagna, got it.");
+//         subChoice = "Lasagna";
+//         break;
+//       case "1":
+//         alert("One Pasta Bolognese, got it.");
+//         subChoice = "Pasta Bolognese";
+//         break;
+//       default:
+//         alert(`Sorry, you either didn't choose a number or that is not something that we have.
+//               Please contact us again if you want to order one of our dishes.`);
+//     }
+//     break;
+
+//   case "Salad":
+//     let subSalad = prompt(`What salad do you want?
+//     We have the following options:
+//     1. Caesar Salad
+//     2. Caprese Salad
+//     3. Greek Salad`);
+
+//     switch (subSalad) {
+//       case "1":
+//         alert("One Caesar Salad, got it.");
+//         subChoice = "Caesar Salad";
+//         break;
+//       case "2":
+//         alert("One Caprese Salad, got it.");
+//         subChoice = "Caprese Salad";
+//         break;
+//       case "3":
+//         alert("One Greek Salad, got it.");
+//         subChoice = "Greek Salad";
+//         break;
+//       default:
+//         alert(`Sorry, you either didn't choose a number or that is not something that we have.
+//               Please contact us again if you want to order one of our dishes.`);
+//     }
+//     break;
+//   default:
+//     alert("That is not one of our dishes. Please try again from the start.");
+// }
 
 // Step 4 - Age
 // Your code goes here
+let age;
+switch (true) {
+  case codeWorking == true:
+    age = prompt(
+      `Who is this order for?
+          1. Adult
+          2. Child`
+    );
 
-let age = prompt(
-  `Who is this order for?
-1. Adult
-2. Child`
-);
+    switch (age) {
+      case "1":
+        alert("One adult portion, perfect.");
+        break;
+      case "2":
+        alert("One kid size portion, perfect.");
+        break;
+      default:
+        alert(
+          "Please choose one of the provided alternatives. Please try again from the start."
+        );
+        codeWorking = false;
+    }
+    break;
+}
+
+// let age = prompt(
+//   `Who is this order for?
+// 1. Adult
+// 2. Child`
+// );
 
 // Step 5 - Order confirmation
 // Your code goes here
 
 let confirmation;
 
-switch (age) {
-  case "1":
-    confirmation =
-      prompt(`Alright, so one adult-sized ${subChoice}. That will be $17.00. 
-  Is this what you want to order?
-  Enter a number to confirm:
-  1. Yes
-  2. No`);
-    break;
-  case "2":
-    confirmation =
-      prompt(`Alright, so one child-sized ${subChoice}. That will be $12.00. 
-  Is this what you want to order?
-  Enter a number to confirm:
-  1. Yes
-  2. No`);
-    break;
-  default:
-    alert(
-      "Sorry, there seems to have been an error somewhere. Please come back again."
-    );
+switch (true) {
+  case codeWorking == true:
+    switch (age) {
+      case "1":
+        confirmation =
+          prompt(`Alright, so one adult-sized ${subChoice}. That will be $17.00. 
+            Is this what you want to order?
+            Enter a number to confirm:
+            1. Yes
+            2. No`);
+        break;
+      case "2":
+        confirmation =
+          prompt(`Alright, so one child-sized ${subChoice}. That will be $12.00. 
+            Is this what you want to order?
+            Enter a number to confirm:
+            1. Yes
+            2. No`);
+        break;
+    }
+
+    switch (confirmation) {
+      case "1":
+        alert(
+          `Thank you for your order! Your food is being prepared and will be ready in 10-15 minutes.`
+        );
+        break;
+      case "2":
+        alert(
+          `That's alright, please feel free to order from us in the future!`
+        );
+        break;
+      default:
+        alert(
+          "Sorry, there seems to have been an error somewhere. Please come back again."
+        );
+    }
     break;
 }
 
-switch (confirmation) {
-  case "1":
-    alert(
-      `Thank you for your order! Your food is being prepared and will be ready in 10-15 minutes.`
-    );
-    break;
-  case "2":
-    alert(`That's alright, please feel free to order from us in the future!`);
-    break;
-  default:
-    alert(
-      "Sorry, there seems to have been an error somewhere. Please come back again."
-    );
-    break;
-}
+// switch (age) {
+//   case "1":
+//     confirmation =
+//       prompt(`Alright, so one adult-sized ${subChoice}. That will be $17.00.
+//   Is this what you want to order?
+//   Enter a number to confirm:
+//   1. Yes
+//   2. No`);
+//     break;
+//   case "2":
+//     confirmation =
+//       prompt(`Alright, so one child-sized ${subChoice}. That will be $12.00.
+//   Is this what you want to order?
+//   Enter a number to confirm:
+//   1. Yes
+//   2. No`);
+//     break;
+//   default:
+//     alert(
+//       "Sorry, there seems to have been an error somewhere. Please come back again."
+//     );
+// }
+
+// switch (confirmation) {
+//   case "1":
+//     alert(
+//       `Thank you for your order! Your food is being prepared and will be ready in 10-15 minutes.`
+//     );
+//     break;
+//   case "2":
+//     alert(`That's alright, please feel free to order from us in the future!`);
+//     break;
+//   default:
+//     alert(
+//       "Sorry, there seems to have been an error somewhere. Please come back again."
+//     );
+// }
