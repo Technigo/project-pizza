23a6cd34329161c95a3aee5e3f02a137192fcf4f
Isa Robertini <robertiniisa@gmail.com>
2023-08-21 15:26:48 +0200

    cleaning up

--- code/script.js
+++ code/script.js
@@ -12,32 +12,32 @@ if (person != null) {
 }
 
 // Step 2 - Food choice
-let foodOption = prompt("What would you like to order? \n 1 Pizza \n 2 Pasta \n 3 Salad");
-if (foodOption == 1) {
-  alert("Pizza is a great choice!");
-} else if (foodOption == 2) {
-  alert("Amazing! Pasta here is delicious!");
-} else if (foodOption == 3) {
-  alert("wow, salad, that is a great choice!");
-} else {
-  alert("Oh... are you not hungry? Please choose one of the options!");
-}
+let foodOption = prompt("What would you like to order? \n 1 Pizza \n 2 Pasta \n 3 Salad"); {
 
+  if (foodOption == 1) {
+    alert("Pizza is a great choice!");
+  } else if (foodOption == 2) {
+    alert("Amazing! Pasta here is delicious!");
+  } else if (foodOption == 3) {
+    alert("wow, salad, that is a great choice!");
+  } else {
+    alert("Oh... are you not hungry? Please choose one of the options!");
+  }
+}
 // Step 3 - Subtype choice
+
 if (foodOption == 1) {
-  let foodOption = prompt("Which Pizza would you like to order? \n 1 Pizza Bianca \n 2 Pizza con pomodorini \n 3 Quattro Stagioni");
+  let pizzaOption = prompt("Which Pizza would you like to order? \n 1 Pizza Bianca \n 2 Pizza con pomodorini \n 3 Quattro Stagioni");
 }
-if (foodOption == 2) {
-  let foodOption = prompt("Which pasta would you like to order? \n 1 Pasta Alio e Olio \n 2 Pasta con Pomodorini \n 3 Pasta allo Scoglio");
-}
-if (foodOption == 3) {
-  let foodOption = prompt("Which salad would you like to order? \n 1 Tuna Salad \n 2 Broccoli Salad \n 3 Fruit Salad");
-} else {
-  alert("Oh... are you not hungry? Please choose one of the options!");
+else if (foodOption == 2) {
+  let pastaOption = prompt("Which pasta would you like to order? \n 1 Pasta Alio e Olio \n 2 Pasta con Pomodorini \n 3 Pasta allo Scoglio");
+} else if (foodOption == 3) {
+  let saladOption = prompt("Which salad would you like to order? \n 1 Tuna Salad \n 2 Broccoli Salad \n 3 Fruit Salad");
 }
 
 // Step 4 - Age
-let ageOption = prompt("Is the food for a child or an adult?");
+let ageOption = prompt("Is the food for a child or an adult?"); {
 
+}
 // Step 5 - Order confirmation
 // Your code goes here
