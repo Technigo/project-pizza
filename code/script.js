2aa9f6cb55c4d79192a14adc357be74ac947b886
Frida Kristiansson <frida.kristiansson@gmail.com>
2023-08-21 17:52:32 +0200

    Step 2 choose food type using a prompt-message. The choice is displayed in an alert-message. The message is written with an if-else statement

--- code/script.js
+++ code/script.js
@@ -1,17 +1,32 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+// alert(
+//   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+// )
 
-let name = prompt("What's your name?")
-alert(
-  `Hey there ${name}, are you ready to order?`
-)
+// let name = prompt("What's your name?")
+// if (name != null) {
+//   alert(
+//     `Hey there ${name}, are you ready to order?`
+//     )
+//   }
 
 // Step 2 - Food choice
-// Your code goes here
+let menuChoices = "What types of food would you like to order? \n \n"
++ "\tEnter a number: \n"
++ "\t1. Pizza\n"
++ "\t2. Pasta\n"
++ "\t3. Salad \n"
+
+let foodChoice = Number(prompt(menuChoices, "Ex. 1"))
+let foodName =""
+if (foodChoice === 1) {foodName = "pizza"}
+else if (foodChoice === 2) {foodName = "pasta"}
+else if (foodChoice === 3) {foodName = "salad"}
+else {foodName = "an invalid number"}
+
+alert(`You have chosen ${foodName}`)
 
 // Step 3 - Subtype choice
 // Your code goes here
