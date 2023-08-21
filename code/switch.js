9f3411194be851a5551da728bd0be884121d67f0
Andrej <nexxuscore@gmail.com>
2023-08-21 09:20:00 +0200

    add switch functionality for main and sub choice of order

--- code/switch.js
+++ code/switch.js
@@ -30,15 +30,20 @@ let order = prompt(
   ""
 );
 
+let foodtype = "";
+
 switch (order) {
   case "1":
     alert("Alright, you want Pizza. Excellent choice!");
+    foodtype = "Pizza";
     break;
   case "2":
     alert("Alright, you want Pasta. Excellent choice!");
+    foodtype = "Pasta";
     break;
   case "3":
     alert("Alright, you want Salad. Excellent choice!");
+    foodtype = "Salad";
     break;
   default:
     alert(`Sorry, you either didn't choose a number or that is not something that we have. 
@@ -48,6 +53,78 @@ switch (order) {
 
 // Step 3 - Subtype choice
 // Your code goes here
+
+let subChoice = "";
+
+switch (foodtype) {
+  case "Pizza":
+    let subPizza = prompt(`What pizza do you want?
+        We have the following options:
+        1. Capricciosa
+        2. Vesuvio
+        3. Margherita`);
+
+    switch (subPizza) {
+      case "1":
+        alert("One Capricciosa, got it.");
+        subChoice = "Capricciosa";
+        break;
+      case "2":
+        alert("One Vesuvio, got it.");
+        subChoice = "Vesuvio";
+        break;
+      case "3":
+        alert("One Margherita, got it.");
+        subChoice = "Margherita";
+        break;
+    }
+    break;
+  case "Pasta":
+    let subPasta = prompt(`What pasta do you want?
+    We have the following options:
+    1. Pasta Carbonara
+    2. Lasagna
+    3. Pasta Bolognese`);
+
+    switch (subPasta) {
+      case "1":
+        alert("One Pasta Carbonara, got it.");
+        subChoice = "Pasta Carbonara";
+        break;
+      case "2":
+        alert("One Lasagna, got it.");
+        subChoice = "Lasagna";
+        break;
+      case "1":
+        alert("One Pasta Bolognese, got it.");
+        subChoice = "Pasta Bolognese";
+        break;
+    }
+    break;
+
+  case "Salad":
+    let subSalad = prompt(`What salad do you want?
+    We have the following options:
+    1. Caesar Salad
+    2. Caprese Salad
+    3. Greek Salad`);
+
+    switch (subSalad) {
+      case "1":
+        alert("One Caesar Salad, got it.");
+        subChoice = "Caesar Salad";
+        break;
+      case "2":
+        alert("One Caprese Salad, got it.");
+        subChoice = "Caprese Salad";
+        break;
+      case "1":
+        alert("One Greek Salad, got it.");
+        subChoice = "Greek Salad";
+        break;
+    }
+    break;
+}
 
 // Step 4 - Age
 // Your code goes here
