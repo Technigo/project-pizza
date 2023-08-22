e15b9142aac850e3f7975ad51aecc325014456c0
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-08-22 17:16:30 +0200

    completed step 2

--- code/script.js
+++ code/script.js
@@ -14,6 +14,29 @@ alert(`Hi ${userName}!`)
 // Step 2 - Food choice
 // Your code goes here
 
+const foodChoice = prompt(`What would you like to eat today? On today's menu we have: 
+1 - Pizza
+2 - Pasta 
+3 - Salad
+Please enter the number of your choice`)
+console.log(foodChoice)
+
+let selectedFood = "";
+
+if (foodChoice === "1") {
+  selectedFood = "Pizza"
+} else if (foodChoice === "2") {
+  selectedFood = "Pasta"
+} else if (foodChoice === "3") {
+  selectedFood = "Salad"
+} else {
+  alert("Invalid choice. Please select valid number.");
+  process.exit(1)
+}
+console.log(selectedFood)
+
+alert(`You chose ${selectedFood}!`)
+
 // Step 3 - Subtype choice
 // Your code goes here
 
