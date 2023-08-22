7511b8ab57c575f20c93aefe2e16a01941d9261d
Inna Kokic <Inna@Innas-MacBook-Pro.local>
2023-08-22 10:16:18 +0200

    Added subtype choise

--- code/script.js
+++ code/script.js
@@ -15,18 +15,30 @@ alert(
 // Step 2 - Food choice
 let food = prompt(
   "What food would you like to order?\nEnter a number:\n1 - Pizza\n2 - Pasta\n3 - Salad");
+  let choise
 if (food ==="1") {
-  alert("You have chosen the delicous pizza!")
+  //choise = pizza
+  alert("You have chosen the delicous pizza!");
 } else if (food ==="2") {
-  alert("You have chosen the creamy pasta!")
+  //choise = pasta
+  alert("You have chosen the creamy pasta!");
 } else if (food ==="3") {
-  alert("You have chosen the healthy salad!")
+  //choise = salad
+  alert("You have chosen the healthy salad!");
 } else {
-  alert("Invalid choise")
+  alert("Invalid choise");
 }
 
 // Step 3 - Subtype choice
-// Your code goes here
+if(food === "1"){
+prompt("Select a pizza type:\nEnter a number:\n1 - Funghi\n2 - Hawaian\n3 - Kebab");
+} else if(food === "2") {
+  prompt("Select a pasta type:\nEnter a number:\n1 - Chicken\n2 - Veggie\n3 - Carbonara");
+} else if(food === "3") {
+  prompt("Select a salad type:\nEnter a number:\n1 - Caesarsalad\n2 - Shrimpsalad\n3 - Greeksalad");
+} else {
+  alert("You have not made a choise of food type");
+}
 
 // Step 4 - Age
 // Your code goes here
