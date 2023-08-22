944c34ee4fe6b087fc7e38fdc2c202d577e5893e
BeckieMorton <beckam11@gmail.com>
2023-08-22 19:07:38 +1000

    tried step 4

--- code/script.js
+++ code/script.js
@@ -14,22 +14,6 @@ alert('Hi, ' + userName + ', we are glad you are here!')
 // Your code goes here
 // The program should use the prompt() method to present the user with a menu of options (Pizza, Pasta, Salad) and prompt them to enter a number corresponding to their choice. Based on the user's input, the program should determine the chosen food type (Pizza, Pasta, Salad) and display a message using the alert() method to confirm the user's selection.
 
-/*do {
-  var foodChoice = prompt("What would you like to eat?\nPlease type 1,2 or 3:\n(1) Pizza\n(2) Pasta\n(3) Salad")
-  if (foodChoice = 1) {
-    alert('You have choosen Pizza!')
-  } else if (foodChoice = 2) {
-    alert('You have choosen Pasta!')
-  } else if (foodChoice = 3) {
-    alert('You have choosen Salad!')
-  } else {
-    alert('Your choice is not available, please enter 1, 2 or 3')
-  }
-} while (foodChoice != 1)
-
-alert('we are now out of the loop') */
-
-
 let foodChoice = 0;
 do {
   foodChoice = prompt("What would you like to eat?\nPlease type 1,2 or 3:\n(1) Pizza\n(2) Pasta\n(3) Salad")
@@ -47,12 +31,14 @@ do {
 while (foodChoice >= 4);
 alert('we are now out of the loop')
 
-
 // Step 3 - Subtype choice
 // Your code goes here
 
 // Step 4 - Age
 // Your code goes here
+
+const userAge = prompt("What is your age?")
+const pizzaSize = userAge >= 18 ? "Adult size pizza" : "Child sized pizza"
 
 // Step 5 - Order confirmation
 // Your code goes here
