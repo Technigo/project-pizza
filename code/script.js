49a927b8b3bcafc2dbc726156d2952de07becca2
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2023-08-23 19:45:25 +0200

    completed step 4

--- code/script.js
+++ code/script.js
@@ -40,28 +40,95 @@ alert(`You chose ${selectedFood}!`)
 // Step 3 - Subtype choice
 // Your code goes here
 
-//Select a pizza type: 
-//Enter a number: 
-//1 - Napolitana
-//2 - Hawaiian
-//3 - Pepperoni
-
 let selectedSubtype = "";
 
 switch (selectedFood) {
   case "Pizza":
     selectedSubtype = prompt(`Please select a pizza type:
-  1 - Napolitana
-  2 - Hawaii
-  3 - Pepperoni
+  1 - Capricciosa
+  2 - Vegetariana
+  3 - Quattro Formaggi
   Please enter the number of your choice.`);
     break;
+  case "Pasta":
+    selectedSubtype = prompt(`Please select a pasta type:
+  1 - Pasta Carbonara
+  2 - Pasta Puttanesca
+  3 - Pasta Bolognese
+  Please enter the number of your choice.`);
+    break;
+  case "Salad":
+    selectedSubtype = prompt(`Please select a salad type:
+  1 - Caprese salad
+  2 - Caesar salad
+  3 - Nicoise salad
+  Please enter the number of your choice.`);
+    break;
+  default:
+    alert("Invalid choice.")
+    process.exit(1)
 }
 
-console.log(selectedSubtype)
+let subtypeName = "";
+if (selectedFood === "Pizza") {
+  switch (selectedSubtype) {
+    case "1":
+      subtypeName = "Capricciosa"
+      break
+    case "2":
+      subtypeName = "Vegeteriana"
+      break
+    case "3":
+      subtypeName = "Quattro Formaggio"
+      break
+  }
+} else if (selectedFood === "Pasta") {
+  switch (selectedSubtype) {
+    case "1":
+      subtypeName = "Pasta Carbonara"
+      break
+    case "2":
+      subtypeName = "Pasta Puttanesca"
+      break
+    case "3":
+      subtypeName = "Pasta Bolognese"
+      break
+  }
+} else if (selectedFood === "Salad") {
+  switch (selectedSubtype) {
+    case "1":
+      subtypeName = "Caprese salad"
+      break
+    case "2":
+      subtypeName = "Caesar salad"
+      break
+    case "3":
+      subtypeName = "Nicoise salad"
+      break
+  }
+}
+
+console.log(subtypeName)
+alert(`You've chosen ${subtypeName}!`);
 
 // Step 4 - Age
 // Your code goes here
+const age = prompt(`Is this food for a child or an adult? Type your age`);
+
+console.log(age)
+
+let price = (age >= 18) ? 120 : 60;
+let ageType = (age >= 18 ? "adult" : "child")
+
+console.log(price)
+console.log(ageType)
+
+const confirm = prompt(`One ${ageType} sized ${subtypeName} will be prepared for you. That'll be ${price}SEK. Are you sure you want to order this?
+Enter a number to confirm:
+1 - Yes
+2 - No`)
+
+console.log(confirm);
 
 // Step 5 - Order confirmation
 // Your code goes here
