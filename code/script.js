e95595cc6b575f44235d490df35b3c07a0e39b55
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-08-23 12:00:45 +0200

    Step two food choice, done

--- code/script.js
+++ code/script.js
@@ -14,7 +14,31 @@ alert(
 )
 
 // Step 2 - Food choice
-// Your code goes here
+
+const foodChoise = prompt(`Please choose from the list:
+  1 - Pizza
+  2 - Pasta
+  3 - Salad
+  Please enter the number of your choise:`)
+
+let selectedFood = ""
+
+
+if (foodChoise === "1") {
+  selectedFood = "pizza"
+} else if (foodChoise === "2") {
+  selectedFood = "pasta"
+} else if (foodChoise === "3") {
+  selectedFood = "salad"
+} else {
+  alert(`You haven't chosen a food. Please select a number from the list.`)
+  Process.exit(1)
+}
+
+alert(
+  `You have chosen ${selectedFood}`
+)
+
 
 // Step 3 - Subtype choice
 // Your code goes here
