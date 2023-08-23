75086e2c0a8395577729e942b05b37a0b23c0f3d
Anna Robertsson <robertsson_anna@hotmail.com>
2023-08-23 12:00:44 +0200

    finished javascript step 2

--- code/script.js
+++ code/script.js
@@ -1,5 +1,7 @@
 // Start here
 
+// NOTE TO SELF `` || '' || "" ????????
+
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
@@ -15,6 +17,29 @@ alert(
 
 // Step 2 - Food choice
 // Your code goes here
+const foodChoice = prompt(`What are you craving today?
+  1 - Pizza
+  2 - Pasta
+  3 - Salad
+  Please choose the number of what you would like to order:`)
+console.log(foodChoice)
+
+let selectedFood = ""
+
+if (foodChoice === "1") {
+selectedFood = "pizza"
+} else if (foodChoice === "2") {
+  selectedFood = "pasta"
+} else if (foodChoice === "3") {
+  selectedFood = "salad"
+} else {
+  alert("We can't identify what type of food you want to order. Please choose a valid number.")
+  process.exit(1)
+}
+
+alert(
+  `You have chosen to order a delicious ${selectedFood}!`
+)
 
 // Step 3 - Subtype choice
 // Your code goes here
