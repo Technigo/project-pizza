60fd071f83bfddeed60f4be3191e1ca9bd1b2773
Idah Collin <idah_collin@hotmail.com>
2023-08-23 12:36:46 +0200

    Step 2

--- code/script.js
+++ code/script.js
@@ -12,6 +12,29 @@ alert("Welcome! Let's order some yummy food " + username + "!");
 
 // Step 2 - Food choice
 // Your code goes here
+// Menu
+var choosefood = prompt("What would you like to order? 1 = Pizza, 2 = Pasta, 3 = Salad", "Type your number of choice here");
+
+let foodName;
+let numericChoosefood = parseInt(choosefood);
+
+switch (numericChoosefood) {
+  case 1:
+    foodName = "Pizza"
+    break;
+  case 2:
+    foodName = "Pasta"
+    break;
+  case 3:
+    foodName = "Salad"
+    break;
+  default:
+    foodName = "Invalid choice";
+}
+
+// Alert confirming menu selection
+alert("Yummy! " + foodName + "! What a great choice!");
+
 
 // Step 3 - Subtype choice
 // Your code goes here
