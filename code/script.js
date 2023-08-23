ced752964a75f8b59f68d741cb84fe78c89596bf
jforjanice <jjanice.cheng@gmail.com>
2023-08-23 22:42:53 +0200

    Farewell message added

--- code/script.js
+++ code/script.js
@@ -1,9 +1,4 @@
 
-// Your code goes here
-
-// Step 5 - Order confirmation
-// Your code goes here
-
 // Step 1 - Welcome and introduction
 alert(
   `Welcome to our pineapple JS Pizzeria. Ready to Start? - Click 'OK' to begin.`
@@ -51,57 +46,79 @@ let selectedChoice = "";
 
 //select pizza
 switch (selectedMenu) {
-  case "pizza":
+  case "Pizza":
     foodName = prompt ( `Which pizza would you like to order?
     1 - Signature Hawaiian
-    2 - Nodic smoked salmon pizza
-    3 - Kabab pizza
-    4 - Kimchi BBQ chicken pizza
+    2 - Nodic Smoked Salmon Pizza
+    3 - Kabab Pizza
+    4 - Kimchi BBQ Chicken Pizza
     Please select the number of your choice`);
-    selectedChoice = ["Signature Hawaiian", "Nodic smoked salmon pizza", "Kabab pizza", "Kimchi BBQ chicken pizza"]
+    selectedChoice = ["Signature Hawaiian", "Nodic Smoked Salmon Pizza", "Kabab Pizza", "Kimchi BBQ chicken pizza"]
     break;
-    case "pasta":
+    case "Pasta":
     foodName = prompt ( `Which pasta would you like to order?
-    1 - Spicy garlic butter linguini
+    1 - Spicy Garlic Butter Linguini
     2 - Spaghetti Chicken Teriyaki
-    3 - Cheesy taco pasta bake
+    3 - Cheesy Taco Pasta Bake
     Please select the number of your choice`);
-    selectedChoice = ["Spicy garlic butter linguini", "Spaghetti Chicken Teriyaki", "Cheesy taco pasta bake"]
+    selectedChoice = ["Spicy Garlic Butter Linguini", "Spaghetti Chicken Teriyaki", "Cheesy Taco Pasta Bake"]
     break;
-    case "salad":
+    case "Salad":
     foodName = prompt ( `Which salad would you like to order?
     1 - Tropical Mango Salad
     2 - Vietnamese Rice Noodle Salad with Chicken
     3 - Caesar Salad with BBQ chicken
     Please select the number of your choice`);
     selectedChoice = ["Tropical Mango Salad", "Vietnamese Rice Noodle Salad with Chicken", "Caesar Salad with BBQ chicken"]
-      //Error message will be shown if invalid input is selected  
+    break; 
+    //Error message will be shown if invalid input is selected  
       default:
       alert("Please select again.");
       process.exit(1);
 }
-alert(`You have selected ${selectedChoice}.`)
+alert(`You have selected ${selectedChoice[0]}.`)
 
 // Step 4 - Age
-// const age = prompt (`Is this an order for kid? Please enter the age.`);
-// let userAge = "",
-// if(userAge => 18){
-//   alert(`You have ordered a ${}`)
-// }
+// Set food price
+let price = "";
+switch(selectedMenu){
+  case "Pizza":
+  price = "110";
+  break;
+  case "Pasta":
+  price = "130";
+  break;
+  case "Salad":
+  price = "120";
+  break;
+}
+
+let childPrice = price/2;
+
+// Get user age with promt function
+// use if else to change the price if the user order for a kid
+let userAge = prompt (`Is this an order for a child or an adult? Please enter the age.`);
+
+  if(userAge >= 18){
+    alert(`You have selected ${selectedChoice[0]}, and the total will be ${price}kr.`);
+  } else if(userAge <= 18){
+    alert(`You have selected ${selectedChoice[0]}, and the total will be ${childPrice}kr.`);
+  } else {
+    alert(`Please select again.`);
+    process.exit(1);
+  }
+  // Step 5 - Order confirmation
+  let confirmation = prompt(`Do you confirm your order? Please enter the number:
+  1 - Yes
+  2 - No `);
+  // Triger different action according to user last answer
+  let farewellMsg = "";
+    if (farewellMsg === 1){
+      alert(`Thank you! Your order confirmation will be send to your email shortly.`)
+    };
+    else if(farewellMsg === 2){
+      alert(`Please select your order again.`)
+      process.exit(1);
+    }
 
 
-// switch (selectedPizza) {
-// case "Nodic smoked salmon pizza":
-//   console.log("You picked Nodic smoked salmon pizza.");
-//   break;
-// case "Kabab pizza":
-//   console.log("You picked kabab pizza.");
-//   break;
-// case "Spicy Italian pizza":
-//   console.log("You picked Spicy Italian pizza.");
-//   break;
-// case "Kimchi BBQ chicken pizza":
-//   console.log("You picked Kimchi BBQ chicken pizza.");
-// defalut:
-// console.log(`Sorry, we are out of ${pizzaChoice}.`);
-// }
