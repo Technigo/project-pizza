61edaf6839e29acf7b91850d5f07994629260a5d
Isa Robertini <robertiniisa@gmail.com>
2023-08-23 13:38:01 +0200

    try

--- code/script.js
+++ code/script.js
@@ -14,7 +14,10 @@ if (person != null) {
 // Step 2 - Food choice
 let foodOptionText;
 
-let foodOption = prompt(`What would you like to order? \n 1 Pizza \n 2 Pasta \n 3 Salad`); {
+let foodOption = prompt(`What would you like to order?
+1 - Pizza 
+2 - Pasta 
+3 - Salad`); {
 
   if (foodOption === `1`) {
     alert(`Pizza is a great choice!`);
@@ -23,13 +26,17 @@ let foodOption = prompt(`What would you like to order? \n 1 Pizza \n 2 Pasta \n 
   } else if (foodOption === `3`) {
     alert(`wow, salad, that is a great choice!`);
   } else {
-    alert(`Oh... are you not hungry? Please choose one of the options!`);
+    alert(`Oh... are you not hungry? 
+    Please choose one of the options!`);
   }
 }
 // Step 3 - Subtype choice
 
 if (foodOption === `1`) {
-  let pizzaOption = prompt(`Which Pizza would you like to order? \n 1 Pizza Bianca \n 2 Pizza con pomodorini \n 3 Quattro Stagioni`);
+  let pizzaOption = prompt(`Which Pizza would you like to order?
+  1 - Pizza Bianca 
+  2 - Pizza con pomodorini 
+  3 - Quattro Stagioni`);
   foodOptionText = `Pizza Bianca`
   if (pizzaOption === `1`) {
     alert(`You have choosen Pizza Bianca, amazing!`);
@@ -43,7 +50,10 @@ if (foodOption === `1`) {
     alert(`Please choose a number between 1-3`);
   }
 } else if (foodOption === `2`) {
-  let pastaOption = prompt(`Which pasta would you like to order? \n 1 Pasta Alio e Olio \n 2 Pasta con Pomodorini \n 3 Pasta allo Scoglio`);
+  let pastaOption = prompt(`Which pasta would you like to order? 
+  1 - Pasta Alio e Olio 
+  2 - Pasta con Pomodorini 
+  3 - Pasta allo Scoglio`);
   if (pastaOption === `1`) {
     foodOptionText = `Pasta Alio e Olio`
     alert(`You have choosen Pasta Alio e Olio, amazing!`);
@@ -58,7 +68,10 @@ if (foodOption === `1`) {
   }
 
 } else if (foodOption === `3`) {
-  let saladOption = prompt(`Which salad would you like to order? \n 1 Tuna Salad \n 2 Broccoli Salad \n 3 Fruit Salad`);
+  let saladOption = prompt(`Which salad would you like to order? 
+  1 - Tuna Salad 
+  2 - Broccoli Salad 
+  3 - Fruit Salad`);
   foodOptionText = `Tuna Salad`
   if (saladOption === `1`) {
     alert(`You have choosen Tuna Salad, amazing!`);
@@ -87,14 +100,20 @@ if (ageOption >= 18) {
 // Step 5 - Order confirmation
 
 if (ageOption >= 18) {
-  let confirmation = prompt(`One ${foodOptionText} will be prepared for you. That will be 15€. Are you sure you want to proceed with your order? \n Enter a number to confirm: \n 1- yes \n 2- no`);
+  let confirmation = prompt(`One ${foodOptionText} will be prepared for you. That will be 15€. Are you sure you want to proceed with your order? 
+  Enter a number to confirm: 
+  1 - yes 
+  2 - no`);
   if (confirmation === `1`) {
     alert(`Thank you for your order! This is you confirmation.`);
   } else if (confirmation === `2`) {
     alert(`Are you not hungry anymore? refresh the page to start over. Otherwise, hope to see you next time!`)
   }
 } else {
-  let confirmation = prompt(`One ${foodOptionText} will be prepared for you. That will be 10€. Are you sure you want to proceed with your order? \n Enter a number to confirm: \n 1- yes \n 2- no`);
+  let confirmation = prompt(`One ${foodOptionText} will be prepared for you. That will be 10€. Are you sure you want to proceed with your order?
+  Enter a number to confirm:
+   1 - yes
+   2 - no`);
   if (confirmation === `1`) {
     alert(`Thank you for your order! This is you confirmation.`);
   } else if (confirmation === `2`) {
