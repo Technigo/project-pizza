d10a6154126beb9b58c7e8420916f2bc9ad1cf58
EvelynDelCarmen <hey@evelyndelcarmen.com>
2023-08-23 21:35:06 +0200

    foodchoice

--- code/script.js
+++ code/script.js
@@ -10,9 +10,29 @@ const userName = prompt("Please enter your name:");
 alert(`Hello,${userName}! Let's get started.`);
 
 
-
 // Step 2 - Food choice
 // Your code goes here
+
+const foodChoice = prompt(`Select your yummy food:
+
+  1 - Pizza
+  2 - pasta
+  3 - Sallad
+  Please enter the number of your choice:`);
+
+let selectedFood = "";
+
+if (foodChoice === "1") {
+  selectedFood = "Pizza";
+} else if (foodChoice === "2") {
+  selectedFood = "Pasta";
+} else if (foodChoice === "3") {
+  selectedFood = "Salad";
+} else {
+
+  alert("Invalid choice. Please select a valid number.");
+  process.exit(1);
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
