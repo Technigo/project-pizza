c7d1b4467ee733e9c3dfbffa63c4c5b6aa3f1b9c
Shiqwan <ulrika.galnander@gmail.com>
2023-08-23 15:25:44 +0200

    Topping choices added

--- code/script.js
+++ code/script.js
@@ -30,7 +30,7 @@ if (foodChoice === "1") {
 } else if (foodChoice === "3") {
   selectedFood = "Salad";
 } else {
-// Note to self, you use the numbers to signify what the choice is to the code, the answ.er you write separaately, you dont "pull" the answer from the prompt
+// Note to self, you use the numbers to "target" what the choice is to the code, the answer you write separately, you dont "pull" the answer-word from the prompt
 alert("We dont serve that number - Please select a valid number for the food you want");
 process.exit(1);
 }
@@ -39,6 +39,55 @@ alert(`Yum! Youve chosen ${selectedFood}!`);
 
 // Step 3 - Subtype choice
 // Your code goes here
+
+//For this iteration, the focus is on enabling the user to choose a subtype of the selected food type. Depending on the previously chosen food type (Pizza, Pasta, Salad), the program should present the user with subtype options specific to that food type. The prompt() method should be used to get the user's input. The program should then determine the chosen subtype and provide feedback using the alert() method to confirm the selection. Tip: Use conditional statements for this.
+
+let chosen = "";
+let options = ""; 
+
+switch (selectedFood) {
+  case "Pizza":
+    chosen = prompt(`Select your toppings apart from Cheese and Tomato:
+    1 - Mushrooms and Ham
+    2 - Pineapple and Banana
+    3 - Beef and Bea
+    Please enter the number of your choice:`);
+  options = ["Mushrooms and Ham", "Pineapple and Banana","Beef and Bea"];
+  break;
+  case "Pasta":
+    chosen = prompt(`Select your type of Pasta:
+    1 - Creamy Lemon
+    2 - Fresh Basil
+    3 - Spicy Tomatoes
+    Please enter the number of your choice:`);
+  options = ["Creamy Lemon", "Fresh Basil","Spicy Tomatoes"];
+  break;
+  case "Salad":
+    chosen = prompt(`Select your salad of choice:
+    1 - Crunchy Broccoli
+    2 - Leafy Lettuce
+    3 - Sundried Feta
+    Please enter the number of your choice:`);
+  options = ["Crunchy Broccoli", "Leafy Lettuce","Sundried Feta"];
+  break;
+  default:
+    alert("Invalid Choice!");
+    process.exit(1);
+}
+
+if (chosen === "1") {
+  alert(
+    `You have selected ${options[0]} as toppings for your ${selectedFood}!`
+    );
+} else if (chosen === "2") {
+  alert(
+    `You have selected ${options[1]} version of ${selectedFood}!`
+    );
+} else if (chosen === "3") { 
+  alert(
+    `You have selected the ${options[2]}-${selectedFood}!`
+    );
+}
 
 
 
