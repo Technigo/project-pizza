89bd44f818c617f7c6ad3e5e0492a6ad21569e15
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-08-23 19:50:26 +0200

    Step 1 and 2

--- code/script.js
+++ code/script.js
@@ -1,13 +1,40 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+
+let userName = prompt(
+  'Please type your name.'
+);
+
+alert(
+  `Welcome ${userName}!`
+);
 
 // Step 2 - Food choice
-// Your code goes here
+let foodChoice = prompt(
+  `What type of food would you like to order?
+  Please enter a number:
+
+  1. Pizza
+  2. Salad
+  3. Kebab`
+);
+
+let foodName = 'Pizza';
+
+if (foodChoice === 2) {
+  foodName = 'Salad'
+}
+
+if (foodChoice === 3) {
+  foodName = 'Kebab'
+};
+
+alert(`You have chosen ${foodName}`);
+
 
 // Step 3 - Subtype choice
 // Your code goes here
