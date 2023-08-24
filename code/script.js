ba27f148b1bd3c8a0027d6842f18ac9d2e4d029f
verawitting <135651734+verawitting@users.noreply.github.com>
2023-08-24 21:28:22 +0200

    Added menu choice and pizza options

--- code/script.js
+++ code/script.js
@@ -1,19 +1,55 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
-let person = prompt("What's your name? :) ", "Write it here!");
 
-alert(`Hi Diego! Also hi ${person}!`)
+alert(
+  `Welcome to our Javascript Pizzeria. \nReady to Start? - Click 'OK' to begin.`
+)
+let person = prompt(`What's your name? :) `, `Write it here!`);
+if (person !== null || `Write it here!` ) {
+alert(`Hi ${person}! Hope you're hungry to order!`);
+} else { 
+  alert(`No worries, let's continue anyway!`)
+} 
+//Cant get the error message right???
 
 // Step 2 - Food choice
-// Your code goes here
+
+let foodType = prompt(`Can I take your order?
+\nChoose from today's menu!
+\n1 - Pizza
+\n2 - Pasta
+\n3 - Salad`,
+`Please enter the number of your dish`); 
+
+let foodTypeConfirm = "";
+if(foodType === "1"){ foodTypeConfirm = `Pizza`;} 
+if(foodType === "2"){ foodTypeConfirm = `Pasta`;} 
+if(foodType === "3"){ foodTypeConfirm = `Salad`;} 
+alert(`You have chosen ${foodTypeConfirm}`)
 
 // Step 3 - Subtype choice
-// Your code goes here
+
+let pizzaType
+let pastaType
+let saladType
+let foodOrder
+
+if (foodTypeConfirm = `Pizza`){
+  pizzaType = prompt(
+    `Great! We have these to choose from:
+    \n1 - Margherita
+    \n2 - Verdure
+    \n3 - Surprise`
+  ); if (pizzaType === "1"){
+    foodOrder = `Margherita`
+  } if (pizzaType === "2"){
+    foodOrder = `Verdure`
+  }if (pizzaType === "3"){
+    foodOrder = `Surprise`
+  }
+  alert(`You have ordered ${foodOrder}`)
+}
 
 // Step 4 - Age
 // Your code goes here
