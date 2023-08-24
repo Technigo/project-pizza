7d84e33f0ca519e18e0dadf2ede7254f433d802e
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-08-24 18:01:06 +0700

    Update script.js
    Update script.js based on the following requirements:
    
    1. the prompt() method to ask for the user’s name.
    2. the prompt() method to present the user with a menu of options (Pizza, Pasta, Salad) and prompt them to enter a number corresponding to their choice.
    3. enabling the user to choose a subtype of the selected food type. Depending on the previously chosen food type (Pizza, Pasta, Salad), the program should present the user with subtype options specific to that food type.

--- code/script.js
+++ code/script.js
@@ -5,9 +5,33 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
+let userName = prompt("What's your name?");
+alert(`Hi ${userName}!`);
 
 // Step 2 - Food choice
 // Your code goes here
+
+let foodChoice = prompt(
+  `Hello ${userName}, what would you like to order?\n1. Pizza\n2. Pasta\n3. Salad`
+);
+let chosenFood;
+switch (foodChoice) {
+  case "1":
+    chosenFood = "Pizza";
+    break;
+  case "2":
+    chosenFood = "Pasta";
+    break;
+  case "3":
+    chosenFood = "Salad";
+    break;
+  default:
+    chosenFood = "Unknown";
+}
+alert(`You've chosen ${chosenFood}.`);
+
+
+
 
 // Step 3 - Subtype choice
 // Your code goes here
