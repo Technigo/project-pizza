251fcc25a092025a0cc5bf619a7b804909f0456d
mariateresepettersson <mariateresepettersson@gmail.com>
2023-08-24 09:36:21 +0200

    Step 1 - enter name and welcome message

--- code/script.js
+++ code/script.js
@@ -6,6 +6,13 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+//Ask user for their name using prompt method (when using conts the variable name doesn't appear in the alert. it says that it's deprecated)
+let name = prompt(`What's your name? - ex. Maria`)
+
+//Display a greeting to confirm users name
+alert(`Hi ${name}! Welcome to Javascript Pizzeria.`)
+
+
 // Step 2 - Food choice
 // Your code goes here
 
