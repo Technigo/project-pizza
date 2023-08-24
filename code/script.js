019b472754fa7a7027247146e83a22f90425daa3
mirelcac <141237662+mirelcac@users.noreply.github.com>
2023-08-24 16:08:45 +0200

    Step 1 and 2

--- code/script.js
+++ code/script.js
@@ -1,19 +1,43 @@
-// Start here
-
-// Step 1 - Welcome and introduction
-// Your code goes here
+// STEP 1 - Welcome and introduction
+//Alertmessage: Welcome
+//Promptmessage: Please enter name
+//Alertmessage: Thank you + name
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  `Welcome to our Mirelas Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
-// Step 2 - Food choice
-// Your code goes here
+let text;
+let NAME = prompt("Please enter your full name");
 
-// Step 3 - Subtype choice
-// Your code goes here
+alert(
+  `Thank you ` + NAME + `! Now continue to your order.`
+)
+//STEP 1 END
 
-// Step 4 - Age
-// Your code goes here
+// STEP 2 - Food choice
+// Promptmessage: Asking the user to choose a meal out of a list of options:
+const foodChoice = prompt(`Select your favourite meal!:
+  1 - Pizza
+  2 - Pasta
+  3 - Salad
+  Please enter the number of your choice:`);
 
-// Step 5 - Order confirmation
-// Your code goes here
+// Connect the customer choice to the list and response messages (4 steps:)
+//1. Initialize a variable to store the selected food.
+let selectedChoice = "";
+
+//2. Determine the selected food based on the user's choice.
+if (foodChoice === "1") {
+  selectedChoice = "Pizza";
+} else if (foodChoice === "2") {
+  selectedChoice = "Pasta";
+} else if (foodChoice === "3") {
+  selectedChoice = "Salad";
+} else {
+  //3. Invalid choice - Errormessage:
+  alert("Invalid choice. Please select a valid number.");
+  process.exit(1);
+}
+//4. Alertmessage: Confirm the selceted food option.
+alert(`You've chosen ${selectedChoice}!`);
+// STEP 2 END //
