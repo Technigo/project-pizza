f08c6922b9787d3fc3c27ffe00b72cfac773f2d5
verawitting <135651734+verawitting@users.noreply.github.com>
2023-08-24 13:36:51 +0200

    first step

--- code/script.js
+++ code/script.js
@@ -5,6 +5,9 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
+let person = prompt("What's your name? :) ", "Write it here!");
+
+alert(`Hi Diego! Also hi ${person}!`)
 
 // Step 2 - Food choice
 // Your code goes here
