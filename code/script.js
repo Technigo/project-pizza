6734c4c8f7469857be05e1037c3adeb55cce71a9
mariateresepettersson <mariateresepettersson@gmail.com>
2023-08-24 19:41:44 +0200

    Updates to step 2

--- code/script.js
+++ code/script.js
@@ -13,35 +13,32 @@ alert(`Hi ${userName}! Welcome to Javascript Pizzeria.`)
 
 
 // Step 2 - Food choice
-//Present options and get user's input
+
 const foodType = prompt(`Choose the type of food that you'd like to order.
 Enter a number:
 1 - Pizza
 2 - Pasta
-3 - Sallad`)
+3 - Salad`);
 
-//Convert user input to a number
-const userChoice = parseInt(foodType)
+const userChoice = parseInt(foodType);
 
-//Determine the choosen food type based on input from user
-let chosenFoodType;
+let chosenFoodType; //Why can't I use const here instead of let?
 
 if (userChoice === 1) {
-  chosenFoodType = "Wonderful, you've chosen Pizza!";
+  chosenFoodType = alert("Wonderful, you've chosen Pizza");
 } else if (userChoice === 2) {
-  chosenFoodType = "Wonderful, you've chosen Pasta!";
+  chosenFoodType = alert("Wonderful, you've chosen Pasta");
 } else if (userChoice === 3) {
-  chosenFoodType = "Wonderful, you've chosen Salad!";
+  chosenFoodType = alert("Wonderful, you've chosen Salad");
 } else {
-  chosenFoodType = "Invalid choice. Please select a number between 1-3."
+  chosenFoodType = alert("Invalid choice. Please select a number between 1-3.");
 }
 
-//Confirmation message 
-alert(chosenFoodType);
+
 
 
 // Step 3 - Subtype choice
-// Your code goes here
+
 
 // Step 4 - Age
 // Your code goes here
