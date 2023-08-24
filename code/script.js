f92be971d996009100f7495834f0d67e61811f61
hamdiolad <oladhamdi@gmail.com>
2023-08-24 13:49:50 +0200

    added name prompt and a welcome alert

--- code/script.js
+++ code/script.js
@@ -6,8 +6,18 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+const userName = prompt("What is you name?")
+
+alert(
+  `Nice to meet you ${userName}!`
+)
+
+
 // Step 2 - Food choice
 // Your code goes here
+
+const foodChoice = prompt("What food would you like to order today? Enter a number: 1.Pizza 2.Pasta 3.Salad ")
+
 
 // Step 3 - Subtype choice
 // Your code goes here
