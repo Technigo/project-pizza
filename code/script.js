be750550a3c44d21cc785c783310cbcdc0d8c702
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-08-24 18:46:17 +0700

    Update script.js
    The updated script.js are the following:
    1. prompt the user to specify if the food is intended for a child or an adult.
    2. confirming the user's order based on their previous choices.

--- code/script.js
+++ code/script.js
@@ -32,12 +32,48 @@ alert(`You've chosen ${chosenFood}.`);
 
 
 
-
 // Step 3 - Subtype choice
 // Your code goes here
+
+let subtypeChoice;
+if (chosenFood !== "Unknown") {
+  if (chosenFood == "Pizza") {
+    subtypeChoice = prompt(
+      `What type of ${chosenFood} would you like?\n1. Margherita\n2. Pepperoni\n3. Veggie`
+    );
+  } else if (chosenFood == "Pasta") {
+    subtypeChoice = prompt(
+      `What type of ${chosenFood} would you like?\n1. Spaghetti\n2. Fettuccine Alfredo\n3. Penne Pesto`
+    );
+  } else if (chosenFood == "Salad") {
+    subtypeChoice = prompt(
+      `What type of ${chosenFood} would you like?\n1. Caesar Salad\n2. Greek Salad\n3. Caprese Salad`
+    );
+  }
+}
+if (subtypeChoice) {
+  alert(`You've chosen ${subtypeChoice}.`);
+}
 
 // Step 4 - Age
 // Your code goes here
 
+let ageChoice = prompt("Is this for a child or an adult?");
+let orderMessage;
+if (ageChoice.toLowerCase() === "child") {
+  orderMessage = `You've ordered a ${chosenFood} for a child. Enjoy your meal!`;
+} else if (ageChoice.toLowerCase() === "adult") {
+  orderMessage = `You've ordered a ${chosenFood} for an adult. Enjoy your meal!`;
+} else {
+  orderMessage = "We're not sure who this order is for. Enjoy your meal!";
+}
+
+
 // Step 5 - Order confirmation
 // Your code goes here
+let confirmation = prompt(`${orderMessage} Confirm your order? (yes/no)`);
+if (confirmation.toLowerCase() === "yes") {
+  alert("Thank you for your order! Your meal will be prepared.");
+} else {
+  alert("We hope to see you again soon. Have a great day!");
+}
