aded3ea3f6c3cd8d5a248f66bbadda2432d22ad1
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2023-08-24 16:24:15 +0200

    -m changes

--- code/script.js
+++ code/script.js
@@ -14,7 +14,7 @@ alert (
 
 // Step 2 - Food choice
 // Your code goes here
-const Food = prompt ("What's your craving today: 1 - Pizza  2 - Pasta  3- Salad Please enter the number of your choice!")
+let Food = prompt ("What's your craving today: 1 - Pizza  2 - Pasta  3- Salad Please enter the number of your choice!")
 
 let selectedFood ="";
 
@@ -44,7 +44,7 @@ alert (
 // Step 3 - Subtype choice
 // Your code goes here Ask the user to inser subtype based on choice of food.
 let subtype  = "";
-let subtypeName = ""; //Store name for the subtypes pizza, pasta and salad
+let subtypeName = []; //Store name for the subtypes pizza, pasta and salad
 
 switch (selectedFood) {
   case "Pizza":
@@ -52,41 +52,29 @@ switch (selectedFood) {
     subtypeName = ["Margherita", "Veggie", "Nutella"];
     break;
     case "Pasta":
-      subtype = prompt ('Select the type of pizza you want: 1 - Shrimp 2 - Veggie 3 - Elf style Please enter the number of your choice:');
+      subtype = prompt ('Select the type of pizza you want: 1 - Shrimppasta 2 - Veggiepasta 3 - Elf style pasta Please enter the number of your choice:');
       subtypeName = ["Shrimp", "Veggie", "Elf style"];
       break;
       case "Salad":
-        subtype = prompt ('Select the type of pizza you want: 1 - Shrimp 2 - Greek 3 - Tomato Please enter the number of your choice:');
+        subtype = prompt ('Select the type of pizza you want: 1 - Shrimpsalad 2 - Greeksalad 3 - Tomatosalad Please enter the number of your choice:');
         subtypeName = ["Shrimp", "Greek", "Tomato"];
         break;
 }
-// To Technigo, I have tried out different codes now, and I now
+// To Technigo, I have tried out different codes now. The problem is that the subtypeName is tied to the variable selectedFood and I can't get it to change. I tried doing and if/if else under every variable, added
+//It's probably supereasy but I think my mind is just stuck at the moment/took water over my head etc etc
 
 if (selectedFood === "Pizza")
 {
   alert(
   `You chose ${subtypeName[0]} as your meal`);
-  (
-    `You chose ${subtypeName[1]} as your meal`);
-    (
-      `You chose ${subtypeName[2]} as your meal`);
-
   
   } else if (selectedFood === "Pasta") {
   alert(
-    `You chose ${subtypeName[0]} as your meal`);
-    (
-      `You chose ${subtypeName[1]} as your meal`);
-      (
-        `You chose ${subtypeName[2]} as your meal`);
+    `You chose ${subtypeName[1]} as your meal`);
 }
 
 else if (selectedFood === "Salad") {
-alert(
-  `You chose ${subtypeName[0]} as your meal`);
-  (
-    `You chose ${subtypeName[1]} as your meal`);
-    (
+alert (
       `You chose ${subtypeName[2]} as your meal`);
 }
 
