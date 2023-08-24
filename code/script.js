9ee68006f0eb7d6cd37e6fad0a06d711a7d86389
Julia Holm <juliaholm@julias-mbp.home>
2023-08-24 13:41:20 +0200

    -

--- code/script.js
+++ code/script.js
@@ -1,19 +1,57 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  `Welcome to my Javascript Pizza shop! Ready to Start? - Click 'OK' to begin.`
+)
+
+// Customers name
+const customerName = prompt(
+  `Please enter your name:`
+)
+
+alert(
+  `Hi, ${customerName}. In the next step, you will pick your food choice.`
 )
 
 // Step 2 - Food choice
-// Your code goes here
+const foodType = prompt (
+  `This is what we have.
+  Enter a number:
+  1 - Pizza
+  2 - Pasta
+  3 - Fish`
+)
+
+let foodChoice
+
+if (foodType == 1) {
+  foodChoice = "Pizza"
+  alert (`You've chosen ${foodChoice}!`)
+}
+
+else if (foodType == 2) {
+  foodChoice = "Pasta"
+  alert (`You've chosen ${foodChoice}!`)
+}
+
+else if (foodType == 3) {
+  foodChoice = "Fish"
+  alert (`You've chosen ${foodChoice}!`)
+}
+
+else {
+  alert (`I don't have the choice you made.
+  Please refresh and pick a number between 1-3.`)
+}
+
+let pizzaType
+let pastaType
+let fishType
+let subtypeChoice
+
+//We offer three delicious pizzas.
+//Enter a number:
 
 // Step 3 - Subtype choice
-// Your code goes here
-
-// Step 4 - Age
-// Your code goes here
-
-// Step 5 - Order confirmation
 // Your code goes here
