608cdae0f17226ed4ea86b07ea64ff5124c473a3
Idah Collin <idah_collin@hotmail.com>
2023-08-24 18:00:45 +0200

    changed var to let and const

--- code/script.js
+++ code/script.js
@@ -1,17 +1,14 @@
-// Start here
-
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 );
 
-var username = prompt("What's your name?", "Confused Developer");
+const username = prompt("What's your name?", "Confused Developer");
 
 alert("Welcome! Let's order some yummy food " + username + "!");
 
 // Step 2 - Food choice
-var choosefood = prompt("What would you like to order? 1 = Pizza, 2 = Pasta, 3 = Salad", "Type your number of choice here");
+let choosefood = prompt("What would you like to order? 1 = Pizza, 2 = Pasta, 3 = Salad", "Type your number of choice here");
 
 let foodName;
 
@@ -22,7 +19,7 @@ if (choosefood === "1") {
 } else if (choosefood === "3") {
   foodName = "Salad";
 } else {
-  alert(`Sorry, invalid number. Please start over.`);
+  alert(`Sorry, invalid number. Please start over.`); process.exit(1);
 }
 
 alert("Yummy! " + foodName + "! What a great choice!");
@@ -37,11 +34,11 @@ if (foodName === "Pizza") {
 } else if (foodName === "Salad") {
   specifyfood = prompt("Select a type of salad: 1 = Spicy salad, 2 = Yummy salad, 3 = Yummy yummy salad", "Type your number of choice here");
 } else {
-  alert(`Sorry, invalid choice. Please start over.`);
+  alert(`Sorry, invalid choice. Please start over.`); process.exit(1);
 }
 
 //Determining the chosen subtype and providing feedback using alert method to confirm the selection.
-var finalchoice
+let finalchoice
 
 if (foodName === "Pizza") {
   if (specifyfood === "1") {
@@ -80,23 +77,23 @@ if (foodName === "Pizza") {
     alert(`You choosed the ${foodName}: ${finalchoice}`);
   }
 } else {
-  alert(`Invalid choice. Please start over.`);
+  alert(`Invalid choice. Please start over.`); process.exit(1);
 }
 
 // Step 4 - Age. Adolescent/adult or child
-var age = prompt("Portion sizes differs depending on if it's a child or adolescent/adult.", "Please type your age here");
+let age = prompt("Portion sizes differs depending on if it's a child or adolescent/adult.", "Please type your age here");
 
 if (age >= 16) {
-  alert(`One adult sized ${finalchoice}. That'll be 10euros.`)
+  alert(`One adult sized ${finalchoice}. That'll be 10euros. Confirm in next box please.`)
 } else if (age <= 15) {
-  alert(`One child sized ${finalchoice}. That'll be 7euros.`)
+  alert(`One child sized ${finalchoice}. That'll be 7euros. Confirm in next box please`)
 }
 
 // Step 5 - Order confirmation
-var confirmation = prompt("Enter a number to confirm: 1: Yes, 2: No.");
+let confirmation = prompt("Enter a number to confirm: 1: Yes, 2: No.");
 
 if (confirmation === "1") {
   alert(`Your ${finalchoice} is being prepared. Yummy!`)
 } else {
-  alert(`Ok, we'll be happy to prepare a ${finalchoice} or another yummy meal for you another time! Welcome back!`)
+  alert(`Ok, we'll be happy to prepare a ${finalchoice} or another yummy meal for you another time! Welcome back!`); process.exit(1);
 }
