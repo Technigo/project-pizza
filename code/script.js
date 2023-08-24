47ee535810db653d91117f90d9acf31361f4bfe6
Sandra <sandragustafsson@sandras-mbp.home>
2023-08-24 13:51:27 +0200

    worked on the issues with orderconfirmation

--- code/script.js
+++ code/script.js
@@ -9,11 +9,15 @@ const username = prompt("Please enter your name!");
 alert(`Hallo ${username} lets make your order!`);
 
 // Step 2 - Food choice
+//
+// ... (your previous code)
+
+// Step 2 - Food choice
 // Your code goes here
 const foodChoice = prompt(`What would you like to eat today?:
   1 - Pasta
   2 - Pizza
-  3 - Sallad
+  3 - Salad
   Please enter the number of your choice:`);
 
 let chosenFood = "";
@@ -23,10 +27,10 @@ if (foodChoice === "1") {
 } else if (foodChoice === "2") {
   chosenFood = "Pizza";
 } else if (foodChoice === "3") {
-  (chosenFood = "Sallad");
+  chosenFood = "Salad";
 } else {
-alert("Invalid choice, please select a valid number! ");
-process.exit(1);
+  alert("Invalid choice, please select a valid number! ");
+  process.exit(1);
 }
 
 alert(`You've chosen to eat a ${chosenFood}!`);
@@ -34,46 +38,64 @@ alert(`You've chosen to eat a ${chosenFood}!`);
 // Step 3 - Subtype choice
 // Your code goes here
 
-let selectedDish = "";
-let dishes = ""; // container for the array.
+let selectedDish = ""; // Initialize selectedDish as an empty string
 
 switch (chosenFood) {
   case "Pasta":
-    selectedDish = prompt(`Please select your favorit Pasta:
+    selectedDish = prompt(`Please select your favorite Pasta:
     1 - Bolognese
     2 - Carbonara
     3 - Fettuccine
     Please enter the number of your choice!`);
-    dishes = ["Bolognese, Carbonara, Fettuccine"];
+    dishes = ["Bolognese", "Carbonara", "Fettuccine"];
     break;
   case "Pizza":
-      selectedDish = prompt(`Please select your favorite Italian Pizza:
-        1 - Margherita (Tomatosauce, mozarella, basil)
-        2 - Caprese (tomato, buffalo mozarella, basil, oliveoil) 
-        3 - Quattro formaggio (Mozarella, Gorgonzola, Provolone, Pecorino)
-        Please enter the number of your choice!`);
-        dishes = ["Margherita, Caprese, Quattro formaggio"];
+    selectedDish = prompt(`Please select your favorite Italian Pizza:
+    1 - Margherita (Tomatosauce, mozarella, basil)
+    2 - Caprese (tomato, buffalo mozarella, basil, oliveoil) 
+    3 - Quattro formaggio (Mozarella, Gorgonzola, Provolone, Pecorino)
+    Please enter the number of your choice!`);
+    dishes = ["Margherita", "Caprese", "Quattro formaggio"];
     break;
-  case "Sallad":
-          selectedDish = prompt(`Please select your favorite sallad!:
-          1 - Ceasarsallad (Chicken, omansallad, parmesan, ceasardressing)
-          2 - Greeksallad (Lettuce, fetacheese, red onions, cucumber, tomato)
-          3 - Shrimpsallas (Lettuce, corn, tomato, cucumber, shrimps , dressing)
-          `);
-          dishes = ["Ceasarsallad, Greeksallad, Shrimpsallad"];
-          break;
-          default:
-            alert("Invalid choice, please select a valid number! ");
-            process.exit(1);
+  case "Salad":
+    selectedDish = prompt(`Please select your favorite salad!:
+    1 - Caesar Salad (Chicken, romaine lettuce, parmesan, Caesar dressing)
+    2 - Greek Salad (Lettuce, feta cheese, red onions, cucumber, tomato)
+    3 - Shrimp Salad (Lettuce, corn, tomato, cucumber, shrimps, dressing)
+    Please enter the number of your choice!`);
+    dishes = ["Caesar Salad", "Greek Salad", "Shrimp Salad"];
+    break;
+  default:
+    alert("Invalid choice, please select a valid number! ");
+    process.exit(1);
 }
+
+// ... (your previous code)
+
 // Step 4 - Age
 // Your code goes here
-const age = prompt(`please enter your age`);
+const age = prompt(`Please enter your age`);
 
-if (age >= 15) {
-  alert(You are a old enough to have a normal sized dish at )
-}
+// Calculate selectedDishNumber here
+const selectedDishNumber = Number(selectedDish) - 1;
 
 
 // Step 5 - Order confirmation
 // Your code goes here
+
+if (selectedDishNumber >= 0 && selectedDishNumber < dishes.length) {
+  const mealSize = age >= 15 ? "normal sized dish" : "juniorsized meal";
+  const mealPrice = age >= 15 ? "13€" : "7€";
+
+  const confirmation = confirm(`Hi ${username}, please confirm your order:
+  You've chosen ${dishes[selectedDishNumber]} for your ${mealSize} at a price of ${mealPrice}.
+  Click OK to confirm or Cancel to decline.`);
+
+  if (confirmation) {
+    alert("Thank you for your order! Your meal will be prepared and will be ready in about 15 minutes.");
+  } else {
+    alert("Thank you for considering us. We hope to serve you in the future. Have a great day!");
+  }
+} else {
+  alert("Invalid dish selection. Please try again.");
+}
