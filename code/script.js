eefd2e718bb1e631a3ff107b77a607c23ac49e29
Manne <manne.johansson@me.com>
2023-08-24 18:58:35 +0200

    Finished step 1

--- code/script.js
+++ code/script.js
@@ -2,9 +2,14 @@
 
 // Step 1 - Welcome and introduction
 // Your code goes here
+
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
+
+// Asks for the user's name using prompt and stores the answer in a variable
+const userName = prompt(`Please enter your name:`);
+alert(`Hi ${userName}! Click 'OK' to choose type of food.`);
 
 // Step 2 - Food choice
 // Your code goes here
