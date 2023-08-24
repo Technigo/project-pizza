edf6a16344c67248f50439963a593eef4327c456
hamdiolad <oladhamdi@gmail.com>
2023-08-24 23:25:11 +0200

    added age and order confirmation

--- code/script.js
+++ code/script.js
@@ -30,12 +30,16 @@ if (foodChoice === "1") {
 } else if (foodChoice === "3") {
   selectedFood = "Salad";
 } else {
-  alert (`Invalid choice. Please select a valid number ${userName}`);
-  process.exit (1);
+  alert(`Invalid choice. Please select a valid number ${userName}`);
+  process.exit(1);
 }
 
 
-alert(`You've choosen ${selectedFood}!`)
+alert(
+  `Nice ${userName} , you've choosen ${selectedFood}!`
+)
+
+
 
 // Step 3 - Subtype choice
 // Your code goes here
@@ -45,41 +49,86 @@ let foodOption = "";
 
 switch (selectedFood) {
   case "Pizza":
-  foodSlot = prompt (`Select a pizza type:
+    foodSlot = prompt(`Select a pizza type:
   1. Napolitana
   2. Pepperoni
   3. Margherita
   Please enter a number of your choice:`);
-  foodOption =["Napolitana", "Pepperoni", "Margherita"];
-  break;
+    foodOption = ["Napolitana", "Pepperoni", "Margherita"];
+    break;
 
   case "Pasta":
-  foodSlot = prompt (`Select a pasta type:
+    foodSlot = prompt(`Select a pasta type:
   1. Spaghetti carbonara
   2. Ferruccine Alfredo
   3. Cheesy Tortellini
   Please enter a number of your choice:`);
-  foodOption =["Spaghetti carbonara", "Ferruccine Alfredo", "Cheesy Tortellini"];
-  break;
+    foodOption = ["Spaghetti carbonara", "Ferruccine Alfredo", "Cheesy Tortellini"];
+    break;
 
   case "Salad":
-    foodSlot = prompt (`Select a salad type:
+    foodSlot = prompt(`Select a salad type:
     1. Ceasar salad
     2. Caprese salad
     3. Greek salad
     Please enter a number of your choice:`);
-    foodOption =["Ceasar salad", " Caprese salad", "Greek salad"]
+    foodOption = ["Ceasar salad", " Caprese salad", "Greek salad"]
     break;
 
-    default:
-      alert (`Invalid choice. Please select a valid number ${userName}`);
-      process.exit (1);
+  default:
+    alert(`Invalid choice. Please select a valid number ${userName}`);
+    process.exit(1);
 }
 
+if (selectedFood === "1") {
+  alert(
+    `Nice ${userName} , you've choosen ${foodOption[0]}!`);
+}
+else if (selectedFood === "2") {
+  alert(`Nice ${userName} , you've choosen ${foodOption[1]}!`);
+}
+else if (selectedFood === "3") {
+  alert(`Nice ${userName} , you've choosen ${foodOption[2]}!`);
+}
+{
+  alert(`Invalid choice. Please select a valid number ${userName}`);
+}
 
 
 // Step 4 - Age
 // Your code goes here
-  
+
+const age = prompt(`Is this food for an adult or a child. Please enter your age.`);
+if (age >= 15) {
+  const confirmation = prompt(`One adult sized ${selectedFood} will be prepared for you. That'll be €20. 
+Are you sure you want to order this? 
+Enter a number to confirm: 
+1. Yes 
+2. No`);
+}
+
+else {
+  const confirmation = prompt(`One child size ${selectedFood} will be prepared for you. That'll be €10. 
+  Are you sure you want to order this? 
+  Enter a numer to confirm: 
+  1. Yes 
+  2. No`);
+}
+
 // Step 5 - Order confirmation
 // Your code goes here
+
+let orderConfirmation = "";
+if (orderConfirmation === "1") {
+  orderConfirmation = "Yes",
+    alert(`Thank you for your order! Your meal will be prepared shortly. See you soon!`);
+}
+
+else if (orderConfirmation === "2") {
+  orderConfirmation = "No",
+    alert(`No problem, welcome back and order any time you want! See you!`)
+}
+
+else {
+  alert(`Invalid choice. Please select a valid number ${userName}`)
+}
