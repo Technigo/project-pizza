e7bdc0176113c40d3a870ebdebc7594e6d2ce7f1
isaackamran <isaackamran@hotmail.com>
2023-08-24 18:54:03 +0200

    step1-2-

--- code/script.js
+++ code/script.js
@@ -1,13 +1,26 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
+
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`);
+  
+// Enter username
+const userName = prompt("Please enter your name");
+
+alert (
+  `Hello, ${userName}! Time to order som pizza.`);
 
 // Step 2 - Food choice
-// Your code goes here
+const pizzaChoice = prompt(`Please select your pizza:
+  1 - Margarita
+  2 - Kebab
+  3 - Fungi
+  4 - Surprise
+  Please enter the number of your choice:`);
+
+alert (
+  `Great you choose, ${pizzaChoice}! Now for the time.`);
 
 // Step 3 - Subtype choice
 // Your code goes here
