d7a14549c04ae361422f870479021e6b30f13152
Carolina Luna <kroluna@gmail.com>
2023-08-24 21:16:30 +0200

    step3

--- code/script.js
+++ code/script.js
@@ -34,7 +34,39 @@ alert(`You have chosen ${selectedFood}`);
 
 // Step 3 - Subtype choice
 // Your code goes here
+let chosenFood = "";
+let foodNames = "";
 
+
+switch (selectedFood) {
+  case "Pizza":
+    chosenFood = prompt(`Select a pizza you'd like to order:
+    1 - Margarita
+    2 - Peperoni
+    3 - Vegetarian
+    Please enter the number for your choice:`);
+    foodNames = ["Margarita", "Peperoni", "Vegetarian"];
+    break;
+  case "Pasta":
+    chosenFood = prompt(`Select a pasta you'd like to order:
+    1 - Tortellini Funghi
+    2 - Gnocchi a la Blue Cheese
+    3 - Spaghetti Roso
+    Please enter the number of your choice:`);
+    foodNames = ["Tortellini Funghi", "Gnocchi a la Blue Cheese", "Spaghetti Roso"];
+    break;
+  case "Salad":
+    chosenFood = prompt(`Select a salad you'd like to order:
+    1 - Ceasar Salad
+    2 - Greek salad 
+    3 - Waldorf Salad
+    Please enter the number of your choice:`);
+    foodNames = ["Ceasar Salad", "Greek Salad", "Waldorf Salad"];
+    break;
+  
+    default:
+    alert("Invalid choice.");
+    process.exit(1);
 // Step 4 - Age
 // Your code goes here
 
