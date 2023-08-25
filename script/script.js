1af1214bce60f951c0fe80e76d7ce5950ecd8c8e
colognia <frauke.schmidt@protonmail.com>
2023-08-25 12:33:32 -0500

    Correct variable names regarding naming convention
    Following JS camelCase correcting some variable names.
    Distribute ternary operator on 3 lines.
    Adding more comments to explain script.

--- script/script.js
+++ script/script.js
@@ -6,17 +6,19 @@ window.alert(
 );
 
 //input and validation for username
-let username = "";
+let userName = "";
+// name seems to be reserved word, in ${name} got crossed out, so picked different one
 do {
-  username = window.prompt("Please fill in your name, minimum 3 characters: ");
-} while (username.length < 3)
-window.alert(`Good to have you here ${username} !`);
-// Your code goes here
+  userName = window.prompt("Please fill in your name, minimum 3 characters: ");
+} while (userName.length < 3)
+window.alert(`Good to have you here ${userName} !`);
+// end step1
 
 // Step 2 - Food choice
 let evaluation = false;
 let choice = null;
 // declare here. if in while-loop not visible later in meal-switch
+
 while (evaluation == false) {
   // not only one =
   choice = window.prompt(`Please choose your preferred type of meal:
@@ -29,6 +31,7 @@ while (evaluation == false) {
   // evaluation of meal type choice
   if (choice === null) {
     throw new Error("Order process terminated.");
+    // triggers an error on purpose which stops script, if user clicked on "cancel"-> cancel deliveres "null"
   } else if (choice != 1 && choice != 2 && choice != 3) {
     // not || ! test if all are wrong
     window.alert("Sorry, invalid choice, please pick 1-3.");
@@ -36,7 +39,7 @@ while (evaluation == false) {
     evaluation = true;
   }
 
-  // --working as well but not cool--
+  // --working as well, but longer--
   // switch (choice) {
   //   case 1:
   //   case 2:
@@ -52,22 +55,22 @@ while (evaluation == false) {
 };
 
 //confirmation user meal type choice
-let mealtype = null;
+let mealType = null;
 choice = Number(choice);
-// ...because is string so far. switch uses strict =
+// ...because is string so far. switch uses strict ===
 switch (choice) {
   case 1:
-    mealtype = "Pizza";
+    mealType = "Pizza";
     break;
   case 2:
-    mealtype = "Pasta";
+    mealType = "Pasta";
     break;
   case 3:
-    mealtype = "Salad";
+    mealType = "Salad";
     break;
 }
-window.alert(`Got it, you picked ${mealtype}.`);
-// Your code goes here
+window.alert(`Got it, you picked ${mealType}.`);
+// end step2
 
 // Step 3 - Subtype choice
 const optionsForPizza = `
@@ -83,57 +86,57 @@ const optionsForSalad = `
 3) Tuna salad
 4) Grandmas noodle salad`;
 
-let subtypes = null;
+let subTypes = null;
 // holds text options for subtypes of meals
 const mealSubtype = [];
 // gets filled with specific meal options
-let ordersum = 0;
+let orderSum = 0;
 switch (choice) {
   case 1:
-    subtypes = optionsForPizza;
+    subTypes = optionsForPizza;
     const pizzaSubtypes = ["Margharita", "Calzone", "Quattro formaggi"];
     for (let i = 0; i < pizzaSubtypes.length; i++) {
       mealSubtype.push(pizzaSubtypes[i]);
     }
-    ordersum = 15;
+    orderSum = 15;
     break;
   case 2:
-    subtypes = optionsForPasta;
+    subTypes = optionsForPasta;
     const pastaSubtypes = ["Bolognese", "al olio"];
     for (let i = 0; i < pastaSubtypes.length; i++) {
       mealSubtype.push(pastaSubtypes[i]);
     }
-    ordersum = 12;
+    orderSum = 12;
     break;
   case 3:
-    subtypes = optionsForSalad;
+    subTypes = optionsForSalad;
     const saladSubtypes = ["Salad greek style", "Asian rice salad", "Tuna salad", "Grandmas noodle salad"];
     for (let i = 0; i < saladSubtypes.length; i++) {
       mealSubtype.push(saladSubtypes[i]);
     }
-    ordersum = 10;
+    orderSum = 10;
 }
 
-//evaluation user suboptions mealchoice 
+//validation user suboptions mealchoice 
 let subchoice = null;
 // holds a number
 evaluation = false;
 // better reset value
 while (evaluation == false) {
-  let usersubchoice = window.prompt("Please enter the number of disired dish:" + subtypes);
-  if (isNaN(usersubchoice) || usersubchoice <= 0 || usersubchoice > mealSubtype.length) {
+  let userSubchoice = window.prompt("Please enter the number of disired dish:" + subTypes);
+  if (isNaN(userSubchoice) || userSubchoice <= 0 || userSubchoice > mealSubtype.length) {
     window.alert(`Sorry, invalid choice, please pick 1 - ${mealSubtype.length} `);
   } else {
     evaluation = true;
   }
-  subchoice = usersubchoice - 1;
+  subchoice = userSubchoice - 1;
   //otherwise value not visible outside. return not working.
 }
 window.alert(`Good choice! You picked: ${mealSubtype[subchoice]} `);
-// Your code goes here
+// end step3
 
 // Step 4 - Age
-//evaluation user age
+//validation user age
 let age = null;
 evaluation = false;
 while (evaluation == false) {
@@ -146,8 +149,7 @@ while (evaluation == false) {
   } else if (ageinput > 0 && ageinput < 16) {
     window.alert("Sorry, you are too youg for business, please ask your parents to order.")
     throw new Error("Order process terminated.");
-  }
-  else if (ageinput > 122) {
+  } else if (ageinput > 122) {
     window.alert("Whow! You are older than the oldest person who ever lived on earth! Or maybe a typo?");
   } else
     evaluation = true;
@@ -156,11 +158,13 @@ while (evaluation == false) {
 //order sum up
 window.alert(`OK, we are almost done.
 This is your order:
-${mealtype}, ${mealSubtype[subchoice]}, price: ${ordersum} Euro.`);
-// Your code goes here
+${mealType}, ${mealSubtype[subchoice]}, price: ${orderSum} Euro.`);
+// end step4
 
 // Step 5 - Order confirmation
 let confirmation = window.confirm("Do you want to order the selected dish now?");
-confirmation === true ? window.alert(`Thank you ${username}, we received your order!
-Our 5 star chef is now preparing your dish right away.`) : window.alert(`What a pity, we hope to see you soon again!`);
-// Your code goes here
+confirmation === true
+  ? window.alert(`Thank you ${userName}, we received your order!
+Our 5 star chef is now preparing your dish right away.`)
+  : window.alert(`What a pity, we hope to see you soon again!`);
+// end step5
