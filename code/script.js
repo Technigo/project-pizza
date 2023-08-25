3c4e857e11183faccbac9f719952c1c0ce901602
jforjanice <jjanice.cheng@gmail.com>
2023-08-25 15:40:29 +0200

    Added selected food to store final choice

--- code/script.js
+++ code/script.js
@@ -6,12 +6,12 @@ alert(
 const userName = prompt(`What is your name?`);
 
 alert(
-  `Hi ${userName}! Don't worry, we don't judge you like pineapple on your pizza
-   here:)`)
+  `Hi ${userName}! Don't worry, we don't judge you like pineapple on your pizza here:)`)
 
 // Step 2 - Food choice
 // Request information of what the customer would like to order
 // User alert to confirm customer's choice
+// I could't make it work in the beginning because I skipped "" for the number
 
 alert(
   `What would you like to order?`
@@ -42,44 +42,67 @@ alert (`You have selected ${selectedMenu}.`)
 // Step 3 - Subtype choice
 // Initiate an array to store different menu
 let foodName = "";
-let selectedChoice = [];
+let selectedChoice = "";
 
 //select pizza
 switch (selectedMenu) {
   case "Pizza":
-    foodName = prompt ( `Which pizza would you like to order?
+    foodName = prompt (`Which pizza would you like to order?
     1 - Signature Hawaiian
     2 - Nodic Smoked Salmon Pizza
     3 - Kabab Pizza
     4 - Kimchi BBQ Chicken Pizza
     Please select the number of your choice`);
-    selectedChoice = ["Signature Hawaiian", "Nodic Smoked Salmon Pizza", "Kabab Pizza", "Kimchi BBQ chicken pizza"]
+    selectedChoice = ["Signature Hawaiian", "Nodic Smoked Salmon Pizza", "Kabab Pizza", "Kimchi BBQ chicken pizza"];
     break;
-    case "Pasta":
-    foodName = prompt ( `Which pasta would you like to order?
+  case "Pasta":
+    foodName = prompt (`Which pasta would you like to order?
     1 - Spicy Garlic Butter Linguini
     2 - Spaghetti Chicken Teriyaki
     3 - Cheesy Taco Pasta Bake
     Please select the number of your choice`);
-    selectedChoice = ["Spicy Garlic Butter Linguini", "Spaghetti Chicken Teriyaki", "Cheesy Taco Pasta Bake"]
+    selectedChoice = ["Spicy Garlic Butter Linguini", "Spaghetti Chicken Teriyaki", "Cheesy Taco Pasta Bake"];
     break;
-    case "Salad":
-    foodName = prompt ( `Which salad would you like to order?
+  case "Salad":
+    foodName = prompt (`Which salad would you like to order?
     1 - Tropical Mango Salad
     2 - Vietnamese Rice Noodle Salad with Chicken
     3 - Caesar Salad with BBQ chicken
     Please select the number of your choice`);
-    selectedChoice = ["Tropical Mango Salad", "Vietnamese Rice Noodle Salad with Chicken", "Caesar Salad with BBQ chicken"]
+    selectedChoice = ["Tropical Mango Salad", "Vietnamese Rice Noodle Salad with Chicken", "Caesar Salad with BBQ chicken"];
     break; 
     //Error message will be shown if invalid input is selected  
-      default:
-      alert("Please select again.");
-      process.exit(1);
+  default:
+    alert("Please select again.");
+    process.exit(1);
 }
-alert(`You have selected ${selectedChoice[]}.`)
+// Display selected choice according to user choice
+// I have problem of displaying the selected choice at the confirmation message. 
+// It showed the whole array instead of the selected choice according to its position.
+// I learned from another classmate at the her demo, I can store the user's choice at this step.
+// It works! 
 
+if (foodName === "1"){
+  alert(`You have selected ${selectedChoice [0]}.`);
+  selectedFood =  selectedChoice [0] ;
+}
+else if (foodName === "2"){
+  alert(`You have selected ${selectedChoice [1]}.`);
+  selectedFood =  selectedChoice [1] ;
+}
+else if (foodName === "3"){
+  alert(`You have selected ${selectedChoice [2]}.`);
+  selectedFood =  selectedChoice [2] ;
+}
+else if (foodName === "4"){
+  alert(`You have selected ${selectedChoice [3]}.`);
+  selectedFood =  selectedChoice [3] ;
+}
 // Step 4 - Age
 // Set food price
+// I only use let and switch here which I don't think is the optimal solution
+// In my head the price should go together with the food choice as an object
+// But I don't know how to create object yet so I solve it with this solution
 let price = "";
 switch(selectedMenu){
   case "Pizza":
@@ -93,16 +116,17 @@ switch(selectedMenu){
   break;
 }
 
+//Lazy solution to set the price for half portion
 let childPrice = price/2;
 
 // Get user age with promt function
-// use if else to change the price if the user order for a kid
+// Use if else to change the price if the user order for a kid
 let userAge = prompt (`Is this an order for a child or an adult? Please enter the age.`);
 
   if(userAge >= 18){
-    alert(`You have selected ${selectedChoice[]}, and the total will be ${price}kr.`);
+    alert(`You have selected ${selectedFood}, and the total will be ${price}kr.`);
   } else if(userAge <= 18){
-    alert(`You have selected ${selectedChoice[]}, and the total will be ${childPrice}kr.`);
+    alert(`You have selected ${selectedFood}, and the total will be ${childPrice}kr.`);
   } else {
     alert(`Please select again.`);
     process.exit(1);
@@ -112,7 +136,6 @@ let userAge = prompt (`Is this an order for a child or an adult? Please enter th
   1 - Yes
   2 - No `);
   // Triger different action according to user last answer
-  let farewellMsg = "";
 
     if (confirmation === "1"){
       alert(`Thank you! Your order confirmation will be send to your email shortly.`);
