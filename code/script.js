d4cfd901139c8c0a8eebd6cac34ae64cbba883ea
Linnea Johansson <Linneajoh@gmail.com>
2023-08-25 10:16:27 +0200

    added food choices

--- code/script.js
+++ code/script.js
@@ -5,12 +5,35 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
+let person = prompt ("Please enter your name");
 
 // Step 2 - Food choice
 // Your code goes here
+let food = prompt (`What type of food would you like to order?
+Please enter a number: 
+1. Pizza
+2. Pasta
+3. Salad`);
 
 // Step 3 - Subtype choice
 // Your code goes here
+//let food = prompt (`Which pizza would you like to order?
+//Please enter a number: 
+//1. Calzone
+//2. Vezuvio
+//3. Hawaii`);
+
+//let food = prompt (`Which pasta would you like to order?
+//Please enter a number: 
+//1. Bolognese
+//2. Carbonara
+//3. Vegetarian`);
+
+//let food = prompt (`Which salad would you like to order?
+//Please enter a number: 
+//1. Ceasar
+//2. Caprese
+//3. Greek salad`);
 
 // Step 4 - Age
 // Your code goes here
