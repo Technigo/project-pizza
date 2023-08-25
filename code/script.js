2606a3b83491362a60cb8a5134f7560d08c7f0a2
Malin <malinlundhe@hotmail.com>
2023-08-25 17:41:09 +0200

    looked through the code, did some changes and worked it thru

--- code/script.js
+++ code/script.js
@@ -33,43 +33,46 @@ alert(`You have good taste! You've chosen ${selectedFood}!`);
 
 // Step 3 - Subtype choice
 // Your code goes here
-let mealChoice="";
-let mealOption="";
+let mealChoice=0;
+let mealOption=[];
 switch (selectedFood) 
 {case "Pizza":
-mealChoice=prompt(`Which pizza do you like to order?
+mealChoice=parseInt (prompt(`Which pizza do you like to order?
 1-Bianco with mozzarella cheese and ham 
 2-Tomatobased pizza with mushrooms
 3-Sweet nutellapizza with berries
-Please enter the number of your choice:`);
+Please enter the number of your choice:`));
 mealOption=["Bianco with mozzarella cheese and ham", "Tomatobased pizza with mushrooms", "Sweet nutellapizza with berries"];
 break;
 case "Kebab":
-mealChoice=prompt(`What kebab-dish do you like to order?
+mealChoice=parseInt (prompt(`What kebab-dish do you like to order?
 1-Kebab with bread
 2-Kebab with pommes frites
 3-Kebab of the house
-Please enter the number of your choice:`)
+Please enter the number of your choice:`))
 mealOption=["Kebab with bread", "Kebab with pommes frites", "Kebab of the house"];
 break;
 case "Salad":
-mealChoice=prompt(`Which salad do you like to order?
+mealChoice=parseInt (prompt(`Which salad do you like to order?
 1-Greek salad
 2-Ceasar salad
 3-Tuna salad
-Please enter the number of your choice:`)
-mealOption=prompt["Greek salad", "Ceasar salad", "Tuna salad"]
-
+Please enter the number of your choice:`))
+mealOption=["Greek salad", "Ceasar salad", "Tuna salad"];
+break;
+  default:
+    alert("Invalid choice for meal subtype.");
 }
 
-if(mealChoice==="1")
-alert(`Good choice! You've selected ${mealOption [0]}`);
-else if(mealChoice==="2")
-alert(`Good choice! You've selected ${mealOption [1]}`);
-else if(mealChoice==="3")
-alert(`Good choice, You've selected ${mealOption [2]}`);
-else
-alert("Oops, that is an invalid choice, please choose between number 1, 2 or 3");
+
+if(mealChoice===1){alert(`Good choice! You've selected ${mealOption[mealChoice - 1]}`);}
+
+else if(mealChoice===2) {alert(`Good choice! You've selected ${mealOption[mealChoice - 1]}`);}
+
+else if(mealChoice===3) {alert(`Good choice, You've selected ${mealOption[mealChoice - 1]}`);}
+
+else {alert("Oops, that is an invalid choice, please choose between number 1, 2 or 3");}
+
 
 // Step 4 - Age
 // Your code goes here
@@ -83,7 +86,9 @@ else
 // Step 5 - Order confirmation
 // Your code goes here
 let confirmation=prompt(`Do you want to order? Please type 1 to order or 2 to cancel order.`)
-if(confirmation==="1")
-alert `Your order is confirmed!`
+if(confirmation==="1" && age>=18)
+{alert (`Your order is confirmed! You have ordered ${mealOption[mealChoice - 1]}, that will be 17€!`)}
+else if (confirmation==="1" && age<18) 
+{alert (`Your order is confirmed! You have ordered ${mealOption[mealChoice - 1]}, that will be 12€!`)}
 else 
-alert `Your order is canceled, welcome back another time`
+{alert `Your order is canceled, welcome back another time`}
