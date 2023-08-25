9e00667a64eed6e06a290f905096952a46d1775c
Sakura Tanaka <sito6496t@gmail.com>
2023-08-26 00:34:39 +0900

    first part of instractions finished

--- code/script.js
+++ code/script.js
@@ -2,20 +2,28 @@
 
 // Step 1 - Welcome and introduction
 // Your code goes here
-alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`);
-let name = prompt("What is your name?");
-alert(`Wellcome to Technigo pizzaria, ${name}`);
 
+alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`);
+const userName = prompt("What is your name?");
+
+alert(`Wellcome to Technigo pizzaria, ${userName}`);
+
+///////////////////////////////////////////////////////////////////////////////
 // Step 2 - Food choice
 // Your code goes here
 
-let foodTypeNum = prompt(
-  "What type of food would you like to eat? \n Enter a number:\n 1 - Pizza \n 2 - Salad \n 3 - Pasta"
+const foodTypeNum = parseInt(
+  prompt(
+    `What type of food would you like to eat?
+   Enter a number:
+    1 - Pizza 
+    2 - Salad 
+    3 - Pasta`
+  )
 );
 
-foodTypeNum = Number(foodTypeNum);
-
 let foodType;
+
 switch (foodTypeNum) {
   case 1:
     foodType = "Pizza";
@@ -28,21 +36,30 @@ switch (foodTypeNum) {
     break;
   default:
     alert("Invalid choice. Please select a number between 1 and 3");
+    process.exsit(1);
 }
 
 // confirming what type of food you want to eat
 
 alert(`Would you want to eat ${foodType}? Click "ok" to confirm`);
 
+///////////////////////////////////////////////////////////////////////////////
 // Step 3 - Subtype choice
 // Your code goes here
+
 let dishNum;
 let yourDish;
+
 if (foodType === "Pizza") {
-  dishNum = prompt(
-    "Which food would like to order? \n Enter a number: \n 1 - Vegan Pizza\n 2 - Vegan Cheese Pizza \n 3 - Vegetarian Pizza"
+  dishNum = parseInt(
+    prompt(
+      `Which food would like to order? \n Enter a number:
+       1 - Vegan Pizza
+       2 - Vegan Cheese Pizza 
+       3 - Vegetarian Pizza`
+    )
   );
-  dishNum = Number(dishNum);
+
   switch (dishNum) {
     case 1:
       yourDish = " Vegan Pizza";
@@ -55,13 +72,19 @@ if (foodType === "Pizza") {
       break;
     default:
       alert("Invalid choice. Please select a number between 1 and 3");
+      process.exsit(1);
   }
 } else if (foodType === "Salad") {
-  dishNum = prompt(
-    "Which food would like to order? \n Enter a number: \n 1 - Vegan Feta Salad\n 2 - Italian Salad \n 3 - Grilled Vegan Cheese Salad"
+  dishNum = parseInt(
+    prompt(
+      `Which food would like to order? 
+       Enter a number: 
+       1 - Vegan Feta Salad
+       2 - Italian Salad 
+       3 - Grilled Vegan Cheese Salad`
+    )
   );
 
-  dishNum = Number(dishNum);
   switch (dishNum) {
     case 1:
       yourDish = " Vegan Feta Salad";
@@ -74,12 +97,19 @@ if (foodType === "Pizza") {
       break;
     default:
       alert("Invalid choice. Please select a number between 1 and 3");
+      process.exsit(1);
   }
 } else if (foodType === "Pasta") {
-  dishNum = prompt(
-    "Which food would like to order? \n Enter a number: \n 1 - Tomato Pasta\n 2 - Vegan Cream Pasta \n 3 - Ruccola and Dryed Tomato Pasta"
+  dishNum = parseInt(
+    prompt(
+      `Which food would like to order? 
+      Enter a number: 
+      1 - Tomato Pasta
+      2 - Vegan Cream Pasta 
+      3 - Ruccola and Dryed Tomato Pasta`
+    )
   );
-  dishNum = Number(dishNum);
+
   switch (dishNum) {
     case 1:
       yourDish = " Tomato Pasta";
@@ -92,17 +122,19 @@ if (foodType === "Pizza") {
       break;
     default:
       alert("Invalid choice. Please select a number between 1 and 3");
+      process.exsit(1);
   }
 }
 
+// confirming which dish you want to eat
+
 alert(`You've chosen ${yourDish}!  - Click "ok" to confirm`);
 
+///////////////////////////////////////////////////////////////////////////////
 // Step 4 - Age
 // Your code goes here
 
-let age = prompt("Is this dish for a child or an adult? - Enter your age.");
-
-age = Number(age);
+let age = parseInt(prompt(`Is this dish for a child or an adult? - Enter your age.`));
 
 let isChild;
 
@@ -112,13 +144,20 @@ if (age >= 12) {
   isChild = true;
 }
 
-let isSure = prompt(
-  `One ${isChild ? "child" : "adult"} sized ${yourDish} will be prepared for you. That'll be ${
-    isChild ? 10 : 15
-  } euro. Are you sure to order this? \n Enter a number to confirm: \n 1 - Yes\n 2 - No`
-);
+///////////////////////////////////////////////////////////////////////////////
+// Step 5 - Order confirmation
+// Your code goes here
 
-isSure = Number(isSure);
+let isSure = parseInt(
+  prompt(
+    `One ${isChild ? "child" : "adult"} sized ${yourDish} will be prepared for you. That'll be ${
+      isChild ? 10 : 15
+    } euro. Are you sure to order this?
+   Enter a number to confirm: 
+  1 - Yes
+  2 - No`
+  )
+);
 
 switch (isSure) {
   case 1:
@@ -129,6 +168,5 @@ switch (isSure) {
     break;
   default:
     alert("Invalid choise. Please enter a number 1 or 2.");
+    process.exsit(1);
 }
-// Step 5 - Order confirmation
-// Your code goes here
