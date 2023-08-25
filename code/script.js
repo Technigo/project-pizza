f85753628d2d0e58b9e132551d533c1dd56e8a52
elbines <141184135+elbines@users.noreply.github.com>
2023-08-25 17:07:53 +0200

    finished version, change wording

--- code/script.js
+++ code/script.js
@@ -37,19 +37,17 @@ else if (options === "4") {
   selectedOption = "Dessert"
 }
 else {
-  alert("Invalid choise. Please select a valid number");//Message works. If invalid number is entered when choosing type of food the message appears.
+  alert("Invalid choise. Please select a valid number");
   process.exit(1);
 }
 
-alert(`You've chosen ${selectedOption}!`);
-
-
+alert(`You've chosen ${selectedOption}! Please continue to the next step to place your order`);
 
 // Step 3 - Subtype choice
 // Your code goes here
 
 let menuFoodSelected = "";
-let typeOf = ""; //array to store for later choises?  for selected type of food from first menu?
+let typeOf = "";
 
 switch (selectedOption) {
   case "Pizza":
@@ -59,7 +57,7 @@ switch (selectedOption) {
     3 - Margherita
     4 - Calzone
     Please enter the number of your choise:`);
-    typeOf = ["Quattro Formaggi", "Pepperoni", "Margherita", "Calzone"]; //obs, don't forget square baccets 
+    typeOf = ["Quattro Formaggi", "Pepperoni", "Margherita", "Calzone"];
     break;
   case "Pasta":
     menuFoodSelected = prompt(`Select type of pasta:
@@ -69,7 +67,7 @@ switch (selectedOption) {
     4 - Yum
     Please enter the number of your choise:`);
     typeOf = ["Carbonara", "Bolognaise", " WoW", "Yum"];
-    break;
+    break;//obs, don't forget square baccets 
   case "Salad":
     menuFoodSelected = prompt(`Select type of salad:
     1 - Green
@@ -88,14 +86,11 @@ switch (selectedOption) {
     Please enter the number of your choise:`);
     typeOf = ["Tiramisu", "Gelato", "Chocolate Fondant", "Yummy in my tummy"]; //HOW to have less than four choises and getting a default message if I enter 4?
     break;
-  default: //Why is default and alert not working?
-    alert(`Invalid choise, Please refresh to try again`);
-    process.exit(1);
 }
 
 if (menuFoodSelected === "1") {
   alert(
-    `Great Choice, You've chosen ${typeOf[0]} from the ${selectedOption} menu!`);
+    `Great Choice, You've chosen a ${typeOf[0]} ${selectedOption}!`);
 } else if (menuFoodSelected === "2") {
   alert(
     `Great Choice, You've chosen ${typeOf[1]} from the ${selectedOption} menu!`);
@@ -107,30 +102,39 @@ else if (menuFoodSelected === "4") {
   alert(
     `Great Choice, You've chosen ${typeOf[3]} from the ${selectedOption} menu!`);
 }
-
-
+else {
+  alert(`Invalid choise, please select a number between 1-4 to make an order`)
+}
 
 // Step 4 - Age
 // Your code goes here
-let answer = "";
-let yourAge = prompt("Enter your age")
-if (yourAge <= 18) {
-  answer = prompt(`You have chosen a child sized ${typeOf}. That will be €15.
-    Enter Y for Yes
-    Enter N for No`);
+// I really wanted to figure out how to get a message to display the order message and the associated cost alert and the confirmation promt in one message. Do I have to change this to an alert and a promt seperated from eachother to meet the requrements?
+
+let order = "";
+
+let age = prompt("Please enter an age to choose if the dish is meant for an adult or a child.")
+if (age >= 18) {
+  order = prompt(`You have chosen an adult sized ${selectedOption} and ${typeOf[parseInt(menuFoodSelected) - 1]}. That will be €20.
+  Enter Y for "Yes" to confirm.
+  Enter N for "No" to decline`);
 }
-else if (yourAge >= 18) {
-  answer = prompt(`You have chosen an adult sized ${typeOf}. That will be €25.
-  Enter Y for Yes
-  Enter N for No`);
+else if (age <= 18) {
+  order = prompt(`You have chosen an child sized ${typeOf[parseInt(menuFoodSelected) - 1]} ${selectedOption} for your meal. That will be €15.
+  Enter Y for "Yes" to confirm.
+  Enter N for "No" to decline`);
 }
-//SOMETHING IS WRONG INSIDE ${typeOf}., THIS IS THE MESSAGE I GET: You have chosen a child sized Quattro Formaggi,Pepperoni,Margherita,Calzone. That will be €15. Enter Y for Yes Enter N for No
-
-// HOW CAN I make everything but numbers to give a default alert?
-
-
+//I spent a lot of time trying to figgure out the right way to write the code so that the typeOf array chosen earlier would be the same here as well. I kept trying with ${typeOf[0]} ${typeOf[0, 1 , 2 , 3]} and so on. Not sure if I copletly understand the "parseInt" yet, but for now it owrks. 
 
 // Step 5 - Order confirmation
 // Your code goes here
-//The final iteration involves confirming the user's order based on their previous choices. The program should evaluate the user's confirmation and if the user confirms, a thank you message should be displayed using the alert() method, indicating that their meal will be prepared. If the user declines, a polite farewell message should be displayed using the alert() method, encouraging them to return for future orders.
+if (order === "Y") {
+  alert(`Thank you for your order.`);
+}
+else if (order === "N") {
+  alert(`Hope to see you again!`);
+}
+else {
+  alert(`Invalid input, your order was not placed. If you still want to order please try again and enter Y to comfirm`)
+}
 
+//if the user confirms, a thank you message should be displayed using the alert() method, indicating that their meal will be prepared. If the user declines, a polite farewell message should be displayed using the alert() method, encouraging them to return for future orders.
