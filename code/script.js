de06eef0a264fc0f0353ddf5a19bbe296b7e4175
mariateresepettersson <mariateresepettersson@gmail.com>
2023-08-25 09:31:30 +0200

    Added changes to step 2, 3 and 4.

--- code/script.js
+++ code/script.js
@@ -14,34 +14,99 @@ alert(`Hi ${userName}! Welcome to Javascript Pizzeria.`)
 
 // Step 2 - Food choice
 
-const foodType = prompt(`Choose the type of food that you'd like to order.
+const foodChoice = prompt(`Choose the type of food that you'd like to order.
 Enter a number:
 1 - Pizza
 2 - Pasta
 3 - Salad`);
 
-const userChoice = parseInt(foodType);
+//Why do I already need to define these 2 in step2?
+let chosenSubtype; //subtype choice - define dish based on user's choice to alert in next step
+let subtypeMessage; //Prepare for next step because I need 2 variables in the condition to define the correct message 
 
-let chosenFoodType; //Why can't I use const here instead of let?
+const foodType = parseInt(foodChoice);
 
-if (userChoice === 1) {
-  chosenFoodType = alert("Wonderful, you've chosen Pizza");
-} else if (userChoice === 2) {
-  chosenFoodType = alert("Wonderful, you've chosen Pasta");
-} else if (userChoice === 3) {
-  chosenFoodType = alert("Wonderful, you've chosen Salad");
+let chosenFoodType; //Declare a variable to store the chosen food type
+
+if (foodType === 1) {
+  chosenFoodType = "Pizza";
+  alert("Wonderful, you've chosen Pizza.");
+} else if (foodType === 2) {
+  chosenFoodType = "Pasta";
+  alert("Wonderful, you've chosen Pasta.");
+} else if (foodType === 3) {
+  chosenFoodType = "Salad";
+  alert("Wonderful, you've chosen Salad.");
 } else {
   chosenFoodType = alert("Invalid choice. Please select a number between 1-3.");
 }
 
-
-
-
 // Step 3 - Subtype choice
+if (chosenFoodType) {
+  let subtype;
 
+  if (chosenFoodType === "Pizza") {
+    subtype = prompt("What type of pizza would you like?\n1 - Margherita\n2 - Pepperoni\n3 - Veggie");
+    subtypeMessage = "Pizza";
+  } else if (chosenFoodType === "Pasta") {
+    subtype = prompt("What type of pasta would you like?\n1 - Spaghetti\n2 - Alfredo\n3 - Pesto");
+    subtypeMessage = "Pasta";
+  } else if (chosenFoodType === "Salad") {
+    subtype = prompt("What type of salad would you like?\n1 - Caesar\n2 - Greek\n3 - Garden");
+    subtypeMessage = "Salad"
+  } else {
+    // Handle invalid food choice
+    alert("Invalid food choice.");
+  }
+
+  // Based on the chosen subtype, set the subtype message
+  if (chosenFoodType === "Pizza") {
+    if (subtype === "1") {
+      chosenSubtype = "Margherita";
+    } else if (subtype === "2") {
+      chosenSubtype = "Pepperoni";
+    } else if (subtype === "3") {
+      chosenSubtype = "Veggie";
+    }
+  } else if (chosenFoodType === "Pasta") {
+    if (subtype === "1") {
+      chosenSubtype = "Spaghetti";
+    } else if (subtype === "2") {
+      chosenSubtype = "Alfredo";
+    } else if (subtype === "3") {
+      chosenSubtype = "Pesto";
+    }
+  } else if (chosenFoodType === "Salad") {
+    if (subtype === "1") {
+      chosenSubtype = "Caesar";
+    } else if (subtype === "2") {
+      chosenSubtype = "Greek";
+    } else if (subtype === "3") {
+      chosenSubtype = "Garden";
+    }
+  }
+
+  // Display a confirmation message for subtype choice
+  if (chosenSubtype) {
+    alert(`Great choice! You selected ${chosenSubtype} ${subtypeMessage}.`);
+  } else {
+    alert("Invalid subtype choice.");
+  }
+}
 
 // Step 4 - Age
-// Your code goes here
+//Use ternary ?
+const age = prompt(`Is this pizza for a child or an adult? Type your age:`);
+
+let dishSize;
+if (age <= 18) {
+  dishSize = prompt(`One child sized ${chosenSubtype} ${subtypeMessage} will be prepared for you. That'll be €10. Are you sure you want to order this? \nEnter a number to confirm: \n1 - Yes \n2 - No`);
+} else {
+  dishSize = prompt(`One adult sized ${chosenSubtype} ${subtypeMessage} will be prepared for you. That'll be €15. Are you sure you want to order this? \nEnter a number to confirm: \n1 - Yes \n2 - No`);
+}
 
 // Step 5 - Order confirmation
-// Your code goes here
+
+
+//alert(`Thank you for your order! Your delicious meal will be prepared. see you soon!`)
+//alert(`No problem, come back and order anytime you want.`)
