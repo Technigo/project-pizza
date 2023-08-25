5d28739b8364db224d074f8d733c2a33b2426876
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-08-25 03:21:51 +0200

    added a function to help background image to load

--- code/script.js
+++ code/script.js
@@ -1,120 +1,122 @@
 // Step 1 - Welcome and introduction
 
-// This "window.onload" function is used to make sure that the background image loads before the code is executing.
+// "window.onload" function and "setTimeout"-function is used to make sure that the background image loads before the code is executing.
+
 window.onload = function () {
+  setTimeout(function() {
 
-  alert(
-    `Welcome to Tara's Pizzeria. Ready to Start? Click 'OK' to begin 🍕🍝🥗`
-  )
+    alert(
+      `Welcome to Tara's Pizzeria. Ready to Start? Click 'OK' to begin 🍕🍝🥗`
+    )
 
-  const guestName = prompt(`Let's start with your name. What do you call yourself?`)
+    const guestName = prompt(`Let's start with your name. What do you call yourself?`)
 
-  alert(
-    `Hi ${guestName}! We are ready to take your order. `
-  )
+    alert(
+      `Hi ${guestName}! We are ready to take your order. `
+    )
 
-  // Step 2 - Main food choice
+    // Step 2 - Main food choice
 
-  const foodChoice = prompt(`Please choose which delicious food you would like to order:
-    1 - Pizza
-    2 - Pasta
-    3 - Salad
+    const foodChoice = prompt(`Please choose which delicious food you would like to order:
+      1 - Pizza
+      2 - Pasta
+      3 - Salad
 
-    Please enter the number of your choice:`)
+      Please enter the number of your choice:`)
 
-  let selectedFood = ""
+    let selectedFood = ""
 
-  if (foodChoice === "1") {
-    selectedFood = "pizza"
-  } else if (foodChoice === "2") {
-    selectedFood = "pasta"
-  } else if (foodChoice === "3") {
-    selectedFood = "salad"
-  } else {
-    alert(`You haven't chosen any food. Please select a number from the list.`)
-    process.exit(1)
-  }
-  alert(
-    `You have chosen a yummy ${selectedFood}!`
-  )
-
-  // Step 3 - Submeny foodchoice, the dish after the main choice
-
-  let subMenu = ""
-  let dishName = ""
-  let selectedDish = ""
-
-  switch (selectedFood) {
-    case "pizza":
-      subMenu = prompt(`Select which pizza you would like:
-    1 - Mexicana
-    2 - Kebabpizza
-    3 - Hawaii
-
-    Please enter the number of your choice:`)
-      dishName = ["Mexicana", "Kebabpizza", "Hawaii"]
-      break
-
-    case "pasta":
-      subMenu = prompt(`Select which pasta you would like:
-    1 - Spagetti Bolognese
-    2 - Pasta Carbonara
-    3 - Pasta Scampi
-
-    Please enter the number of your choice:`)
-      dishName = ["Spagetti Bolognese", "Pasta Carbonara", "Pasta Scampi"]
-      break
-
-    case "salad":
-      subMenu = prompt(`Select which salad you would like:
-    1 - Caesar Salad
-    2 - Roasted Turkey Club Salad
-    3 - Classic Greec Salad
-
-    Please enter the number of your choice:`)
-      dishName = ["Caesar Salad", "Roasted Turkey Club Salad", "Classic Greec Salad"]
-      break
-    default:
-      alert(`"Invalid Choice. You need to select a number."`)
+    if (foodChoice === "1") {
+      selectedFood = "pizza"
+    } else if (foodChoice === "2") {
+      selectedFood = "pasta"
+    } else if (foodChoice === "3") {
+      selectedFood = "salad"
+    } else {
+      alert(`You haven't chosen any food. Please select a number from the list.`)
       process.exit(1)
-  }
-  selectedDish = dishName[subMenu - 1]
-  alert(`You have chosen a delicious ${selectedDish}!`)
+    }
+    alert(
+      `You have chosen a yummy ${selectedFood}!`
+    )
 
-  // Step 4 - Check age to define which portion size the customer wants
+    // Step 3 - Submeny foodchoice, the dish after the main choice
 
-  const guestAge = parseInt(
-     prompt(`To determine the portion size, please type your age:`)
-   )
+    let subMenu = ""
+    let dishName = ""
+    let selectedDish = ""
 
-  // The order message that defines what the customer has ordered and how much
-  // they must pay. Kids up to 12 y.o. pay less. Age 13 and above pay full price  
+    switch (selectedFood) {
+      case "pizza":
+        subMenu = prompt(`Select which pizza you would like:
+      1 - Mexicana
+      2 - Kebabpizza
+      3 - Hawaii
 
-  let orderMessage = ""
-  if (guestAge <= 12) {
-    orderMessage = `You have chosen a smaller size of ${selectedDish}. That will be £7 euros.`
-  } else if (guestAge >= 13) {
-    orderMessage = `You have chosen a regular size portion of ${selectedDish}. That will be £11 euros.`
-  } else {
-    alert(`Invalid age choice. Please insert a number.`)
-     process.exit(1)
-  }
-  
-  // Step 5 - Order confirmation
-  const confirmationChice = parseInt(
-    prompt(`${orderMessage}
-      Choose a number to confirm:
-      1 - Yes, please!
-      2 - No, I changed my mind.
-    `)
-  )
+      Please enter the number of your choice:`)
+        dishName = ["Mexicana", "Kebabpizza", "Hawaii"]
+        break
 
-  if (confirmationChice === 1) {
-    alert("Thank you for your order. Your food is being prepared and will soon be ready!🍕🍝🥗")
-  } else if (confirmationChice === 2) {
-    alert("No worries, we hope to see you back soon! 🍕🍝🥗")
-  } else {
-    alert("Invalid choice. Please select 1 or 2 above.")
-  }
+      case "pasta":
+        subMenu = prompt(`Select which pasta you would like:
+      1 - Spagetti Bolognese
+      2 - Pasta Carbonara
+      3 - Pasta Scampi
 
+      Please enter the number of your choice:`)
+        dishName = ["Spagetti Bolognese", "Pasta Carbonara", "Pasta Scampi"]
+        break
+
+      case "salad":
+        subMenu = prompt(`Select which salad you would like:
+      1 - Caesar Salad
+      2 - Roasted Turkey Club Salad
+      3 - Classic Greec Salad
+
+      Please enter the number of your choice:`)
+        dishName = ["Caesar Salad", "Roasted Turkey Club Salad", "Classic Greec Salad"]
+        break
+      default:
+        alert(`"Invalid Choice. You need to select a number."`)
+        process.exit(1)
+    }
+    selectedDish = dishName[subMenu - 1]
+    alert(`You have chosen a delicious ${selectedDish}!`)
+
+    // Step 4 - Check age to define which portion size the customer wants
+
+    const guestAge = parseInt(
+      prompt(`To determine the portion size, please type your age:`)
+    )
+
+    // The order message that defines what the customer has ordered and how much
+    // they must pay. Kids up to 12 y.o. pay less. Age 13 and above pay full price  
+
+    let orderMessage = ""
+    if (guestAge <= 12) {
+      orderMessage = `You have chosen a smaller size of ${selectedDish}. That will be £7 euros.`
+    } else if (guestAge >= 13) {
+      orderMessage = `You have chosen a regular size portion of ${selectedDish}. That will be £11 euros.`
+    } else {
+      alert(`Invalid age choice. Please insert a number.`)
+      process.exit(1)
+    }
+    
+    // Step 5 - Order confirmation
+    const confirmationChice = parseInt(
+      prompt(`${orderMessage}
+        Choose a number to confirm:
+        1 - Yes, please!
+        2 - No, I changed my mind.
+      `)
+    )
+
+    if (confirmationChice === 1) {
+      alert("Thank you for your order. Your food is being prepared and will soon be ready!🍕🍝🥗")
+    } else if (confirmationChice === 2) {
+      alert("No worries, we hope to see you back soon! 🍕🍝🥗")
+    } else {
+      alert("Invalid choice. Please select 1 or 2 above.")
+    }
+  }, 1000)
 };
