661462d41d14864e0c19d9a13c42662e23e0300d
EvelynDelCarmen <hey@evelyndelcarmen.com>
2023-08-26 23:22:07 +0200

    added the age section and did final steps

--- code/script.js
+++ code/script.js
@@ -47,27 +47,27 @@ let foodNames = ""
 switch (selectedFood) {
   case "Pizza":
     subFood = prompt(`Select your pizza type:
-    1 - Margherita
-    2 - Napolitana
-    3 - Bianca 
+      1 - Margherita
+      2 - Napolitana
+      3 - Bianca 
     Please enter the number of your choice:`);
     foodNames = ["Margherita", "Napolitana", "Bianca"];
     break;
 
   case "Pasta":
     subFood = prompt(`Select your pasta type:
-    1 - Cacio e pepe
-    2 - Vongole
-    3 -  Carbonara 
+      1 - Cacio e pepe
+      2 - Vongole
+      3 -  Carbonara 
     Please enter the number of your choice:`);
     foodNames = ["Cacio e pepe", "Vongole", "Carbonara"];
     break;
 
   case "Salad":
     subFood = prompt(`Select your salad type:
-    1 - Pomodoro e basilico
-    2 - Buratta
-    3 - Melon e peperoncino
+      1 - Pomodoro e basilico
+      2 - Buratta
+      3 - Melon e peperoncino
     Please enter the number of your choice:`);
     foodNames = ["Pomodoro e basilico", "Buratta", "Melon e peperoncino"];
     break;
@@ -78,25 +78,58 @@ switch (selectedFood) {
 }
 
 if (subFood === "1") {
-  alert(`Great choice, You've selected ${foodNames[0]} for your ${selectedFood}!`)
+  alert(`Great choice, You've selected ${foodNames[parseInt(subFood) - 1]} for your ${selectedFood}!`)
 } else if (subFood === "2") {
-  alert(`Great choice, You've selected ${foodNames[1]} for your ${selectedFood}!`)
+  alert(`Great choice, You've selected ${foodNames[parseInt(subFood) - 1]} for your ${selectedFood}!`)
 } else if (subFood === "3") {
-  alert(`Great choice, You've selected ${foodNames[2]} for your ${selectedFood}!`)
+  alert(`Great choice, You've selected ${foodNames[parseInt(subFood) - 1]} for your ${selectedFood}!`)
 }
 
 // Step 4 - Age
 // Your code goes here
 
-const ageNumber = prompt("Is this food for a child or an adult? Type your age:");
 
-if (ageNumber >= 18) {
-  alert("You're an adult!, your order will cost 15€")
-} else {
-  alert("You're a teenager! your order will cost 7€")
+let age = prompt("Is this food for a child or an adult? Type your age:");
+
+if (age >= 18) {
+  prompt(`You've chosen a regular sized ${foodNames[parseInt(subFood) - 1]}! This will cost you 15€. Are you sure you want to order this?
+
+  
+  1 - Yes
+  2 - No
+  
+  Please enter a number to confirm:`);
+
+} else if (age <= 18) {
+  prompt(`You've chosen a small sized ${foodNames[parseInt(subFood) - 1]}! This will cost you 10€. Are you sure you want to order this? 
+
+  1 - Yes
+  2 - No
+  
+  Please enter a number to confirm:`);
+}
+
+// Step 5 - Order confirmation
+// Your code goes here 
+
+let order = "";
+
+if (order === "1") {
+  alert(`Thank you for your order.`);
+}
+else if (order === "2") {
+  alert(`Hope to see you again!`);
 }
 
 
 
-// Step 5 - Order confirmation
-// Your code goes here
+
+
+
+
+
+
+
+
+
+
