fb59e3059fcffc938002e81e27d6207ef34ccb8a
elbines <141184135+elbines@users.noreply.github.com>
2023-08-27 21:36:45 +0200

    finished-ish

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Project Name
 
-Replace this readme with your own information about your project. Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+The assigment was to Build a JavaScript Pizzeria by using basic JavaScript concepts like variables, conditionals, and native methods. 
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I tried to plan the project by setting "goals" for each day. f.eks: day 3: do step 3 and 4. 
+I wanted to have as much as possible done by the demo on friday. I also knew that I would not have time to do the stretch goals this weekend so I focused on getting the 5 steps done, but if I had more time I would have wanted to do the stretch goals. 
+
+I'm not good at asking for help, so doing so in slack was for me the hardest part this week. Everyone was very helpful so hopefully I´ll remember that for next time. 
+
+I tried a few things (that didn´t work) by making a duplicate of the code where I tested things. The reason for this is that I ruined a lot during the pre-course by "testing" and had a hard time figuring out how to "go back" to where the problems started. 
 
 ## View it live
 
--- code/script.js
+++ code/script.js
@@ -3,27 +3,33 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start?? - Click 'OK' to begin.`
+  `
+  WELCOME TO ELBINES PIZZERIA
+
+  Click OK to start your order.`
 )
 
-//CAN I make it so only letters will be accepted? 
-//HOW can I make the first letter a capital?
 let firstName = prompt("Please enter your first name", "First Name")
 let lastName = prompt("Please enter your last name", "Last Name")
-alert("Welcome " + firstName + " " + lastName + "!")
-//I dont need to devide first and last name in two separate strings, should i make one "yourname" string instead?
+alert("Welcome " + firstName + "!")
+//I dont need to devide first and last name in two separate strings, should i make one "yourName" string instead? Edit: I decided to keep them separated but to only use firstName in the greeting. Code used before: alert("Welcome " + firstName + " " + lastName + "")
+
+//HOW can I change the fist letter in the imput to automatically change to a capital letter? I tested some things, but they did not work.
+//CAN I make it so only letters will be accepted? Tested; did not figure it out. 
 
 
 // Step 2 - Food choice
 // Your code goes here
-const options = prompt(`Please select an option you would like to order:
+let options = prompt(`Please select what you would like by entering the number of the catagory below: 
+
 1 - Pizza
 2 - Pasta
 3 - Salad
-4 - Dessert`);
-//Variable to store selected foodchoise? 
+4 - Desserts`);
+
+//I need to choose words that are simpler and that explain the variable betetr
 let selectedOption = "";
-//Determine selected foodchoise based on the users number choise. 
+
 if (options === "1") {
   selectedOption = "Pizza"
 }
@@ -37,11 +43,13 @@ else if (options === "4") {
   selectedOption = "Dessert"
 }
 else {
-  alert("Invalid choise. Please select a valid number");
+  alert("ERROR. Please select a valid number");
   process.exit(1);
 }
 
-alert(`You've chosen ${selectedOption}! Please continue to the next step to place your order`);
+alert(`You have chosen "${selectedOption}". Continue to the next step to choose a dish.`);
+
+//HOW can I have less than four choises and get a default message if I enter 4 for example? Now im getting "great choise, you have chosen undefined" 
 
 // Step 3 - Subtype choice
 // Your code goes here
@@ -51,90 +59,98 @@ let typeOf = "";
 
 switch (selectedOption) {
   case "Pizza":
-    menuFoodSelected = prompt(`Select a type of Pizza:
+    menuFoodSelected = prompt(`Please select what you would like by entering the number:
+
     1 - Quattro Formaggi
     2 - Pepperoni
     3 - Margherita
     4 - Calzone
-    Please enter the number of your choise:`);
+    `);
     typeOf = ["Quattro Formaggi", "Pepperoni", "Margherita", "Calzone"];
     break;
   case "Pasta":
-    menuFoodSelected = prompt(`Select type of pasta:
+    menuFoodSelected = prompt(`Please select what you would like by entering the number:
+
     1- Carbonara
     2 - Bolognaise 
-    3 - WoW
-    4 - Yum
-    Please enter the number of your choise:`);
-    typeOf = ["Carbonara", "Bolognaise", " WoW", "Yum"];
-    break;//obs, don't forget square baccets 
+    3 - Cacio e pepe
+    4 - Spaghetti allo Scoglio
+    `);
+    typeOf = ["Carbonara", "Bolognaise", " Cacio e pepe", "Spaghetti allo Scoglio"];
+    break;//obs, don't forget square baccets for the arry
   case "Salad":
-    menuFoodSelected = prompt(`Select type of salad:
-    1 - Green
-    2 - Rainbow 
-    3 - Happy
-    4 - Yey
-    Please enter the number of your choise:`);
-    typeOf = ["Green", "Rainbow", "Happy", "Yey"];
+    menuFoodSelected = prompt(`Please select what you would like by entering the number:
+
+    1 - Green Godess
+    2 - Rainbow
+    3 - Greek
+    4 - Fruit
+    `);
+    typeOf = ["Green Godess", "Rainbow", "Greek", "Fruit"];
     break;
   case "Dessert":
-    menuFoodSelected = prompt(`Select type of dessert:
+    menuFoodSelected = prompt(`Please select what you would like by entering the number:
+
     1 - Tiramisu
     2 - Gelato
     3 - Chocolate Fondant
-    4 - Yummy in my tummy 
-    Please enter the number of your choise:`);
-    typeOf = ["Tiramisu", "Gelato", "Chocolate Fondant", "Yummy in my tummy"]; //HOW to have less than four choises and getting a default message if I enter 4?
+    4 - Surprise dish: Yummy in my tummy
+    `);
+    typeOf = ["Tiramisu", "Gelato", "Chocolate Fondant", "Surprise dish; Yummy in my tummy"];
     break;
 }
 
 if (menuFoodSelected === "1") {
   alert(
-    `Great Choice, You've chosen a ${typeOf[0]} ${selectedOption}!`);
+    `Great Choice, You have chosen: ${typeOf[0]} as your ${selectedOption}!`);
 } else if (menuFoodSelected === "2") {
   alert(
-    `Great Choice, You've chosen ${typeOf[1]} from the ${selectedOption} menu!`);
+    `Great Choice, You have chosen: ${typeOf[1]} as your ${selectedOption}!`);
 } else if (menuFoodSelected === "3") {
   alert(
-    `Great Choice, You've chosen ${typeOf[2]}  from the ${selectedOption} menu!`);
+    `Great Choice, You have chosen: ${typeOf[2]} as your ${selectedOption}!`);
 }
 else if (menuFoodSelected === "4") {
   alert(
-    `Great Choice, You've chosen ${typeOf[3]} from the ${selectedOption} menu!`);
+    `Great Choice, You have chosen: ${typeOf[3]} as your ${selectedOption}!`);
 }
 else {
   alert(`Invalid choise, please select a number between 1-4 to make an order`)
 }
+//Should I have used the parseInt here as well to simplyfy the code? Would that make it easier to specify texts for each of the foodtypes?
 
 // Step 4 - Age
 // Your code goes here
-// I really wanted to figure out how to get a message to display the order message and the associated cost alert and the confirmation promt in one message. Do I have to change this to an alert and a promt seperated from eachother to meet the requrements?
+// I really wanted to figure out how to get a message to display  the choises made and the associated cost together with the confirmation promt in one message. Do I have to change this to an alert and a promt seperated from eachother to meet the requrements?
 
 let order = "";
 
-let age = prompt("Please enter an age to choose if the dish is meant for an adult or a child.")
+let age = prompt("Please enter your age to choose if the dish is meant for an adult or a child.")
 if (age >= 18) {
-  order = prompt(`You have chosen an adult sized ${selectedOption} and ${typeOf[parseInt(menuFoodSelected) - 1]}. That will be €20.
+  order = prompt(`You have chosen an adult sized ${typeOf[parseInt(menuFoodSelected) - 1]} ${selectedOption}. That will be €20.
+
   Enter Y for "Yes" to confirm.
   Enter N for "No" to decline`);
 }
 else if (age <= 18) {
-  order = prompt(`You have chosen an child sized ${typeOf[parseInt(menuFoodSelected) - 1]} ${selectedOption} for your meal. That will be €15.
+  order = prompt(`You have chosen an child sized ${typeOf[parseInt(menuFoodSelected) - 1]} ${selectedOption}. That will be €15.
+  
   Enter Y for "Yes" to confirm.
   Enter N for "No" to decline`);
 }
-//I spent a lot of time trying to figgure out the right way to write the code so that the typeOf array chosen earlier would be the same here as well. I kept trying with ${typeOf[0]} ${typeOf[0, 1 , 2 , 3]} and so on. Not sure if I copletly understand the "parseInt" yet, but for now it owrks. 
+//I spent a lot of time trying to figgure out the right way to write the code so that the typeOf array chosen earlier would be the same here as well. I kept trying with ${typeOf[0]} ${typeOf[0, 1 , 2 , 3]} and so on. I got help on slack and during the demos! Not sure if I copletly understand the "parseInt" yet, but for now it works. 
 
 // Step 5 - Order confirmation
 // Your code goes here
+//How to make is so it dosn't matter if a capital letter is entered or a small letter?
+
 if (order === "Y") {
-  alert(`Thank you for your order.`);
+  alert(`Thank you for your order! We will start preparing it right away. We hope you enjoy it!`);
 }
 else if (order === "N") {
-  alert(`Hope to see you again!`);
+  alert(`Not hungry? No worries, we'll be ready for your order when you are!`);
 }
 else {
-  alert(`Invalid input, your order was not placed. If you still want to order please try again and enter Y to comfirm`)
+  alert(`ERROR. Your order was not placed. If you still want to order please try again and enter Y to comfirm`)
 }
 
-//if the user confirms, a thank you message should be displayed using the alert() method, indicating that their meal will be prepared. If the user declines, a polite farewell message should be displayed using the alert() method, encouraging them to return for future orders.
