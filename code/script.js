45608529b4ce3dcfb2cec15b5520272f75937939
Linnea Johansson <Linneajoh@gmail.com>
2023-08-27 16:35:08 +0200

    added alert

--- code/script.js
+++ code/script.js
@@ -3,8 +3,9 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 let person = prompt (`Please enter your name`);
-
-alert (`Hello ${person}!`);
+alert(
+  `Hello ${person}!`
+)
 
 // Step 2 - Food choice
 let foodType = prompt (`What type of food would you like to order?
@@ -32,46 +33,45 @@ else {
 // Step 3 - Subtype choice
 let subtype;
 if (chosenFoodType == `pizza`) {
-  subtype = prompt (`Which pizza would you like to order?
+  subtype = prompt (`What type of pizza would you like to order?
   Please enter a number: 
   1. Calzone
   2. Vezuvio
   3. Hawaii`);
 }
 else if (chosenFoodType == `pasta`) {
-  subtype = prompt (`Which pasta would you like to order?
+  subtype = prompt (`What type of pasta would you like to order?
   Please enter a number: 
   1. Bolognese
   2. Carbonara
   3. Vegetarian`);
 }
 else if (chosenFoodType == `salad`) {
-  subtype = prompt (`Which salad would you like to order?
+  subtype = prompt (`What type of salad would you like to order?
   Please enter a number: 
-  1. Ceasar
-  2. Caprese
+  1. Ceasar salad 
+  2. Kebab salad 
   3. Greek salad`);
 }
 else {
   alert (`Invalid choice, please try again.`);
 }
-
 alert (`Great choice of ${chosenFoodType} ${person}!`);
 
 // Step 4 - Age
-let age = prompt (`is this for a child or an adult? Please enter you age below:`);
+let age = prompt (`Is this for a child or an adult? Please enter you age below:`);
 
 let confirm;
 
 if (age >= 18) {
   confirm = prompt (`You will get an adult sized ${chosenFoodType}, the total vill be 99 SEK. Do you confirm?
-  Please enter 1 for yes and 2 for no
+  Please enter 1 for yes and 2 for no:
   1. Yes
   2. No `); 
 }
 else if (age < 18) {
   confirm = prompt (`You will get a child sized ${chosenFoodType}, the total vill be 75 SEK. Do you confirm?
-  Please enter 1 for yes and 2 for no
+  Please enter 1 for yes and 2 for no:
   1. Yes
   2. No `); 
 }
