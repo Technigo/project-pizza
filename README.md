42b3eb37dbac9f7af439ffb784f38a553c874e17
Anna Robertsson <robertsson_anna@hotmail.com>
2023-08-27 22:11:49 +0200

    updated readme

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# Project Name
+## Project Pizza
 
-Replace this readme with your own information about your project. Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+In this project we were supposed to try out basic JavaScript by creating a pizzeria. By using prompts the user could give their input about different things, like what food they wanted to order. With the input they left we were then supposed to use alerts to confirm the choices the user made.
 
 ## The problem
 
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I have found it challenging to understand JavaScript so I had a hard time to even start the project. To try to understand more of JavaScript I have watched videos, turned to chatGPT for explanations, read materials online and watched code sessions and Q & A with our code coach. I found it very valuable to discuss the code and problems with it with a team mate, and that helped me moving forward.
+I tried to start with the stretch goals but I struggled to much with it, but that is something that I would give another try if I had more time.
+
+In this project no styling was needed, since the focus was the JavaScript. I do want to have a footer in all my projects with links to my LinkedIn etc. though. Therefore I added a few things, but it went smoothly since I used the same footer as I did in a previous project with a few small adjustments. I needed to add a 1 second timeout for the first alert though, since everything in the footer didn't have time to load otherwise because the alert appeared so fast.
 
 ## View it live
 Please visit: https://project-pizza-anna-robertsson.netlify.app/
--- code/script.js
+++ code/script.js
@@ -1,18 +1,15 @@
-// Start here
-
-// NOTE TO SELF `` || '' || "" ????????
-
-// Step 1 - Welcome and introduction
-// Your code goes here
-//Slows down the first alert to make everything have time to load
+//Slows down the first alert to make everything on the site have time to load before the alert appears
 window.onload = function () { 
   setTimeout(function() {
 
+// Step 1 - Welcome and introduction
+//An alert with a welcoming message
 alert(
   `Welcome to our Pizzeria The Cheesy Slice! Ready to Start? - Click 'OK' to begin.`
 )
 
-const guestName = prompt("Dear guest, what is your name?")
+//Using the prompt method to ask for the name of the customer and an alert uses that variable to greet the customer
+const guestName = prompt(`Dear guest, what is your name?`)
 console.log(guestName)
 
 alert(
@@ -20,24 +17,28 @@ alert(
 )
 
 // Step 2 - Food choice
-// Your code goes here
-const foodChoice = prompt(`What are you craving today?
-  1 - Pizza
-  2 - Pasta
-  3 - Salad
-  Please choose the number of what you would like to order.`)
+//Ask the customer for what type of food they want and confirm what they selected in an alert
+const foodChoice = parseInt(
+  prompt(
+    `What are you craving today?
+    1 - Pizza
+    2 - Pasta
+    3 - Salad
+    Please choose the number of what you would like to order.`
+  )
+)
 console.log(foodChoice)
 
 let selectedFood = ""
 
-if (foodChoice === "1") {
+if (foodChoice === 1) {
 selectedFood = "pizza"
-} else if (foodChoice === "2") {
+} else if (foodChoice === 2) {
   selectedFood = "pasta"
-} else if (foodChoice === "3") {
+} else if (foodChoice === 3) {
   selectedFood = "salad"
 } else {
-  alert("We can't identify what type of food you want to order. Please choose a valid number.")
+  alert(`We can't identify what type of food you want to order. Please choose a valid number.`)
   process.exit(1)
 }
 
@@ -46,7 +47,7 @@ alert(
 )
 
 // Step 3 - Subtype choice
-// Your code goes here
+//Using the conditional statement switch method for the customer to choose what dish they want depending on what food they selected previously
 let subMenu = ""
 let dishName = ""
 let selectedDish = ""
@@ -77,11 +78,11 @@ switch (selectedFood) {
     dishName = ["Caesar salad", "Greek salad", "Cobb salad"]
     break
   default:
-    alert("We can't identify what dish you want to order. Please choose a valid number.")
+    alert(`We can't identify what dish you want to order. Please choose a valid number.`)
     process.exit(1)
 }
 
-selectedDish = dishName[subMenu - 1] //Tillägg från Sus.
+selectedDish = dishName[subMenu - 1] 
 
 console.log(dishName)
 
@@ -90,9 +91,9 @@ alert(
 )
 
 // Step 4 - Age
-// Your code goes here
+//Using a prompt to ask for the age of the customer in order to decide a price
 const guestAge = parseInt(
-  prompt("To estimate how big your appetite is - if you're a child or an adult, please tell us your age.")
+  prompt(`To estimate how big your appetite is - if you're a child or an adult, please tell us your age.`)
 )
 
 let orderMessage = ""
@@ -102,13 +103,12 @@ if (guestAge <= 12) {
 } else if (guestAge >= 13) {
   orderMessage = `You have ordered a large ${selectedDish}. That will be 130 SEK. Are you happy with this order?`
 } else {
-  alert("We can't identify your age. Remember - it's just a number! Please enter how old you are below.")
+  alert(`We can't identify your age. Remember - it's just a number! Please enter how old you are below.`)
   process.exit(1)
 }
 
-
 // Step 5 - Order confirmation
-// Your code goes here
+//Using a prompt to ask the customer to accept or decline their order which ends with an alert confirming the order
 const orderConfirmation = parseInt(
   prompt(`${orderMessage}
     Enter a number to confirm or decline your order
@@ -119,15 +119,15 @@ const orderConfirmation = parseInt(
 
 if (orderConfirmation === 1) {
   alert(
-    "Thank you for your order! Your meal is being cooked. Welcome!"
+    `Thank you for your order! Your meal is being cooked. Welcome!`
   )
 } else if (orderConfirmation === 2) {
-  alert("No worries! We hope you'll come back and try our food some other time!")
+  alert(`No worries! We hope you'll come back and try our food some other time!`)
 } else {
-  alert("Invalid choice. Enter 1 to confirm your order or 2 to decline.")
+  alert(`Invalid choice. Enter 1 to confirm your order or 2 to decline.`)
 }
 
-//The delay of the first alert in milliseconds
+//Sets the delay of the first alert in milliseconds
 }, 1000)
 
 }
