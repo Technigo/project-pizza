0dc56b6c8100deae59b15cb1846f3b62d88153ec
verawitting <135651734+verawitting@users.noreply.github.com>
2023-08-27 18:57:16 +0200

    Fixed code problems, it's running

--- code/script.js
+++ code/script.js
@@ -12,8 +12,7 @@ alert(`Hi ${person}! Hope you're hungry to order!`);
 
 // Step 2 - Food choice
 // A function to be able to call step 2 again if user don't type valid number.
-chooseFoodType();
-function chooseFoodType (){
+
 let foodType
 let foodTypeConfirm
 foodType = prompt(`Can I take your order?
@@ -24,16 +23,19 @@ Choose from today's menu!
 `Please enter the number of your dish`); 
 
 foodTypeConfirm = "";
-if(foodType === "1"){ foodTypeConfirm = `Pizza`;} 
-else if(foodType === "2"){ foodTypeConfirm = `Pasta`;} 
-else if(foodType === "3"){ foodTypeConfirm = `Salad`;} 
+if (foodType === "1"){
+  foodTypeConfirm = `Pizza`;
+} else if (foodType === "2"){
+  foodTypeConfirm = `Pasta`;
+} else if(foodType === "3"){
+  foodTypeConfirm = `Salad`;} 
 else { 
   alert("Sorry, we did not catch that. Please type a number.");
-  return chooseFoodType ();
+  process.exit(1);
 }
 alert(`You have chosen ${foodTypeConfirm}. Click ok to see the menu.`);
 
-}
+
 
 // Step 3 - Subtype choice
 
@@ -43,67 +45,87 @@ let saladType
 let foodPrice
 let foodOrder
 
-if (foodTypeConfirm === `Pizza`){
+if (foodTypeConfirm == `Pizza`){
   pizzaType = prompt(
     `Great! We have these to choose from:
-    \n1. Margherita 11€
-    \n2. Verdure    12€
-    \n3. Surprise   14€`
-  ); 
-  if (pizzaType === "1"){
-    foodOrder = `Margherita`;
-    foodPrice = 11;
-  } 
-  else if (pizzaType === "2"){
-    foodOrder = `Verdure`;
-    foodPrice = 12;
-  }
-  else if (pizzaType === "3"){
-    foodOrder = `Surprise`;
-    foodPrice = 14;
-  }
-  alert(`You have ordered the Pizza ${foodOrder}! That'll be ${foodPrice} €, please.`)
-} 
-
-else if (foodTypeConfirm === `Pasta`){
+    1. Margherita 11€
+    2. Verdure    12€
+    3. Surprise   14€`
+    );
+} else if (foodTypeConfirm == `Pasta`){
   pastaType = prompt(
     `Great! We have these to choose from:
     1. Arrabbiata   12€
     2. Casio e Pepe 10€
     3. Alfredo      9€`
-  ); 
-  if (pastaType === "1"){
+    );
+} else if (foodTypeConfirm == `Salad`){
+  saladType = prompt(
+    `Great! We have these to choose from:
+    1. Panzanella   6€
+    2. Caprese      7€
+    3. Giardiniera  8€`
+    );
+} else {
+  alert(`Sorry, we did not catch that. Type a number.`)
+  process.exit(1);
+}
+// Value for input
+  if (foodTypeConfirm == "Pizza"){
+    if (pizzaType === "1"){
+    foodOrder = `Margherita`;
+    foodPrice = 11;
+  } else if (pizzaType === "2"){
+    foodOrder = `Verdure`;
+    foodPrice = 12;
+  } else if (pizzaType === "3"){
+    foodOrder = `Surprise`;
+    foodPrice = 14;
+  } else {alert("Invalid choice. Refresh and try again.");
+  process.exit(1);
+  }
+}
+  
+  else if (foodTypeConfirm === "Pasta"){
+  if (pastaType === 1){
     foodOrder = `Arrabbiata`;
     foodPrice = 12;
   } 
-  else if (pastaType === "2"){
+  else if (pastaType === 2){
     foodOrder = `Casio e Pepe`;
     foodPrice = 10;
   }
-  else if (pastaType === "3"){
+  else if (pastaType === 3){
     foodOrder = `Alfredo`;
     foodPrice = 9;
   }
-  alert(`You have ordered the Pasta, ${foodOrder}! That'll be ${foodPrice} €, please!`)
-} 
-else if (foodTypeConfirm === `Salad`){
-saladType = prompt(
-  `Great! We have these to choose from:
-  \n1. Panzanella   6€
-  \n2. Caprese      7€
-  \n3. Giardiniera  8€`
-); if (saladType === "1"){
+  else {alert("Invalid choice. Refresh and try again.");
+  process.exit(1);
+  }
+}
+  
+else if (foodTypeConfirm === "Salad"){
+if (saladType === 1){
   foodOrder = `Panzanella`;
   foodPrice = 6;
-} if (saladType === "2"){
+} 
+else if (saladType === 2){
   foodOrder = `Caprese`;
   foodPrice = 7;
-}if (saladType === "3"){
+}
+else if (saladType === 3){
   foodOrder = `Giardiniera`;
   foodPrice = 8;
 }
-alert(`You have ordered the ${foodOrder} Salad! Thatll be ${foodPrice} €, thank you!`)
-} else {(alert(`Please type a number, 1, 2 or 3!`))}
+else {alert("Invalid choice. Refresh and try again.");
+  process.exit(1);
+  }
+}
+else {alert("Invalid choice. Refresh and try again.");
+process.exit(1);
+}
+
+alert(`You have ordered the ${foodOrder}! Thatll be ${foodPrice} €, thank you!`)
 
 // Step 4 - Age
 let babyPrice = (foodPrice/2)
@@ -114,12 +136,15 @@ if (age <= 16 ) {
 else { alert(`All in order! A big portion for ${foodPrice} €.`)}
 
 // Step 5 - Order confirmation
-if (age <= 16) {
-  confirm(`Best ${person}! You have ordered the small ${foodTypeConfirm} ${foodOrder} for only ${babyPrice}. Is this correct?`)
-}
-if (age > 16) {
-  confirm(`Best ${person}! You have ordered a ${foodTypeConfirm} ${foodOrder} for ${foodPrice}. Is this correct?`)
-}
-if (confirm == true){alert(`Fantastic! Your meal is being prepared!`)}
-else {alert(`So sorry we got I wrong, please try again!`)}
+let orderConfirmation;
 
+if (age <= 16) {
+  orderConfirmation = prompt(`Best ${person}! You have ordered the small ${foodTypeConfirm} ${foodOrder} for only ${babyPrice}. Is this correct? Enter 1.`)
+} else if (age > 16) {
+  orderConfirmation = prompt(`Best ${person}! You have ordered a ${foodTypeConfirm} ${foodOrder} for ${foodPrice}. Is this correct? Enter 1`)
+} else {alert(`So sorry we got I wrong, please try again!`)
+}
+
+
+if (orderConfirmation == "1"){alert(`Fantastic! Your meal is being prepared!`)}
+else {alert(`So sorry we got I wrong, please try again!`)}
