3d3f9c021be9f8a45369f452b5df1f474a550efd
nnmuunn <nnmuunn@gmail.com>
2023-09-24 23:27:32 +0200

    first commit

--- code/script.js
+++ code/script.js
@@ -4,16 +4,70 @@
 // Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+
+const userName = prompt("Let me know your Name!");
+
+alert(
+  `Welcome ${userName}:) Lets get started with the order!`
+);
 
 // Step 2 - Food choice
-// Your code goes here
+const foodOption = prompt(`Using a number, what would you like to eat today?
+1 - Buuz
+2 - Tsuiwan
+3 - Guraltai Shul`);
 
 // Step 3 - Subtype choice
-// Your code goes here
+let selectedFood = "";
+
+if (foodOption === "1") {
+    selectedFood = "Buuz";
+  } else if (foodOption === "2") {
+    selectedFood = "Tsuiwan";
+  } else if (foodOption === "3") {
+    selectedFood = "Guraltai Shul";
+  } else {
+    alert("Please choose a number between 1-3 :)");
+    process.exit(1);
+  }
+alert(`Great! One ${selectedFood} is getting ready for you soon.`);
+
+// Step 3 - Subtype choice
+const sideDish = prompt(`Using a number, what side dish would you like to have?
+1 - Salat
+2 - Cucumber
+3 - Spicy tomato
+4 - none`);
+let selectedSideDish = "";
+
+if (sideDish === "1") {
+  selectedSideDish = "Salat";
+  } else if (sideDish === "2") {
+    selectedSideDish = "Cucumber";
+  } else if (sideDish === "3") {
+    selectedSideDish = "Spicy tomato";
+  } else if (sideDish === "4") {
+    selectedSideDish = "no side dish";
+  } else {
+    alert("Please choose a number between 1-4 :)");
+    process.exit(1);
+  }
+alert(`Great! One ${selectedSideDish} is getting ready for you soon as well.`);
 
 // Step 4 - Age
-// Your code goes here
+const adultChid = prompt(`Is this meal for a child or adult?`);
+if (adultChid === 'child') {
+  alert(`That will be 13 euros.`);
+  } else {
+    alert(`That will be 16 euros.`);
+  }
 
 // Step 5 - Order confirmation
-// Your code goes here
+const confirmation = confirm(`Delicious! Your todays meal is ${selectedFood} with ${selectedSideDish}.`);
+if(confirmation) {
+  alert(`Your order is getting prepared by now. We hope that your ${selectedFood} with ${selectedSideDish} is the best one you ever had. Enjoy! :))`);
+} else {
+  alert(`Please come again!`);
+  process.exit(1);
+}
