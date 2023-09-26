2a2709bb58461013eda0ddf5ee2f1517c9ed444f
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-09-26 21:20:18 +0200

    Step 2 completed

--- code/script.js
+++ code/script.js
@@ -10,6 +10,24 @@ let Name = prompt("Enter your name: ");
 alert("hi " + Name + "!");
 // Step 2 - Food choice
 // Your code goes here
+let choice = prompt(
+  "Please choose your food:\n Enter a number (Pizza = 1, Pasta = 2, Salad = 3):"
+);
+let food;
+switch (choice) {
+  case "1":
+    food = "Pizza";
+    break;
+  case "2":
+    food = "Pasta";
+    break;
+  case "3":
+    food = "Salad";
+    break;
+  default:
+    food = "Invalid choice";
+}
+alert("You selected: " + food);
 
 // Step 3 - Subtype choice
 // Your code goes here
