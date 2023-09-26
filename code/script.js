c843ddf47b027b9bdcfdc67148c448d8f6e75c9a
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-09-26 20:29:34 +0200

    Step 1 completed

--- code/script.js
+++ code/script.js
@@ -4,8 +4,10 @@
 // Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
-
+);
+//alert("Please enter your name here:");
+let Name = prompt("Enter your name: ");
+alert("hi " + Name + "!");
 // Step 2 - Food choice
 // Your code goes here
 
