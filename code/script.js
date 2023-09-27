8cd7aba0f93e254ba2462043296cc993768ce1ec
Naima Malik <noma_m@hotmail.com>
2023-09-27 20:43:17 +0200

    Step 1

--- code/script.js
+++ code/script.js
@@ -1,10 +1,12 @@
 // Start here
 
+const pizzeriaName = "Naima's Pizzeria";
+
 // Step 1 - Welcome and introduction
 // Your code goes here
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+alert(`Welcome to ${pizzeriaName}. Ready to Start? - Click 'OK' to begin.`);
+const customerName = prompt("Please enter your name.");
+alert(`Nice to meet you, ${customerName}!`);
 
 // Step 2 - Food choice
 // Your code goes here
