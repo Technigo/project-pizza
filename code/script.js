2e6eda2089509eb42b59870d0fcccee26810005a
Viyan.UX Engineer <74237057+starblazingunicorn@users.noreply.github.com>
2023-09-27 21:08:17 +0200

    add prompt method

--- code/script.js
+++ code/script.js
@@ -1,13 +1,16 @@
 // Start here
+alert(
+  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+);
 
 // Step 1 - Welcome and introduction
 // Your code goes here
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+let person = prompt("Please enter your name:");
+alert(`Nice to meet you, ` + person + `!`);
 
 // Step 2 - Food choice
-// Your code goes here
+let food = prompt(`What type of food would you like to order?\n\n Enter a number:\n1. Pizza\n2. Pasta\n3. Salad`);
+alert(`Invalid choice. Please select a number between 1 and 3`);
 
 // Step 3 - Subtype choice
 // Your code goes here
