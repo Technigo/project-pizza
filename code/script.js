f3ad5fda402a7075630a936bccdc15d6ccf11a39
Calleobe <145662831+Calleobe@users.noreply.github.com>
2023-09-27 14:20:07 +0200

    Step 3 completed

--- code/script.js
+++ code/script.js
@@ -31,6 +31,46 @@ alert("You selected: " + food);
 
 // Step 3 - Subtype choice
 // Your code goes here
+let subtype;
+let subtypeText;
+if (choice == "1") {
+  subtype = prompt("Choose Pizza:\nCalzone = 1, Capriciosa = 2, Hawaii = 3");
+  if (subtype == "1") {
+    subtypeText = "Calzone";
+  } else if (subtype == "2") {
+    subtypeText = "Capriciosa";
+  } else if (subtype == "3") {
+    subtypeText = "Hawaii";
+  } else {
+    exit;
+  }
+} else if (choice == "2") {
+  subtype = prompt("Choose Pasta:\nSpaghetti = 1, Lasagne = 2, Macaroni = 3");
+  if (subtype == "2") {
+    subtypeText = "Spaghetti";
+  } else if (subtype == "2") {
+    subtypeText = "Lasagne";
+  } else if (subtype == "3") {
+    subtypeText = "Macaroni";
+  } else {
+    exit;
+  }
+} else if (choice == "3") {
+  subtype = prompt("Choose Sallad:\nChicken = 1, Greek = 2, Cesar = 3");
+  if (subtype == "2") {
+    subtypeText = "Chicken";
+  } else if (subtype == "2") {
+    subtypeText = "Greek";
+  } else if (subtype == "3") {
+    subtypeText = "Cesar";
+  } else {
+    exit;
+  }
+} else {
+  alert("Invalid choice");
+}
+
+alert("You selected: " + subtypeText);
 
 // Step 4 - Age
 // Your code goes here
