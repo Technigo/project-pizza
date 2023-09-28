ec9e727acd5ba36f3527d89fa4cd7ce6d67cd2d7
SETI108 <seti108@gmail.com>
2023-09-28 16:01:39 +0200

    On step 4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project Name
 
-Replace this readme with your own information about your project. Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+First week's challenge in Technigo React/Redux Upskill course.
 
 ## The problem
 
--- code/script.js
+++ code/script.js
@@ -6,12 +6,80 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+// Write a prompt to ask for the name, and store the answer in a variable.
+const firstName = prompt('Please enter your first name!');
+
+//Create a new alert using the name variable, to say something like “Hi Diego!”
+
+alert(`Hi ${firstName}!`);
+
 // Step 2 - Food choice
-// Your code goes here
+
+// Nested arrays will reduce the need for many variables for food and prices
+const foodChoices =[['Pizza', 10], ['Pasta', 8], ['Salad', 20]];
+
+// Use promt to show the user the menu
+
+const foodChoiceOfUser = prompt(`
+  Please choose what food you want to order by writing the number next to the dish:
+
+  - 1 Pizza 10 €
+  - 2 Pasta 8 $
+  - 3 Salad 20 $
+`);
+
+
+//Alert the user with the chosen food item
+
+alert(`You've ordered ${foodChoices[foodChoiceOfUser - 1][0]} for a cost of ${foodChoices[foodChoiceOfUser - 1][1]} € `)
 
 // Step 3 - Subtype choice
-// Your code goes here
+// Adding the array for the subtypes
+const pizzaToppings = ['Pineapple', 'Banana', 'Ketchup'];
+const pastaToppings = ['Olives', 'Feta cheese', 'Sun-dried tomatoes'];
+const saladToppings = ['Grapes', 'Avocado', 'Red onion'];
+let toppingChoice;
 
+console.log(foodChoiceOfUser);
+
+switch (foodChoiceOfUser){
+  case '1':
+    toppingChoice = prompt(`
+    Choose your pizza toppings by writing the number corresponding to the topping you like:
+      - 1 Pineapple
+      - 2 Banana
+      - 3 Ketchup`);
+      alert(`You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${pizzaToppings[toppingChoice -1]}. `)
+  break;
+  
+  case '2':
+    toppingChoice = prompt(`
+    Choose your pasta toppings by writing the number corresponding to the topping you like:
+      - 1 Olives
+      - 2 Feta cheese
+      - 3 Sun-dried tomatoes
+    `);
+    alert(`You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${pastaToppings[toppingChoice -1]}. `)
+  break;
+  
+  case '3':
+    toppingChoice = prompt(`
+    Choose your salad toppings by writing the number corresponding to the topping you like:
+     - 1 Grapes
+     - 2 Avocado
+     - 3 Red onion
+      `);
+      alert(`You've chosen ${foodChoices[foodChoiceOfUser - 1][0]} with ${saladToppings[toppingChoice -1]}. `)
+  break;
+
+  default:
+    alert(`That's not one of the choices!`)
+    exit(1);
+}
+
+
+
+// const subFoodChoices = prompt(`Choose your toppings by writing the number corresponding to the topping you like:`)
 // Step 4 - Age
 // Your code goes here
 
