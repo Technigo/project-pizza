779c6b5edc1052e82bd7bc189ae44137803fd214
psterner <psterner@hotmail.com>
2023-09-28 15:35:17 +0200

    Cleaner code. Save foodType in variable. Print alert only once

--- code/script.js
+++ code/script.js
@@ -10,7 +10,7 @@ alert(`Nice to meet you, ${prompt(`Please enter your name:`)}!`);
 
 // Step 2 - Food choice
 // Your code goes here
-const foodType = prompt(`
+let foodType = prompt(`
   What type of food would you like to order?
   Enter a number:
   1: Pizza 🍕
@@ -19,17 +19,30 @@ const foodType = prompt(`
  `);
 
 if (foodType == 1) {
-  alert(`You´ve chosen Pizza!`);
+  foodType = "Pizza";
 } else if (foodType == 2) {
-  alert(`You´ve chosen Pasta!`);
+  foodType = "Pasta";
 } else if (foodType == 3) {
-  alert(`You´ve chosen Salad!`);
+  foodType = "Salad";
 } else {
   alert(`Invalid choice. Please select a number between 1 and 3.`);
+  exit(1);
 }
+
+alert(`You´ve chosen ${foodType}!`);
 
 // Step 3 - Subtype choice
 // Your code goes here
+// For this iteration, the focus is on enabling the user to choose a
+// subtype of the selected food type. Depending on the previously
+// chosen food type (Pizza, Pasta, Salad), the program should present
+// the user with subtype options specific to that food type.
+// The prompt() method should be used to get the user's input.
+// The program should then determine the chosen subtype and provide
+// feedback using the alert() method to confirm the selection.
+// Tip: Use conditional statements for this.
+// const foodChoice;
+// const subType = [];
 
 // Step 4 - Age
 // Your code goes here
