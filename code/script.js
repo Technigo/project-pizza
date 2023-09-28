467bb09cf0663cf9f61b428cd06c8b3aa9796ebe
Disabledbarrel <elin.elisabeth.larsson@gmail.com>
2023-09-28 21:38:03 +0200

    feat(elin): week1 step 2

--- code/script.js
+++ code/script.js
@@ -1,18 +1,30 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
 	`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 );
 
-// Step 2 - Food choice
 const userName = prompt("Please enter your name beloved customer");
 
 alert(`Welcome to our mighty fine pizzeria ${userName}`);
 
+// Step 2 - Food choice
+const menu = {
+	1: "pizza",
+	2: "pasta",
+	3: "salad",
+};
+
+const order = prompt(
+	"Please provide your preferred meal. Enter 1 for pizza, 2 for pasta or 3 for salad."
+);
+
+const chosenMeal = menu[order];
+
+alert(`You have ordered ${chosenMeal ? chosenMeal : "nothing"}`);
+
 // Step 3 - Subtype choice
-// Your code goes here
 
 // Step 4 - Age
 // Your code goes here
