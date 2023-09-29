52461a1ae70ae0cffe2ba59e6378394ec49d8b30
Kris Albert Lee <krisalbert@gmail.com>
2023-09-29 15:50:26 +0200

    after subtype choice

--- code/script.js
+++ code/script.js
@@ -6,11 +6,50 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+const firstName = prompt("Please enter your name:");
+console.log(firstName);
+
+alert(`Nice to meet you, ${firstName}.`);
+
 // Step 2 - Food choice
 // Your code goes here
+const foodChoice = prompt(
+  `What type of food would you like to order?
+
+    Enter a number:
+    ${foodChoiceText}
+  `
+)
+
+if (foodChoices[foodChoice] == undefined) {
+  alert(`Invalid answer. please select a number between 1 and ${Object.entries(foodChoices).length}`);
+} else {
+  alert(`You've chosen ${foodChoices[foodChoice]} `)
+}
+
+
+let subtypeChoiceText;
+switch(foodChoices[foodChoice]) {
+  case foodChoices["1"]:
+    subtypeChoiceText = pizzaSubchoiceText;
+    break;
+  case foodChoices["2"]:
+    subtypeChoiceText = pastaSubchoiceText;
+    break;
+  case foodChoices["3"]:
+    subtypeChoiceText = saladSubchoiceText;
+    break;
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
+const subtypeChoice = prompt(
+  `Select a ${foodChoices[foodChoice]} type?
+
+    Enter a number:
+    ${subtypeChoiceText}
+  `
+)
 
 // Step 4 - Age
 // Your code goes here
---
+++ code/variables.js
@@ -0,0 +1,88 @@
+const foodChoices = {
+  1: "Pizza",
+  2: "Pasta",
+  3: "Salad"
+};
+
+// Dynamically generate the food choices we have
+let foodChoiceText = "\n";
+Object.entries(foodChoices).forEach(choice => {
+  foodChoiceText += `${choice[0]} - ${choice[1]} \n`;
+});
+
+const pizzaSubchoice = [{
+    key: "1",
+    name: "Margherita",
+    adultPrice: 12,
+    kidsPrice: 6
+  }, {
+    key: "2",
+    name: "Napolitana",
+    adultPrice: 14,
+    kidsPrice: 7
+  }, {
+    key: "3",
+    name: "Pepperoni",
+    adultPrice: 18,
+    kidsPrice: 9
+  },
+];
+
+// Dynamically generate the food choices we have
+let pizzaSubchoiceText = "\n";
+pizzaSubchoice.forEach(choice => {
+  pizzaSubchoiceText += `${choice.key} - ${choice.name} \n`;
+});
+
+const pastaSubchoice = [{
+  key: "1",
+  name: "Spaghetti Carbonara",
+  adultPrice: 13,
+  kidsPrice: 6.50
+}, {
+  key: "2",
+  name: "Fettucini Alfredo",
+  adultPrice: 15,
+  kidsPrice: 7.50
+}, {
+  key: "3",
+  name: "Cheesy Tortellini",
+  adultPrice: 16,
+  kidsPrice: 8
+},
+];
+
+// Dynamically generate the food choices we have
+let pastaSubchoiceText = "\n";
+pastaSubchoice.forEach(choice => {
+pastaSubchoiceText += `${choice.key} - ${choice.name} \n`;
+});
+
+const saladSubchoice = [{
+    key: "1",
+    name: "Ceasar",
+    adultPrice: 10,
+    kidsPrice: 5
+  }, {
+    key: "2",
+    name: "Niçoise",
+    adultPrice: 11,
+    kidsPrice: 5.50,
+  }, {
+    key: "3",
+    name: "Waldorf",
+    adultPrice: 14,
+    kidsPrice: 7
+  }, {
+    key: "4",
+    name: "Caprese",
+    adultPrice: 10.50,
+    kidsPrice: 5.25
+  },
+];
+
+// Dynamically generate the food choices we have
+let saladSubchoiceText = "\n";
+saladSubchoice.forEach(choice => {
+saladSubchoiceText += `${choice.key} - ${choice.name} \n`;
+});
