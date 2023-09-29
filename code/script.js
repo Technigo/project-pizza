75611353734f1df77857e2ec42c3ccb7fd397619
Liliane <80630596+LilianeCharlie@users.noreply.github.com>
2023-09-29 16:38:25 +0200

    Added prompt and alert

--- code/script.js
+++ code/script.js
@@ -2,9 +2,16 @@
 
 // Step 1 - Welcome and introduction
 // Your code goes here
+
+//Welcome-message for the customer to Javascript Pizzeria
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+//Asking/prompt the customer to put in their name
+const customerName = prompt('Please enter your name for your order:');
+
+//Alert to say hello to customer
+alert(`Hi there ${customerName}! What would you like to order?`);
 
 // Step 2 - Food choice
 // Your code goes here
