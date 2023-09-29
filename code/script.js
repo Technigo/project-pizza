f695c66b47a288d37248b36b49823682aadc83f8
carolina <carolina.beatriz.cardenas@gmail.com>
2023-09-29 14:14:07 +0200

    Yo hice los 2 primeros pasos

--- code/script.js
+++ code/script.js
@@ -5,10 +5,32 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
+let nombre = prompt("what your is name:");
 
+alert(`Hello ${nombre}`);
 // Step 2 - Food choice
 // Your code goes here
+let food = prompt(`what do you want?
+1 - Pizza
+2 - Pasta
+3 - Ensalada`);
+let nameFood = "";
 
+if(food ===1) 
+{
+  nameFood = "Pizza"
+}
+else if (food ===2)
+{
+  nameFood = "Pasta"
+}
+else if ( food === 3)
+{ 
+  nameFood = "Ensalada"
+}
+
+
+alert(`You chose ${nameFood}`)
 // Step 3 - Subtype choice
 // Your code goes here
 
