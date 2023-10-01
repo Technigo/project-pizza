27e3cd6987f7d5c1fa8605162fd79fa03a9659cd
carolina <carolina.beatriz.cardenas@gmail.com>
2023-10-01 13:08:22 +0200

    step5 refactor

--- code/script.js
+++ code/script.js
@@ -38,27 +38,79 @@ else
 alert(`You chose ${nameFood}`)
 // Step 3 - Subtype choice
 // Your code goes here
- let subTypeFood=""
-if(nameFood=== "Pizza")
+ let subType = ""
+ let subTypeFood = ""
+if(nameFood === "Pizza")
 {
-  subTypeFood = prompt(`What do you want?
-  1 - Hawaiana
+  subType = prompt(`Select a pizza type:
+  1 - Hawaian
   2 - Pepperoni
   3 - Capriccosa `);
+ 
+  if(subType === "1")
+  {
+    subTypeFood = "Hawaian"
+  }
+  else if(subType === "2")
+  {
+    subTypeFood = "Pepperoni"
+  } else if(subType === "3")
+  {
+    subTypeFood = "Capricciosa"
+  }
+  else
+  {
+    alert(`Invalid choice. Please select a number between 1 and 3.`)
+    exit()
+  }
 //Haga esto
-}else if(nameFood=== "Pasta")
+}else if(nameFood === "Pasta")
 {
-  subTypeFood = prompt(`What do you want?
+  subType = prompt(`Select a pasta type:
   1 - Lasagne
   2 - Bolonesa
   3 - Ravioli `);
+
+  if(subType === "1")
+  {
+    subTypeFood = "Lasagne"
+  }
+  else if(subType === "2")
+  {
+    subTypeFood = "Bolonesa"
+  } else if(subType === "3")
+  {
+    subTypeFood = "Ravioli"
+  }
+  else
+  {
+    alert(`Invalid choice. Please select a number between 1 and 3.`)
+    exit()
+  }
 }
 else 
 {
-  subTypeFood = prompt(`What do you want?
+  subType = prompt(`Select a salad type:
   1 - Ceasar
   2 - Thai
   3 - Greek `);
+
+  if(subType === "1")
+  {
+    subTypeFood = "Cesar"
+  }
+  else if(subType === "2")
+  {
+    subTypeFood = "Thai"
+  } else if(subType === "3")
+  {
+    subTypeFood = "Greek"
+  }
+  else
+  {
+    alert(`Invalid choice. Please select a number between 1 and 3.`)
+    exit()
+  }
 };
 
 // Step 4 - Age
@@ -76,7 +128,7 @@ else
   adultOrChild ="adult";
   cost = "€15";
 }
-alert(`One ${adultOrChild} sized ${subTypeFood} ${nameFood} will be prepared for you. That will be ${cost}`);
+alert(`One ${adultOrChild} sized ${subTypeFood} ${nameFood.toLowerCase()} will be prepared for you. That will be ${cost}`);
 
 // Step 5 - Order confirmation
 // Your code goes here
@@ -85,7 +137,7 @@ Enter a number to confirm:
   1 - Yes
   2 - No` );
 
-if(confirmed === "1" )
+if( confirmed === "1" )
 {
   alert(`Thank you for your order! Your delicious meal will be prepared. See you soon! `)
 }
