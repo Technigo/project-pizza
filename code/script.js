34f4ce872bd2399cbddefbc00330bfebda8456c3
Umamaheswari Thiyagarajan <umagithub23@users.noreply.github.com>
2023-10-01 16:57:18 +0200

    completed Step 2 - Food choice

--- code/script.js
+++ code/script.js
@@ -11,7 +11,34 @@ const userName = prompt(userQuestion)
 alert(`Hi ${userName}!`)
 
 // Step 2 - Food choice
-// Your code goes here
+userQuestion = "Please select the menu of your choice.\n Enter a number: \n 1 - Pizza \n 2 - Pasta \n 3 - Salad"
+const userChoice = prompt(userQuestion)
+
+let choiceName
+
+switch (userChoice) {
+  case "1":
+    choiceName = "Pizza"
+    break
+  case "2":
+    choiceName = "Pasta"
+    break
+  case "3":
+    choiceName = "Salad"
+    break
+  default:
+    choiceName = "Invalid"
+    break
+}
+
+if (choiceName === "Invalid")
+{
+  alert(`Invalid choice! Please choose between 1 - 3!`)
+}
+else
+{
+  alert(`You've chosen ${choiceName}!`)
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
