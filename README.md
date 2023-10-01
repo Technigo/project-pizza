006c0802a50ced142e541df7971fb11f877121c4
Your Name <you@example.com>
2023-10-01 11:54:15 +0200

    Final draft

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
 # Project Name
-
-Replace this readme with your own information about your project. Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Building a simple Pizzeria using Javascript where the user interacts with prompts storing the inputs as variables. The user is to navigate through a simple order flow and recieve an order confirmation with details of the selected items.
 
 ## The problem
-
-
-Describe how you approached to problem, and what tools and techniques you used to solve it. How did you plan? What technologies did you use? If you had more time, what would be next?
+I started out working through the stages one by one ensuring that each step executed as it should. When one section would cause an error i re-wrote the code from sctarch for some extra practice of the syntax and logic.
+The first version of the code contains "if else" statements as well as nested if statements with strict comparisons to make the code execute according to the brief. Once I had a working solution I started a new script file replacing as many "if else" statements with switch cases and arrays. If I had more time I would have liked to create proper text input fields and maybe add buttons to give more of a "submission form" type of feel.
 
 ## View it live
-
 Have you deployed your project somewhere? Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about
--- code/script.js
+++ code/script.js
@@ -6,14 +6,162 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+//Asking for name
+const orderName = prompt("Please provide a name:");
+alert(`Hello ${orderName}, can I take your order please.`);
+
 // Step 2 - Food choice
 // Your code goes here
+//Food variables
+let foodSelection
+let mealType
+
+
+foodSelection = prompt(`
+  What would you like to order?
+  Please provide a number:
+  1 - Pizza
+  2 - Pasta
+  3 - Salad
+`);
+
+// Updating variable names based on numers selected
+if (foodSelection === "1") {
+  foodSelection = "Pizza";
+
+} else if (foodSelection === "2") {
+  foodSelection = "Pasta";
+
+} else if (foodSelection === "3") {
+  foodSelection = "Salad";
+
+} else {
+  alert("This item is not on the meny please try again");
+  exit(1)
+}
+
+//Chosen main meal
+alert(`Great, you have chosen ${foodSelection}!`);
+
+
 
 // Step 3 - Subtype choice
 // Your code goes here
 
+// Pizza options
+if (foodSelection === "Pizza") {
+  mealType = prompt(`
+  What type of pizza would you like to order?
+  Please provide a number:
+  1 - Margerita
+  2 - Hawaian
+  3 - Pepperoni
+  `);
+
+  // Updating variable names based on numers selected
+  if (mealType === "1") {
+    mealType = "Margerita";
+
+  } else if (mealType === "2") {
+    mealType = "Hawaian";
+
+  } else if (mealType === "3") {
+    mealType = "Pepperoni";
+
+  } else {
+    alert("That option is currently unavailable");
+    exit(1)
+  }
+
+  // Pasta options
+} else if (foodSelection === "Pasta") {
+  mealType = prompt(`
+  What type of pasta would you like to order?
+  Please provide a number:
+  1 - Carbonara
+  2 - Tortellini
+  3 - Fettucini
+  `);
+
+  // Updating variable names based on numers selected
+  if (mealType === "1") {
+    mealType = "Carbonara";
+
+  } else if (mealType === "2") {
+    mealType = "Tortellini";
+
+  } else if (mealType === "3") {
+    mealType = "Fettucini";
+
+  } else {
+    alert("That option is currently unavailable");
+    exit(1)
+  }
+
+  // Salad options
+} else if (foodSelection === "Salad") {
+  mealType = prompt(`
+  What type of salad would you like to order?
+  Please provide a number:
+  1 - Greek salad
+  2 - Ceasar salad
+  3 - Tuna salad
+  `);
+
+  // Updating variable names based on numers selected
+  if (mealType === "1") {
+    mealType = "Greek salad";
+
+  } else if (mealType === "2") {
+    mealType = "Ceasar salad";
+
+  } else if (mealType === "3") {
+    mealType = "Tuna salad";
+
+  } else {
+    alert("That option is currently unavailable");
+    exit(1)
+  }
+
+} else {
+  alert("The meal you have seceted is not on the menu.");
+  exit(1)
+}
+
+//Complete meal order
+alert(`Great choice! Our ${mealType} ${foodSelection} is amazing!`);
+
+
 // Step 4 - Age
 // Your code goes here
+let sizeOfPerson
+let costOfMeal
+
+let ageOfPerson = prompt("How old are you?");
+if (ageOfPerson >= 18) {
+  sizeOfPerson = "adult";
+  costOfMeal = "€15";
+
+} else {
+  sizeOfPerson = "child";
+  costOfMeal = "€10";
+}
+
 
 // Step 5 - Order confirmation
 // Your code goes here
+const orderStatus = prompt(`
+ Order summary:
+ One ${sizeOfPerson} size ${foodSelection} comes to a total cost of ${costOfMeal}.
+ Do you wish to place the order?
+ 1 - Yes
+ 2 - No`);
+
+
+if (orderStatus === "1") {
+  alert("Thank you! We have recieved your order. Your order number is 4372");
+
+} else {
+  alert("Take your time. We look forward to hearing from you again.");
+  exit(1)
+}
