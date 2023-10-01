c4e8d8781fa12c9ff5a02d5862ff782139cbe639
carolina <carolina.beatriz.cardenas@gmail.com>
2023-10-01 12:43:31 +0200

    step5

--- code/script.js
+++ code/script.js
@@ -5,15 +5,16 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
-let nombre = prompt("what your is name:");
+const nameClient = prompt("What your is name:");
 
-alert(`Hello ${nombre}`);
+alert(`Hello ${nameClient}`);
 // Step 2 - Food choice
 // Your code goes here
-let food = prompt(`what do you want?
+const food = prompt(`What do you want?
 1 - Pizza
 2 - Pasta
-3 - Ensalada`);
+3 - Salad`);
+
 let nameFood = "";
 
 if(food === "1") 
@@ -24,38 +25,41 @@ else if (food === "2")
 {
   nameFood = "Pasta"
 }
-else 
+else if(food === "3") 
 { 
-  nameFood = "Ensalada"
+  nameFood = "Salad"
 }
-
+else{
+  alert(`Invalid choice. Please select a number between 1 and 3.`)
+exit()
+}
 
 alert(`You chose ${nameFood}`)
 // Step 3 - Subtype choice
 // Your code goes here
  let subTypeFood=""
 if(nameFood=== "Pizza"){
-  subTypeFood = prompt(`what do you want?
+  subTypeFood = prompt(`What do you want?
 1 - Hawaiana
 2 - Pepperoni
-3 - Caprichosa `);
+3 - Capriccosa `);
 //Haga esto
 }else if(nameFood=== "Pasta"){
-  subTypeFood = prompt(`what do you want?
-  1 - Lasana
+  subTypeFood = prompt(`What do you want?
+  1 - Lasagne
   2 - Bolonesa
   3 - Ravioli `);
 }
 else {
-  subTypeFood = prompt(`what do you want?
+  subTypeFood = prompt(`What do you want?
   1 - Ceasar
   2 - Thai
-  3 - Griego `);
+  3 - Greek `);
 };
 
 // Step 4 - Age
 // Your code goes here
-let age = prompt("Is this food for a child or an adult? Type your age:");
+const age = prompt("Is this food for a child or an adult? Type your age:");
 let adultOrChild="";
 let cost = "";
 if(parseInt(age)<18){
@@ -67,9 +71,21 @@ adultOrChild ="adult";
 cost = "€15";
 }
 alert(`One ${adultOrChild} sized ${subTypeFood} ${nameFood} will be prepared for you. That will be ${cost}`);
-prompt(`Are you sure you want to order this?
+
+// Step 5 - Order confirmation
+// Your code goes here
+const confirmed = prompt(`Are you sure you want to order this?
 Enter a number to confirm:
 1 - Yes
 2 - No` );
-// Step 5 - Order confirmation
-// Your code goes here
+
+if(confirmed === "1" ){
+alert(`Thank you for your order! Your delicious meal will be prepared. See you soon! `)
+}
+else if( confirmed === "2" ){
+alert(`No problem, come back and order anytime you want.`)
+}
+else{
+  alert(`Invalid choice. Please select a number between 1 and 3.`)
+  exit()
+}
