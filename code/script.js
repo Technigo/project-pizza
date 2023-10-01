e1ace5f4c6b7eae54e84b18769a366a00375826c
carolina <carolina.beatriz.cardenas@gmail.com>
2023-10-01 12:47:29 +0200

    step5

--- code/script.js
+++ code/script.js
@@ -11,9 +11,9 @@ alert(`Hello ${nameClient}`);
 // Step 2 - Food choice
 // Your code goes here
 const food = prompt(`What do you want?
-1 - Pizza
-2 - Pasta
-3 - Salad`);
+  1 - Pizza
+  2 - Pasta
+  3 - Salad`);
 
 let nameFood = "";
 
@@ -29,28 +29,32 @@ else if(food === "3")
 { 
   nameFood = "Salad"
 }
-else{
+else
+{
   alert(`Invalid choice. Please select a number between 1 and 3.`)
-exit()
+  exit()
 }
 
 alert(`You chose ${nameFood}`)
 // Step 3 - Subtype choice
 // Your code goes here
  let subTypeFood=""
-if(nameFood=== "Pizza"){
+if(nameFood=== "Pizza")
+{
   subTypeFood = prompt(`What do you want?
-1 - Hawaiana
-2 - Pepperoni
-3 - Capriccosa `);
+  1 - Hawaiana
+  2 - Pepperoni
+  3 - Capriccosa `);
 //Haga esto
-}else if(nameFood=== "Pasta"){
+}else if(nameFood=== "Pasta")
+{
   subTypeFood = prompt(`What do you want?
   1 - Lasagne
   2 - Bolonesa
   3 - Ravioli `);
 }
-else {
+else 
+{
   subTypeFood = prompt(`What do you want?
   1 - Ceasar
   2 - Thai
@@ -62,13 +66,15 @@ else {
 const age = prompt("Is this food for a child or an adult? Type your age:");
 let adultOrChild="";
 let cost = "";
-if(parseInt(age)<18){
-adultOrChild = "child";
-cost = "€10";
+if(parseInt(age)<18)
+{
+  adultOrChild = "child";
+  cost = "€10";
 }
-else{
-adultOrChild ="adult";
-cost = "€15";
+else
+{
+  adultOrChild ="adult";
+  cost = "€15";
 }
 alert(`One ${adultOrChild} sized ${subTypeFood} ${nameFood} will be prepared for you. That will be ${cost}`);
 
@@ -76,16 +82,19 @@ alert(`One ${adultOrChild} sized ${subTypeFood} ${nameFood} will be prepared for
 // Your code goes here
 const confirmed = prompt(`Are you sure you want to order this?
 Enter a number to confirm:
-1 - Yes
-2 - No` );
+  1 - Yes
+  2 - No` );
 
-if(confirmed === "1" ){
-alert(`Thank you for your order! Your delicious meal will be prepared. See you soon! `)
+if(confirmed === "1" )
+{
+  alert(`Thank you for your order! Your delicious meal will be prepared. See you soon! `)
 }
-else if( confirmed === "2" ){
-alert(`No problem, come back and order anytime you want.`)
+else if( confirmed === "2" )
+{
+  alert(`No problem, come back and order anytime you want.`)
 }
-else{
+else
+{
   alert(`Invalid choice. Please select a number between 1 and 3.`)
   exit()
 }
