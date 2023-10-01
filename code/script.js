c980677f31abde054f96ea0ab6bb3c6a3b6bc9f1
Akshayasadanandan <akshayasadanandankv1@gmail.com>
2023-10-01 06:26:37 +0200

    Add Step 2 - Food choice

--- code/script.js
+++ code/script.js
@@ -5,11 +5,26 @@
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
-customer_name = prompt(`what is your name`)
+customer_name = prompt(`Please enter your name`)
 
-alert(`hi ${customer_name}!`)
+alert(`hi ${customer_name}!, nice to meet you`)
 // Step 2 - Food choice
 // Your code goes here
+food_choice = prompt(`What type of food would you like to order?
+  Enter a number:
+  1- Pizza
+  2- Pasta
+  3- Salad`)
+
+if (food_choice == 1) {
+  alert(`You have chosed Pizza`)
+} else if (food_choice == 2) {
+  alert(`You have chosen Pasta`)
+} else if (food_choice == 3) {
+  alert(`You have chosen Salad`)
+} else {
+  alert(`Invalid choice. Please select a number between 1 and 3.`)
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
