bb5050a99a9499a0595cff01b3699a1ca9d18a48
Emelie Segell <segell.emelie@gmail.com>
2023-10-06 17:28:43 +0200

    add username and user experience add username from user input and improve user experience

--- code/script.js
+++ code/script.js
@@ -2,42 +2,60 @@
 let selectedFoodName;
 let selectedSubtypeName;
 let selectedSubtype;
+let price;
 
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 );
 
+const userName = prompt("Please enter your name:");
+alert(`Nice to meet you, ${userName}!`);
+
 // Step 2 - Food choice
 const selectedFood = prompt(
-  "What would you like to eat? 1 - Pizza, 2 - Pasta, 3 - Salad"
+  "What would you like to eat? \n Please enter a number: \n 1 - Pizza \n 2 - Pasta \n 3 - Salad"
 );
 
+switch (selectedFood) {
+  case "1":
+    selectedFoodName = "pizza";
+    break;
+  case "2":
+    selectedFoodName = "pasta";
+    break;
+  case "3":
+    selectedFoodName = "salad";
+    break;
+  default:
+    alert("Invalid Choice - please choose a number between 1 and 3");
+    exit(1);
+}
+alert(`You have chosen ${selectedFoodName}!`);
+
 // Step 3 - Subtype choice
-if (selectedFood === "1") {
-  selectedFoodName = "pizza";
+if (selectedFoodName === "pizza") {
   selectedSubtype = prompt(
-    `Good choice with the ${selectedFoodName}! What would you like on your pizza? 1 - Broccoli, 2 - Pineaple, 3 - Shrimp`
+    `Good choice with the ${selectedFoodName}! \n What would you like on your pizza? \n Please enter a number: \n 1 - Broccoli \n 2 - Pineaple \n 3 - Shrimp`
   );
   switch (selectedSubtype) {
     case "1":
-      selectedSubtypeName = "Broccoli";
+      selectedSubtypeName = "broccoli";
       break;
     case "2":
-      selectedSubtypeName = "Pineaple";
+      selectedSubtypeName = "pineaple";
       break;
     case "3":
-      selectedSubtypeName = "Shrimp";
+      selectedSubtypeName = "shrimp";
       break;
     default:
-      alert("wrong!");
+      alert("Invalid Choice - please choose a number between 1 and 3");
       exit(1);
   }
 }
 
-if (selectedFood === "2") {
-  selectedFoodName = "Pasta";
+if (selectedFoodName === "Pasta") {
   selectedSubtype = prompt(
-    `Good choice with the ${selectedFoodName}! What would you like on your pasta? 1 - Meat, 2 - Vegetables, 3 - Cheese`
+    `Good choice with the ${selectedFoodName}! \n What would you like on your pasta? Please enter a number: \n 1 - Meat \n 2 - Vegetables \n 3 - Cheese`
   );
   switch (selectedSubtype) {
     case "1":
@@ -50,14 +68,13 @@ if (selectedFood === "2") {
       selectedSubtypeName = "cheese";
       break;
     default:
-      alert("wrong!");
+      alert("Invalid Choice - please choose a number between 1 and 3");
       exit(1);
   }
 }
-if (selectedFood === "3") {
-  selectedFoodName = "Salad";
+if (selectedFoodName === "salad") {
   selectedSubtype = prompt(
-    `Good choice with the ${selectedFoodName}! What would you like on your salad? 1 - Chicken, 2 - Feta Cheese, 3 - Bacon`
+    `Good choice with the ${selectedFoodName}! \n What would you like on your salad? Please enter a number: \n 1 - Chicken \n 2 - Feta Cheese \n 3 - Bacon`
   );
 
   switch (selectedSubtype) {
@@ -71,27 +88,30 @@ if (selectedFood === "3") {
       selectedSubtypeName = "bacon";
       break;
     default:
-      alert("wrong!");
+      alert("Invalid Choice - please choose a number between 1 and 3");
       exit(1);
   }
 }
 
 if (selectedFood !== "1" && selectedFood !== "2" && selectedFood !== "3") {
-  alert("Try again!");
+  alert("Something went wrong with the order, please reaload to try again");
   exit(1);
 }
+alert(`You have chosen ${selectedFoodName} with ${selectedSubtypeName}!`);
 
 // Step 4 - Age
 const age = prompt("Please write the age of the person the order is for: ");
 
-age >= 13 ? (ageCategory = "adult") : (ageCategory = "kids");
+age >= 13
+  ? (ageCategory = "adult") && (price = 200)
+  : (ageCategory = "kids") && (price = 100);
 
 // Step 5 - Order confirmation
 const confirmation = prompt(
-  `You've ordered the ${ageCategory} order of ${selectedFoodName} with ${selectedSubtypeName}, please confirm: 1 - OK, 2 - No... `
+  `You've ordered the ${ageCategory} order of ${selectedFoodName} with ${selectedSubtypeName}. That will be ${price} SEK \n Please confirm: \n 1 - OK \n 2 - Not what I wanted `
 );
 if (confirmation === "1") {
-  alert(`Thank you for your order!`);
+  alert(`Thank you for your order, ${userName}!`);
 } else if (confirmation === "2") {
   alert("I see, please reload the page to make a new order");
 } else {
