0fd3db89b0df01ca1de211fbbcae8ff3b34c041f
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2024-01-25 17:08:17 +0100

    -m refactored the code

--- code/script.js
+++ code/script.js
@@ -1,109 +1,78 @@
-141621// Start here
+// Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`);
 
-const person = prompt ("Please enter your name")
-
-alert (
-  `Welcome ${person}`
-)
+const person = prompt("Please enter your name");
+alert(`Welcome ${person}`);
 
 // Step 2 - Food choice
-// Your code goes here
-let Food = prompt ("What's your craving today: 1 - Pizza  2 - Pasta  3- Salad Please enter the number of your choice!")
+let food = prompt("What's your craving today: 1 - Pizza  2 - Pasta  3- Salad Please enter the number of your choice!");
+let selectedFood = "";
 
-let selectedFood ="";
-
-
-
-//If the choice is 1, it is pizza
-//If the choice is 2, it is Pasta
-//If the choice is 3, it is Salad
-if (Food === "1") {
- selectedFood ="Pizza";
+// Refactor to Switch Case
+switch (food) {
+    case "1":
+        selectedFood = "Pizza";
+        break;
+    case "2":
+        selectedFood = "Pasta";
+        break;
+    case "3":
+        selectedFood = "Salad";
+        break;
+    default:
+        alert("Invalid choice. Please select a valid number.");
+        Process.exit(1);
 }
-else if (Food === "2") {
-  selectedFood ="Pasta";
-}
-else if (Food ==="3") {
-  selectedFood ="Salad";
-}
-else {
-  alert ("Invalid choice. Please select a valid number.");
-  Process.exit(1);
-}
-alert (
-  `You chosen ${selectedFood}`
-);
 
+alert(`You chose ${selectedFood}`);
 
 // Step 3 - Subtype choice
-// Your code goes here Ask the user to inser subtype based on choice of food.
-let subtype  = "";
-let subtypeName = []; //Store name for the subtypes pizza, pasta and salad
+let subtype = "";
+let subtypeName = []; // Store names for the subtypes pizza, pasta, and salad
 
 switch (selectedFood) {
-  case "Pizza":
-    subtype = prompt ('Select the type of pizza you want: 1 - Margherita 2 - Veggie 3 - Nutella Please enter the number of your choice:');
-    subtypeName = ["Margherita", "Veggie", "Nutella"];
-    break;
+    case "Pizza":
+        subtype = prompt('Select the type of pizza you want: 1 - Margherita 2 - Veggie 3 - Nutella Please enter the number of your choice:');
+        subtypeName = ["Margherita", "Veggie", "Nutella"];
+        break;
     case "Pasta":
-      subtype = prompt ('Select the type of pasta you want: 1 - Shrimppasta 2 - Veggiepasta 3 - Elf style pasta Please enter the number of your choice:');
-      subtypeName = ["Shrimp", "Veggie", "Elf style"];
-      break;
-      case "Salad":
-        subtype = prompt ('Select the type of salad you want: 1 - Shrimpsalad 2 - Greeksalad 3 - Tomatosalad Please enter the number of your choice:');
+        subtype = prompt('Select the type of pasta you want: 1 - Shrimp pasta 2 - Veggie pasta 3 - Elf style pasta Please enter the number of your choice:');
+        subtypeName = ["Shrimp", "Veggie", "Elf style"];
+        break;
+    case "Salad":
+        subtype = prompt('Select the type of salad you want: 1 - Shrimp salad 2 - Greek salad 3 - Tomato salad Please enter the number of your choice:');
         subtypeName = ["Shrimp", "Greek", "Tomato"];
         break;
 }
 
+// You can refactor the following alert into a single line outside the switch, as it's the same for all cases.
+alert(`You chose to order a  ${subtypeName[parseInt(subtype) - 1]} ${selectedFood} . Good choice!!.`);
 
-if (selectedFood === "Pizza") {
-  alert(`You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
-} else if (selectedFood === "Pasta") {
-  alert(`You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
-} else if (selectedFood === "Salad") {
-  alert(`You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
-}
 // Step 4 - Age
-// Your code goes here
+let age = prompt("Type your age in numbers to help me figure out if you want a kid's size or an adult size portion");
 
-let age = prompt ("Type your age in numbers to help med figure out if you want a kid size or an adult size portion")
-if (age>=19) {
-  alert (`The price is 150 SEK`);
+// Refactor to Switch Case
+switch (true) {
+    case age >= 19:
+        alert(`You chose and adult size portion. The price is 150 SEK`);
+        break;
+    case age <= 18:
+        alert(`You chose a kid's size portion. The price is 100 SEK`);
+        break;
 }
-else if (age<=18) {
-  alert (`The price is 100 SEK`);
-}
-
 
 // Step 5 - Order confirmation
-// Your code goes here
-
-if (selectedFood === "Pizza")
-{
-  alert(
-  `You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
-  
-
-  } else if (selectedFood === "Pasta") {
-  alert(
-    `You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
-}
-
-else if (selectedFood === "Salad") {
-alert(
-  `You chose ${selectedFood} and ${subtypeName[parseInt(subtype) - 1]} as your meal.`);
-}
-
-let order = prompt ("Do you want to place an order? Yes or no")
-if (order==="yes") {
-  alert (`Thank you for your order.`);
-}
-else if (order==="no") {
-  alert (`Hope to see you again!`);
+let order = prompt("Do you want to place an order? Yes or No");
+switch (order.toLowerCase()) {
+    case "yes":
+        alert(`Thank you for your order. Pick it up in 10 minutes!`);
+        break;
+    case "no":
+        alert(`Sorry to hear you changed tour mind! Hope to see you again!`);
+        break;
+    default:
+        alert(`Invalid response.`);
+        break;
 }
