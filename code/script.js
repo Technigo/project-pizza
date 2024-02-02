10fc0a4b3b9f4a70e851fce9bb2b5935202b27d9
Martin Jönsson <martin.t.joensson@gmail.com>
2024-02-02 21:05:03 +0100

    Sub-choices

--- code/script.js
+++ code/script.js
@@ -16,25 +16,75 @@ alert(
 // Your code goes here
 
 let category = prompt("Ok, " + name + " what would you like to order:\n1. Pizza\n2. Milkshake\n3. Salad")
-let catName
 if (category === "1"){
-  let catName = "Pizza"
-  alert("Pizza is a great choice!")
+  var catName = "Pizza";
 }
 else if (category === "2"){
-  let catName = "Milkshake"
-  alert("Milkshake is a great choice!")
+  var catName = "Milkshake";
 }
 else if (category === "3"){
-  let catName = "Salad"
-  alert("Salad is a great choice!")
+  var catName = "Salad";
 }
 else {
   alert("We cannot process orders outside of our categories.\nPlease restart the application.")
 }
+alert(catName + " is a great choice!")
 
 // Step 3 - Subtype choice
 // Your code goes here
+let subPizza;
+let subMilkshake;
+let subSalad;
+if (catName === "Pizza"){
+  let subPizza = prompt("Pizza? Great choice. Which pizza would you like?\n1. Vesuvio\n2. Margherita\n3. Capricciosa")
+  if (subPizza === "1"){
+    var subName = "Vesuvio"
+  }
+  else if (subPizza === "2"){
+    var subName = "Margherita"
+  }
+  else if (subPizza === "3"){
+    var subName = "Capricciosa"
+  }
+  else {
+    alert("We cannot process orders outside of our categories.\nPlease restart the application.")
+  }
+}
+else if (catName === "Milkshake"){
+  let subMilkshake = prompt("Milkshake? Great choice. Which flavour would you like?\n1. Vanilla\n2. Chocolate\n3. Strawberry")
+  if (subMilkshake === "1"){
+    var subName = "Vanilla Milkshake"
+  }
+  else if (subMilkshake === "2"){
+    var subName = "Chocolate Milkshake"
+  }
+  else if (subMilkshake === "3"){
+    var subName = "Strawberry Milkshake"
+  }
+  else {
+    alert("We cannot process orders outside of our categories.\nPlease restart the application.")
+  }
+}
+else if (catName === "Salad"){
+  let subSalad = prompt("What type of salad would you like?\n1. Ceasar salad\n2. Halloumi salad\n3. Ham and cheese salad")
+  if (subSalad === "1"){
+    var subName = "Ceasar salad"
+  }
+  else if (subSalad === "2"){
+    var subName = "Halloumi salad"
+  }
+  else if (subSalad === "3"){
+    var subName = "Ham and cheese salad"
+  }
+  else {
+    alert("We cannot process orders outside of our categories.\nPlease restart the application.")
+  }
+}
+else {
+  alert("I'm sorry, we cannot process orders outside of the predetemined dishes.\nPlease restart your order.")
+}
+alert(subName + " is a great choice!")
+
 
 // Step 4 - Age
 // Your code goes here
