e3a39af0bd5299c02b0e086e9aa697069c8d4979
Martin Jönsson <martin.t.joensson@gmail.com>
2024-02-02 20:34:09 +0100

    Food choices

--- code/script.js
+++ code/script.js
@@ -15,6 +15,24 @@ alert(
 // Step 2 - Food choice
 // Your code goes here
 
+let category = prompt("Ok, " + name + " what would you like to order:\n1. Pizza\n2. Milkshake\n3. Salad")
+let catName
+if (category === "1"){
+  let catName = "Pizza"
+  alert("Pizza is a great choice!")
+}
+else if (category === "2"){
+  let catName = "Milkshake"
+  alert("Milkshake is a great choice!")
+}
+else if (category === "3"){
+  let catName = "Salad"
+  alert("Salad is a great choice!")
+}
+else {
+  alert("We cannot process orders outside of our categories.\nPlease restart the application.")
+}
+
 // Step 3 - Subtype choice
 // Your code goes here
 
