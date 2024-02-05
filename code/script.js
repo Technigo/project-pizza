e1305a8594b66ef8433ddf089fb4e16746e5380e
Pernilla Sterner <psterner@hotmail.com>
2024-02-05 10:06:00 +0100

    Add Food choice

--- code/script.js
+++ code/script.js
@@ -11,7 +11,27 @@ const username = prompt(`Please enter your name:`);
 alert(`Nice to meet you, ${username}!`);
 
 // Step 2 - Food choice
-// Your code goes here
+let foodChoice = prompt(`
+  What type of food would you like to order?
+
+  Enter a number:
+  1. Pizza
+  2. Pasta
+  3. Salad
+
+`);
+
+if (foodChoice === "1") {
+  foodChoice = "Pizza";
+} else if (foodChoice === "2") {
+  foodChoice = "Pasta";
+} else if (foodChoice === "3") {
+  foodChoice = "Salad";
+} else {
+  alert(`Invalid choice. Please select a number between 1 and 3.`);
+}
+
+alert(`You've chosen ${foodChoice}!`);
 
 // Step 3 - Subtype choice
 // Your code goes here
