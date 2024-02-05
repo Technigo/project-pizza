564e21638041d25e66427e56cde7dd15f932813f
Pernilla Sterner <psterner@hotmail.com>
2024-02-05 10:00:01 +0100

    Add Welcome and Introduction

--- code/script.js
+++ code/script.js
@@ -1,10 +1,14 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+
+// Store name in variable and greet new user
+const username = prompt(`Please enter your name:`);
+
+alert(`Nice to meet you, ${username}!`);
 
 // Step 2 - Food choice
 // Your code goes here
