f36543c7fad464f0a0201e7d30d563918ddedeb4
Pernilla Sterner <psterner@hotmail.com>
2024-02-05 19:28:36 +0100

    Add loops for step 3 to restart the question if no or wrong answer is submitted

--- code/switch.js
+++ code/switch.js
@@ -17,9 +17,11 @@ window.addEventListener("load", function () {
     }
   }
 
+  let foodChoice;
+
   while (true) {
     // Step 2 - Food choice
-    let foodChoice = prompt(`
+    foodChoice = prompt(`
     What type of food would you like to order?
 
     Enter a number:
@@ -44,61 +46,65 @@ window.addEventListener("load", function () {
         continue;
     }
     alert(`You've chosen ${foodChoice}!`);
-    break; // Exit loop only if choice is valid
+    break;
+  }
+
+  // Step 3 - Subtype choice
+  const pizzaSubtypes = ["Napolitana", "Hawaian", "Pepperoni"];
+  const pastaSubtypes = [
+    "Spaghetti Carbonara",
+    "Fettucine Alfredo",
+    "Cheesy Tortellini",
+  ];
+  const saladSubtypes = ["Ceaser Salad", "Caprese Salad", "Greek Salad"];
+  let foodSubtypes = [];
+
+  switch (foodChoice) {
+    case "Pizza":
+      foodSubtypes = pizzaSubtypes;
+      break;
+    case "Pasta":
+      foodSubtypes = pastaSubtypes;
+      break;
+    case "Salad":
+      foodSubtypes = saladSubtypes;
+      break;
+    default:
+      alert(`Invalid ${foodChoice} type choice.`);
+  }
+
+  while (true) {
+    // Prompt the subtypes corresponding food choice
+    let subtypeChoice = prompt(`
+    Select a ${foodChoice} type
+
+    Enter a number:
+    1. ${foodSubtypes[0]}
+    2. ${foodSubtypes[1]}
+    3. ${foodSubtypes[2]}
+
+    `);
+
+    // Translate number into corresponding subtype
+    switch (subtypeChoice) {
+      case "1":
+        subtypeChoice = `${pizzaSubtypes[subtypeChoice - 1]}`;
+        break;
+      case "2":
+        subtypeChoice = `${pastaSubtypes[subtypeChoice - 1]}`;
+        break;
+      case "3":
+        subtypeChoice = `${saladSubtypes[subtypeChoice - 1]}`;
+        break;
+      default:
+        alert(`Invalid choice. Please select a number between 1 and 3.`);
+        continue;
+    }
+
+    alert(`You've chosen ${subtypeChoice}!`);
+    break;
   }
 });
-
-//   // Step 3 - Subtype choice
-//   const pizzaSubtypes = ["Napolitana", "Hawaian", "Pepperoni"];
-//   const pastaSubtypes = [
-//     "Spaghetti Carbonara",
-//     "Fettucine Alfredo",
-//     "Cheesy Tortellini",
-//   ];
-//   const saladSubtypes = ["Ceaser Salad", "Caprese Salad", "Greek Salad"];
-//   let foodSubtypes = [];
-
-//   switch (foodChoice) {
-//     case "Pizza":
-//       foodSubtypes = pizzaSubtypes;
-//       break;
-//     case "Pasta":
-//       foodSubtypes = pastaSubtypes;
-//       break;
-//     case "Salad":
-//       foodSubtypes = saladSubtypes;
-//       break;
-//     default:
-//       alert(`Invalid ${foodChoice} type choice.`);
-//   }
-
-//   // Prompt the subtypes corresponding food choice
-//   let subtypeChoice = prompt(`
-//     Select a ${foodChoice} type
-
-//     Enter a number:
-//     1. ${foodSubtypes[0]}
-//     2. ${foodSubtypes[1]}
-//     3. ${foodSubtypes[2]}
-
-// `);
-
-//   // Translate number into corresponding subtype
-//   switch (subtypeChoice) {
-//     case "1":
-//       subtypeChoice = `${pizzaSubtypes[subtypeChoice - 1]}`;
-//       break;
-//     case "2":
-//       subtypeChoice = `${pastaSubtypes[subtypeChoice - 1]}`;
-//       break;
-//     case "3":
-//       subtypeChoice = `${saladSubtypes[subtypeChoice - 1]}`;
-//       break;
-//     default:
-//       alert(`Invalid choice. Please select a number between 1 and 3.`);
-//   }
-
-//   alert(`You've chosen ${subtypeChoice}!`);
 
 //   // Step 4 - Age
 //   let userAge = prompt(`Is this food for a child or adult? Type your age:`);
