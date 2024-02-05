419db756e7d0ffb6dfc146b205af96b04c7a45f8
Trista <Trista8703@hotmail.com>
2024-02-05 18:14:57 +0200

    step 2

--- code/script.js
+++ code/script.js
@@ -6,8 +6,18 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 alert(`Hi, ` + prompt(`What is your name?`) + `!`)
+
 // Step 2 - Food choice
 // Your code goes here
+const orderFood = prompt(`What type of food would you like to order? Please enter the number of the food. 1. Pizza 2. Pasta 3. Salad`);
+
+if (orderFood == 1 ){
+  console.log (alert (`You have chosen pizza! Good choice!`));
+} else if (orderFood == 2){
+  console.log (alert (`You have chosen pasta! Good choice!`));
+} else if (orderFood == 3){
+  console.log (alert ( `You have chosen salad! Good choice!`));
+} else {console.log (alert (`Invalid typing, please enter the number of food.`))}
 
 // Step 3 - Subtype choice
 // Your code goes here
