556a4f75d9c6d77ad610dae105308e3644d9d8fa
Frida Svensson <fridamarisvensson@gmail.com>
2024-02-06 20:18:58 +0100

    Step 1 finished

--- code/script.js
+++ code/script.js
@@ -4,10 +4,23 @@
 // Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
+
+const firstName = prompt(`What's your name?`);
+alert(`Hello ${firstName}! 👋`);
+console.log(`Hello ${firstName}! 👋`);
 
 // Step 2 - Food choice
 // Your code goes here
+
+const foodChoice = prompt(
+  `What are you craving today?
+  1 - Pizza 🍕
+  2 - Pasta 🍝
+  3 - Salad 🥗
+  Please enter the number of your choice`
+);
+console.log($(foodChoice));
 
 // Step 3 - Subtype choice
 // Your code goes here
