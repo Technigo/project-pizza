fb93585c8f237e333a3089386b904d5d1d5ca74c
unknown <linda.frischknecht@bluewin.ch>
2024-02-06 13:09:41 +0100

    starting the project

--- code/script.js
+++ code/script.js
@@ -3,8 +3,18 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  "Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin."
 )
+
+prompt(
+  "Please enter your name"
+  )
+
+  let person = prompt("Please enter your name")
+
+  if (person != null) {
+    document.getElementById(elementId, string) ="Hello" + person + "Nice to meet you!"
+  }
 
 // Step 2 - Food choice
 // Your code goes here
