037db16201c663c83a8c9a878db3a56da20521c5
Lovisa Åberg <lovisaaberg@airsomtorlovisa.home>
2024-02-06 21:54:02 +0100

    Step 2 - Food choice

--- code/script.js
+++ code/script.js
@@ -17,6 +17,33 @@ alert(
 // Step 2 - Food choice
 // Your code goes here
 
+const foodChoice = prompt(
+  `What would you like to order?
+  Please enter the number of your choice:
+  1 - Pizza
+  2 - Pasta
+  3 - Salad
+  `
+)
+
+console.log(foodChoice)
+
+let selectedFood = ""
+
+if (foodChoice === "1") {
+  selectedFood = "Pizza"
+} else if (foodChoice === "2") {
+  selectedFood = "Pasta"
+} else if (foodChoice === "3") {
+  selectedFood = "Salad"
+} else {
+  alert("Invalid choice, please try again.")
+  exit(1)
+}
+
+alert(
+  `You chose ${selectedFood}, is that correct?`)
+
 // Step 3 - Subtype choice
 // Your code goes here
 
