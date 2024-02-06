2d9558ffaf84f810bf33ed9a698e89d5c28ea39e
Teddy <yoo.ji.yg@gmail.com>
2024-02-06 15:11:30 +0100

    added subtypes for the menu item selection

--- code/script.js
+++ code/script.js
@@ -8,10 +8,10 @@ alert(
 
 let yourName = prompt("What is your name?");
 
-alert('Welcome, ' + yourName + '!');
+alert(`Welcome, ${yourName}!`);
 
 // Step 2 - Food choice
-let menuItem = prompt(
+let menuSelect = prompt(
   `What would you like to order?
     Enter a number:
     1. Pizza  
@@ -19,20 +19,47 @@ let menuItem = prompt(
     3. Salad`
 );
 
+// let selectedItem = ""
 
-if (menuItem = 1) {
+if (menuSelect === "1") {
   alert('You selected Pizza');
-} else if (menuItem = 2) {
+} else if (menuSelect === "2") {
   alert('You selected Pasta');
 } else {
   alert('You selected Salad');
 };
 
 
-
-
 // Step 3 - Subtype choice
-// Your code goes here
+if (menuSelect === "1") {
+  prompt(
+    `Select the type of pizza:
+    Enter a number:
+    1. Hawaiian
+    2. Margherito
+    3. Pepperoni`
+  )
+} else if (menuSelect === "2") {
+  prompt(
+    `Select the type of pasta:
+    Enter a number:
+    1. Spaghetti alla Puttanesca
+    2. Carbonara
+    3. Pesto alla Genovese`
+  )
+} else { 
+  prompt(
+    `Select the type of salad:
+    Enter a number:
+    1. Caprese
+    2. Insalata di Riso
+    3. Panzanella`
+  )
+}
+
+
+
+
 
 // Step 4 - Age
 // Your code goes here
