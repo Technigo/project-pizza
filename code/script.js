f9839321183ed1bd5ade22393f92631d141dbf7c
Tejpex <equis_angel@hotmail.com>
2024-02-06 12:11:23 +0100

    First draft. Added some alerts and prompts.

--- code/script.js
+++ code/script.js
@@ -1,4 +1,5 @@
 // Start here
+let pizzaMenu = ("Margerita (tomato, cheese)", "Al Funghi (tomato, cheese, mushrooms)" )
 
 // Step 1 - Welcome and introduction
 // Your code goes here
@@ -6,11 +7,30 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+// Ask for name.
+
+prompt (
+  "What is your name?"
+)
+// Welcome with name
+
+alert ("Welcome" + "name")
 // Step 2 - Food choice
 // Your code goes here
 
+prompt ("What do you want to eat? Choose between pizza, burger or salad")
+
+let foodChoice=("pizza")
+
 // Step 3 - Subtype choice
 // Your code goes here
+
+if foodChoice === pizza {
+  prompt ("What type of pizza do you want? Write the number on the menu:")
+  table (pizzaMenu)
+} else {
+  prompt ("What type of salad do you want? Write the number on the menu:")
+}
 
 // Step 4 - Age
 // Your code goes here
