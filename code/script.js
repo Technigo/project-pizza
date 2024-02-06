422a04b7f004d2f78902cfb87878f2128fe7b4e8
Annika Sigfridsson <sigfridsson.annika@gmail.com>
2024-02-06 16:04:25 +0100

    first changes

--- code/script.js
+++ code/script.js
@@ -4,16 +4,93 @@
 // Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+
+)
+prompt (
+  'enter your name'
 )
 
 // Step 2 - Food choice
 // Your code goes here
+alert(
+  'What food are we getting today?'
+)
 
 // Step 3 - Subtype choice
 // Your code goes here
+alert(
+  'What type of'
+)
 
 // Step 4 - Age
 // Your code goes here
+alert(
+  'How old are you, you have to be 18 years old to proceed'
+  
+)
+prompt (
+  'enter your age'
+)
 
 // Step 5 - Order confirmation
 // Your code goes here
+alert(
+  'Order confirmation, please hold'
+)
+
+
+
+
+ const greeting = "Hello Class"
+
+ const movies = [
+  {
+    movie: "Wolf of Wallstreet"
+  }
+ ]
+ movies [0] ['movie'] = "Barbie"
+ movies [1] ['movie'] = "Barbie"
+ movies [2] ['movie'] = "Barbie"
+ console.log(Object.keys(movies).length)
+
+ //thank you message
+
+ let age = 15
+ if(age >= 18){
+ console.log ("You are an adult, pay 15 euros")
+} else {
+  console.log ("Teenager, ")
+}
+
+let score = 45 
+if (score >=  90){
+  console.log("Console")
+} else if (score>= 80) {
+  console.log ("Great job you chose b")
+} else if (score >= 70) {
+console.log ("you get a salad")
+} else if (score >=  60) {
+  console.log ("you get a salad")}
+
+  let isHungry = true
+  switch (choosedDish)
+{
+  case "Pizza":
+    console.log("Its winter")
+}
+
+const musicSelection = prompt(
+  "Select your favourit musik"
+  "1- Salad 2- Pizza "
+)
+
+console.log(musicSelection)
+
+selectedGenre = ""
+if musicseletion === "1"{
+  selectedGenre
+} else if (musicSelection === "2")
+
+else {
+  alert ("Invalid choise, please pick a number between 1 and 3")
+}
