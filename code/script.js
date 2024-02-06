bacc64b8734220a64892ffa903fe647f7badb9a2
Your El1an3 <eliane_w@yahoo.com>
2024-02-06 18:56:46 +0100

    Co-authored-by: sofia <sofia32057@users.noreply.github.com> Co-authored-by: Yifan-858 <Yifan-858@users.noreply.github.com>

--- code/script.js
+++ code/script.js
@@ -7,7 +7,7 @@ alert(
   "Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin."
 );
 
-let yourName = prompt("Please enter your name:");
+let yourName = prompt("Please enter your name:", "Eliane");
 
 alert("Hello " + yourName + "!");
 
@@ -15,70 +15,173 @@ alert("Hello " + yourName + "!");
 // Your code goes here
 
 let yourChoice = prompt(
-  "Please choose between Pizza, Pasta or Salad. <br> Enter the number 1 for Pizza, 2 for Pasta or 3 for Salad"
+  `Please choose between Pizza, Pasta or Salad.
+  Enter the number:
+  1 for Pizza
+  2 for Pasta
+  3 for Salad`
 );
 
-if (yourChoice == 1) {
-  alert("You've chosen Pizza");
-} else if (yourChoice == 2) {
-  alert("You've chosen Pasta");
-} else if (yourChoice == 3) {
-  alert("You've chosen Salad");
+let foodName;
+if (yourChoice === "1") {
+  foodName = "Pizza";
+} else if (yourChoice === "2") {
+  foodName = "Pasta";
+} else if (yourChoice === "3") {
+  foodName = "Salad";
+}
+
+if (foodName === "Pizza") {
+  alert("You've chosen " + foodName);
+} else if (foodName === "Pasta") {
+  alert("You've chosen " + foodName);
+} else if (foodName === "Salad") {
+  alert("You've chosen " + foodName);
 } else {
   alert(
-    "Please choose a number between 1 and 3: 1 for Pizza, 2 for Pasta or 3 for Salad"
+    `Please choose a number between 1 and 3:
+    1 for Pizza
+    2 for Pasta
+    3 for Salad`
   );
 }
 //Loop!?!?
-
 // Step 3 - Subtype choice
 // Your code goes here
 
-if (yourChoice == 1) {
-  alert(
-    "You can now choose between: 1 - Pizza Margherita, 2 - Pizza ai Funghi, 3 - Pizza Vegetariana"
+let yourSubchoice;
+if (foodName === "Pizza") {
+  yourSubchoice = prompt(
+    `You can now choose between:
+    1 - Pizza Margherita
+    2 - Pizza ai Funghi
+    3 - Pizza Vegetariana`
   );
-} else if (yourChoice == 2) {
-  alert(
-    "You can now choose between: 1 - Arrabiata, 2 - Aglio e Olio, 3 - Carbonara"
+} else if (foodName === "Pasta") {
+  yourSubchoice = prompt(
+    `You can now choose between:
+    1 - Arrabiata
+    2 - Aglio e Olio
+    3 - Carbonara`
   );
-} else if (yourChoice == 3) {
-  alert(
-    "You can now choose between: 1 - Green Salad, 2 -  Mixed Salad, 3 - Greek Salad"
+} else if (foodName === "Salad") {
+  yourSubchoice = prompt(
+    `You can now choose between:
+    1 - Green Salad
+    2 - Mixed Salad
+    3 - Greek Salad`
   );
 }
 
-let yourSubchoice = prompt("Make your choice now");
-
-if (yourChoice == 1 && yourSubchoice == 1) {
-  alert("Good choice! You've put a Pizza Margherita in your basket.");
-} else if (yourChoice == 1 && yourSubchoice == 2) {
-  alert("Good choice! You've put a Pizza ai Funghi in your basket.");
-} else if (yourChoice == 1 && yourSubchoice == 3) {
-  alert("Good choice! You've put a Pizza Vegetariana in your basket.");
-} else if (yourChoice == 2 && yourSubchoice == 1) {
-  alert("Good choice! You've put a Pasta Arrabiata in your basket.");
-} else if (yourChoice == 2 && yourSubchoice == 2) {
-  alert("Good choice! You've put a Pasta Aglio e Olio in your basket.");
-} else if (yourChoice == 2 && yourSubchoice == 3) {
-  alert("Good choice! You've put a Pasta Carbonara in your basket.");
-} else if (yourChoice == 3 && yourSubchoice == 1) {
-  alert("Good choice! You've put a Green Salad in your basket.");
-} else if (yourChoice == 3 && yourSubchoice == 2) {
-  alert("Good choice! You've put a Mixed Salad in your basket.");
-} else if (yourChoice == 3 && yourSubchoice == 3) {
-  alert("Good choice! You've put a Greek Salad in your basket.");
+/* -- do not delete - this works!!!!! --
+let pizzaChoice;
+if (foodName === "Pizza" && yourSubchoice === "1") {
+  pizzaChoice = "Pizza Margherita";
+  alert("Good choice! You've put a " + pizzaChoice + " in your basket.");
+} else if (foodName === "Pizza" && yourSubchoice === "2") {
+  pizzaChoice = "Pizza ai Funghi";
+  alert("Good choice! You've put a " + pizzaChoice + " in your basket.");
+} else if (foodName === "Pizza" && yourSubchoice === "3") {
+  pizzaChoice = "Pizza Vegetariana";
+  alert("Good choice! You've put a " + pizzaChoice + " in your basket.");
 }
-// Step 4 - Age
-// Your code goes here
 
-let yourAge = prompt("Please enter your age:");
+let pastaChoice;
+if (foodName === "Pasta" && yourSubchoice === "1") {
+  pastaChoice = "Pasta Arrabiata";
+  alert("Good choice! You've put a " + pastaChoice + " in your basket.");
+} else if (foodName === "Pasta" && yourSubchoice === "2") {
+  ppastaChoice = "Pasta Aglio e Olio";
+  alert("Good choice! You've put a " + pastaChoice + " in your basket.");
+} else if (foodName === "Pasta" && yourSubchoice === "3") {
+  pastaChoice = "Pasta Carbonara";
+  alert("Good choice! You've put a " + pastaChoice + " in your basket.");
+}
 
+let saladChoice;
+if (foodName === "Salad" && yourSubchoice === "1") {
+  saladChoice = "Green Salad";
+  alert("Good choice! You've put a " + saladChoice + " in your basket.");
+} else if (foodName === "Salad" && yourSubchoice === "2") {
+  saladChoice = "Mixed Salad";
+  alert("Good choice! You've put a " + saladChoice + " in your basket.");
+} else if (foodName === "Salad" && yourSubchoice === "3") {
+  saladChoice = "Greek Salad";
+  alert("Good choice! You've put a " + saladChoice + " in your basket.");
+}
+*/
+
+let foodChoice;
+if (foodName === "Pizza" && yourSubchoice === "1") {
+  foodChoice = "Pizza Margherita";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Pizza" && yourSubchoice === "2") {
+  foodChoice = "Pizza ai Funghi";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Pizza" && yourSubchoice === "3") {
+  foodChoice = "Pizza Vegetariana";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Pasta" && yourSubchoice === "1") {
+  foodChoice = "Pasta Arrabiata";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Pasta" && yourSubchoice === "2") {
+  foodChoice = "Pasta Aglio e Olio";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Pasta" && yourSubchoice === "3") {
+  foodChoice = "Pasta Carbonara";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Salad" && yourSubchoice === "1") {
+  foodChoice = "Green Salad";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Salad" && yourSubchoice === "2") {
+  foodChoice = "Mixed Salad";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+} else if (foodName === "Salad" && yourSubchoice === "3") {
+  foodChoice = "Greek Salad";
+  alert("Good choice! You've put a " + foodChoice + " in your basket.");
+}
+
+// Step 4.1 - Age
+// This part defines size of the meal based on the age of the person ordering
+// Step 4.2 - This part associates costs to the meal
+let yourAge = prompt("Please enter your age to define the size of your meal:");
+
+let orderCost = 10;
 if (yourAge <= 12) {
-  alert("You ordered a child size meal!");
+  yourAge = "child size";
+  alert(
+    "You ordered a " +
+      yourAge +
+      " " +
+      foodChoice +
+      " for " +
+      orderCost +
+      " EUR!"
+  );
 } else if (yourAge > 12) {
-  alert("You ordered a regular size meal!");
+  yourAge = "regular size";
+  alert(
+    "You ordered a " +
+      yourAge +
+      " " +
+      foodChoice +
+      " for " +
+      orderCost + //check math here - it's not working!!
+      5 +
+      " EUR!"
+  );
 }
+
+let yourOrder = prompt(
+  "Thank you for ordering with us. Based on your choices you ordered " +
+    yourAge +
+    " " +
+    foodChoice +
+    " for " +
+    orderCost + // Check math here - it's not working!!
+    " EUR. " +
+    "Please confirm your order now. Enter Yes or No to confirm."
+);
 
 // Step 5 - Order confirmation
-// Your code goes here
+// Your code goes here*/
