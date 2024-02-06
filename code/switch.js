6b2807a1e4d42b9053e45575d0fe60d3fbc662ba
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2024-02-06 22:05:10 +0100

    improve the readability

--- code/switch.js
+++ code/switch.js
@@ -2,41 +2,50 @@
 
 // Step 1 - Welcome and introduction
 // Your code goes here
+
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 );
+
 let userName = prompt("What's your name?");
-alert(`Hi, ${userName}!`);
+
+if (userName.length === 0) {
+  alert(`Hello, stranger!`);
+} else {
+  alert(`Hi, ${userName}!`);
+}
 
 // Step 2 - Food choice
 // Your code goes here
 let userInput = prompt(
-  "What type of food would you like to order?\n Enter a number:\n 1 - Pizza\n 2 - Pasta\n 3 - Salad"
+  `What type of food would you like to order?Enter a number:
+     1 - Pizza
+     2 - Pasta
+     3 - Salad`
 );
 
-let orderNumber = Number(userInput);
+let orderNumber = parseInt(userInput);
 let mealType;
 
 switch (orderNumber) {
   case 1:
     mealType = "Pizza";
-    alert("You've chosen Pizza!");
     break;
 
   case 2:
     mealType = "Pasta";
-    alert("You've chosen Pasta!");
     break;
 
   case 3:
     mealType = "Salad";
-    alert("You've chosen Salad!");
     break;
 
   default:
     alert("Invaild input. Please choose a number between 1-3.");
     break;
 }
+
+alert(`You've chosen ${mealType}!`);
 
 // Step 3 - Subtype choice
 // Your code goes here
@@ -46,114 +55,132 @@ let subMealType;
 switch (orderNumber) {
   case 1:
     subInput = prompt(
-      "Select a Pizza type: \n Enter a number:\n 1 - Napolitana\n 2 - Hawaian\n 3 - Pepperoni"
+      `Select a Pizza type: 
+      Enter a number:
+      1 - Napolitana
+      2 - Hawaian
+      3 - Pepperoni`
     );
 
-    let pizzaType = Number(subInput);
+    let pizzaType = parseInt(subInput);
 
     switch (pizzaType) {
       case 1:
         subMealType = "Napolitana";
-        alert("You've chosen Napolitana!");
         break;
 
       case 2:
         subMealType = "Hawaian";
-        alert("You've chosen Hawaian!");
         break;
 
       case 3:
         subMealType = "Pepperoni";
-        alert("You've chosen Pepperoni!");
         break;
 
       default:
         alert("Invaild pizza type choice.");
         break;
     }
+
     break;
 
   case 2:
     subInput = prompt(
-      "Select a Pasta type: \n Enter a number:\n 1 - Spaghetti Carbonara\n 2 - Fettuccine Alfredo\n 3 - Cheesy Tortellini"
+      `Select a Pasta type: 
+      Enter a number:
+      1 - Spaghetti Carbonara
+      2 - Fettuccine Alfredo
+      3 - Cheesy Tortellini`
     );
 
-    let pastaType = Number(subInput);
+    let pastaType = parseInt(subInput);
 
     switch (pastaType) {
       case 1:
         subMealType = "Spaghetti Carbonara";
-        alert("You've chosen Spaghetti Carbonara!");
         break;
 
       case 2:
         subMealType = "Fettuccine Alfredo";
-        alert("You've chosen Fettuccine Alfredo!");
         break;
 
       case 3:
         subMealType = "Cheesy Tortellini";
-        alert("You've chosen Cheesy Tortellini!");
         break;
 
       default:
         alert("Invaild pasta type choice.");
         break;
     }
+
     break;
 
   case 3:
     subInput = prompt(
-      "Select a Salad type: \n Enter a number:\n 1 - Caesar Salad\n 2 - Caprese Salad\n 3 - Greek Salad"
+      `Select a Salad type: 
+      Enter a number:
+      1 - Caesar Salad
+      2 - Caprese Salad
+      3 - Greek Salad`
     );
 
-    let saladType = Number(subInput);
+    let saladType = parseInt(subInput);
 
     switch (saladType) {
       case 1:
         subMealType = "Caesar Salad";
-        alert("You've chosen Caesar Salad!");
         break;
 
       case 2:
         subMealType = "Caprese Salad";
-        alert("You've chosen Caprese Salad!");
         break;
 
       case 3:
         subMealType = "Greek Salad";
-        alert("You've chosen Greek Salad!");
         break;
 
       default:
         alert("Invaild salad type choice.");
         break;
     }
+
     break;
 
   default:
-    alert("Invaild input. Please try again.");
+    alert(`Hmm..something seems to be wrong. Please choose again!`);
     break;
 }
+
+alert(`You've chosen ${subMealType}!`);
 
 // Step 4 - Age
 // Your code goes here
 
 let userAge = prompt("Is this food for a child or an adult? Type your age:");
 
-let ageNumber = Number(userAge);
+let ageNumber = parent(userAge);
+
+const childPrice = 10;
+const adultPrice = 15;
+
 let userConfirm;
 
 switch (true) {
   case ageNumber > 0 && ageNumber <= 17:
     userConfirm = prompt(
-      `One child sized ${subMealType} ${mealType} will be prepared for you. That'll be 10€. Are you sure you want to order this?\n Enter a number to confirm:\n 1 - Yes\n 2 - No`
+      `One child sized ${subMealType} ${mealType} will be prepared for you. That'll be ${childPrice}€. Are you sure you want to order this?
+      Enter a number to confirm:
+      1 - Yes 
+      2 - No`
     );
     break;
 
   case ageNumber > 17:
     userConfirm = prompt(
-      `One adult sized ${subMealType} ${mealType} will be prepared for you. That'll be 15€. Are you sure you want to order this?\n Enter a number to confirm:\n 1 - Yes\n 2 - No`
+      `One adult sized ${subMealType} ${mealType} will be prepared for you. That'll be ${adultPrice}€. Are you sure you want to order this? 
+      Enter a number to confirm: 
+      1 - Yes 
+      2 - No`
     );
     break;
 
