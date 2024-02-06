21858e01a9f55b4d1e44211196409376f97f1a15
Annika Sigfridsson <sigfridsson.annika@gmail.com>
2024-02-06 17:47:57 +0100

    changes made 2

--- code/script.js
+++ code/script.js
@@ -6,31 +6,58 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 
 )
-prompt (
-  'enter your name'
-)
+
+
+let userName = prompt("Enter your name here")
+let chosenName = userName;
+alert ("Hello" + " " + chosenName + "!")
 
 // Step 2 - Food choice
 // Your code goes here
-alert(
-  'What food are we getting today?'
-)
 
+let choice = prompt (`What food are we getting today? 
+Please choose 1 for Pizza,
+ 2 for Pasta
+  and 3 for Salad` )
+if (choice === 1){
+  alert ("Pizza")
+} else if (choice === 2) {
+  alert ("Salad")
+} else if (choice == 3) {
+alert ("Pasta")
+} else if (choice >=4) {
+  alert ("Reload the page to continue ordering")}
+
+
+switch (choice){
+  case "1":
+    alert("You chose pizza");
+    break;
+    case "2":
+      alert("You chose Salad");
+      break;
+      case "3":
+        alert("You chose Pasta");
+        break;
+        default:
+          alert("Invalid choice. Please chose 1, 2 or 3")
+        
+}
 // Step 3 - Subtype choice
 // Your code goes here
-alert(
-  'What type of'
-)
+
 
 // Step 4 - Age
 // Your code goes here
-alert(
-  'How old are you, you have to be 18 years old to proceed'
-  
-)
-prompt (
-  'enter your age'
-)
+
+ let age = prompt ("Please enter your age")
+ let chosenAge = age;
+ if(age >= 17){
+ console.log ("You are above 18 and can proceed to the checkout")
+} if (age <= 17) {
+  console.log ("too young to proceed ")
+}
+
 
 // Step 5 - Order confirmation
 // Your code goes here
@@ -38,59 +65,10 @@ alert(
   'Order confirmation, please hold'
 )
 
-
-
-
- const greeting = "Hello Class"
-
- const movies = [
-  {
-    movie: "Wolf of Wallstreet"
-  }
- ]
- movies [0] ['movie'] = "Barbie"
- movies [1] ['movie'] = "Barbie"
- movies [2] ['movie'] = "Barbie"
- console.log(Object.keys(movies).length)
-
  //thank you message
 
- let age = 15
- if(age >= 18){
- console.log ("You are an adult, pay 15 euros")
-} else {
-  console.log ("Teenager, ")
-}
 
-let score = 45 
-if (score >=  90){
-  console.log("Console")
-} else if (score>= 80) {
-  console.log ("Great job you chose b")
-} else if (score >= 70) {
-console.log ("you get a salad")
-} else if (score >=  60) {
-  console.log ("you get a salad")}
 
-  let isHungry = true
-  switch (choosedDish)
-{
-  case "Pizza":
-    console.log("Its winter")
-}
 
-const musicSelection = prompt(
-  "Select your favourit musik"
-  "1- Salad 2- Pizza "
-)
 
-console.log(musicSelection)
 
-selectedGenre = ""
-if musicseletion === "1"{
-  selectedGenre
-} else if (musicSelection === "2")
-
-else {
-  alert ("Invalid choise, please pick a number between 1 and 3")
-}
