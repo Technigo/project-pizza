5030354bc1b05a2988c037125a858a82aa01a94f
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-02-07 15:29:57 +0100

    completed step 2

--- code/script.js
+++ code/script.js
@@ -6,13 +6,37 @@ alert(
   `Welcome to our JavaScript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
-let name = prompt("What is your name?")
+let userName = prompt("What is your name?")
 alert(
-  `Ciao ${name}, great to see you!`
+  `Ciao ${userName}, great to see you! Click 'OK' to begin your order.`
   )
 
 // Step 2 - Food choice
 // Your code goes here
+
+let foodSelection = prompt (
+  `What do you want to order?
+  Please enter the corresponding number:
+  1 - Pizza
+  2 - Pasta
+  3 - Salad`
+)
+
+let selectedDish = ""
+
+if (foodSelection === "1") {
+  selectedDish = "pizza"
+} else if (foodSelection === "2") {
+  selectedDish = "pasta"
+} else if (foodSelection === "3") {
+  selectedDish = "salad"
+} else {
+  alert("Invalid choice, please try again!")
+  exit(1)
+}
+
+alert(`You have selected ${selectedDish}! Click 'OK' to continue.`)
+
 
 // Step 3 - Subtype choice
 // Your code goes here
