9a6108a6d48a22fe30e5aa8ae0d19dae2e12d8a0
unknown <linda.frischknecht@bluewin.ch>
2024-02-07 10:27:49 +0100

    added the food choice

--- code/script.js
+++ code/script.js
@@ -6,7 +6,7 @@ alert(
   "Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin."
 )
 
-//Asking person for name with promt
+//Asking person for name with prompt
 
 let person = prompt("Please enter your name")
 if (person) {
@@ -21,6 +21,30 @@ if (person) {
 
 // Step 2 - Food choice
 // Your code goes here
+
+let food = prompt(`Please select your choice of Food by entering the number.
+1 - Pizza
+2 - Pasta
+3 - Lasagne`)
+if (food == 1) {
+  alert (
+    `You choose Pizza.`
+  )
+} else if (food == 2) {
+  alert (
+    `You choose Pasta.`
+  )
+} else if (food == 3) {
+  alert (
+    `You choose Lasagne.`
+  )
+} else {
+  alert (
+    `You haven't choosen anything.`
+  )
+}
+
+
 
 // Step 3 - Subtype choice
 // Your code goes here
