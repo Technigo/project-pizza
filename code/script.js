33d46c3b982d0c81763eef7c8ff4e34304094daf
Your Name <you@example.com>
2024-02-07 11:06:51 +0100

    Made steps 1 and 2 of the code exercise

--- code/script.js
+++ code/script.js
@@ -1,17 +1,47 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+
+alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`)
+let userName = prompt(`What's your name?`)
+let upperCaseName = userName.toUpperCase()
+alert(`Hi ${upperCaseName}! What a preatty name you have!
+Welcome into our pizzeria ${upperCaseName},
+
+Press Ok to continue.`)
 
 // Step 2 - Food choice
-// Your code goes here
+let mainDish = prompt(`What are you going to eat? You have 3 options:
+ 1 - Pizza
+ 2 - Pasta
+ 3 - Salad
+ (answer by typing 1, 2 or 3)`)
 
+switch (mainDish){
+  case "1":
+  alert (`Youmy! Pizza 🍕
+  
+  OK to continue.`)
+    break
+  case "2":
+  alert (`The HGI option:D Pasta!! 🍝
+
+  OK to continue.`)
+    break
+  case "3":
+  alert (`Salad 🥗, the healthy choise!
+  
+  OK to continue.`)
+    break
+  default:
+    alert (`Make sure you are typing one of the 3 options (1, 2 or 3).
+    
+Reload the page and start again.`)
+}
 // Step 3 - Subtype choice
-// Your code goes here
+prompt(
 
+)
 // Step 4 - Age
 // Your code goes here
 
