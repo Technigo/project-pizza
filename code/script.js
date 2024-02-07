a9d22d9dd3de2c3f3017b4026510e35a71c53054
unknown <linda.frischknecht@bluewin.ch>
2024-02-07 12:14:37 +0100

    added subchoices, having some styling problems

--- code/script.js
+++ code/script.js
@@ -19,6 +19,7 @@ if (person) {
   )
   }
 
+
 // Step 2 - Food choice
 // Your code goes here
 
@@ -26,6 +27,7 @@ let food = prompt(`Please select your choice of Food by entering the number.
 1 - Pizza
 2 - Pasta
 3 - Lasagne`)
+
 if (food == 1) {
   alert (
     `You choose Pizza.`
@@ -45,9 +47,63 @@ if (food == 1) {
 }
 
 
-
 // Step 3 - Subtype choice
 // Your code goes here
+
+let subtype = []
+if (food == 1) {
+  subtype = ["1 - Margherita","2 - Pepperoni","3 - Hawaiian"]
+} else if (food == 2) {
+  subtype = ["1 - Carbonara","2 - Bolognese","3 - Alfredo"]
+} else if (food == 3) {
+  subtype = ["1 - With Meet","2 - Vegetarian","3 - Vegan"]
+} else {
+  subtype = ["nothing"]
+}
+
+let choice = prompt (`Please select your meal from the following by entering the number\n ${subtype.join("\n")}`)
+if (food == 1 && choice == 1) {
+  alert (
+    `You choose Margherita.`
+  )
+} else if (food == 1 && choice == 2) {
+  alert (
+    `You choose Pepperoni.`
+  )
+} else if (food == 1 && choice == 3) {
+  alert (
+    `You choose Hawaiian.`
+  )
+} else if (food == 2 && choice == 1) {
+  alert (
+    `You choose Carbonara.`
+  )
+} else if (food == 2 && choice == 2) {
+  alert (
+    `You choose Bolognese.`
+  )
+} else if (food == 2 && choice == 3) {
+  alert (
+    `You choose Alfredo.`
+  )
+} else if (food == 3 && choice == 1) {
+  alert (
+    `You choose with meet.`
+  )
+} else if (food == 3 && choice == 2) {
+  alert (
+    `You choose vegetarian.`
+  )
+} else if (food == 3 && choice == 3) {
+  alert (
+    `You choose vegan.`
+  )
+} else {
+  alert (
+    `You choose nothing.`
+  )
+}
+
 
 // Step 4 - Age
 // Your code goes here
