ec46219626bb13695e06dcb92838757d6d9b4f31
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-02-07 10:05:53 +0100

    Completed step 2 in the instructions using if and if else

--- code/script.js
+++ code/script.js
@@ -10,15 +10,42 @@ alert(
 //   "Please enter your name!"
 // )
 
-let yourName = prompt("Please enter your name!")
+let yourName = prompt(`Please enter your name!`)
 // console.log("Hi " + yourName + ", nice to see you!")
 
 alert(
-  "Hi " + yourName + ", nice to see you!"
+  `Hi ${yourName}, nice to see you!`
 )
 
 // Step 2 - Food choice
 // Your code goes here
+
+const preferedFood = prompt(
+  `What do you want to eat today?
+
+  1 - Pizza
+  2 - Pasta
+  3 - Salad
+
+  Please enter the number of your choice.
+  `
+)
+
+let selectedFood = ""
+
+if (preferedFood === "1") {
+  selectedFood = "Pizza"
+} else if (preferedFood === "2") {
+  selectedFood = "Pasta"
+} else if (preferedFood === "3") {
+  selectedFood = "Salad"
+} else {
+  alert("I'm afraid that's not an option. Please start again and select one of the options (1-3).")
+  exit(1)
+}
+
+alert(
+  `You chose ${selectedFood}. Great choise!`)
 
 // Step 3 - Subtype choice
 // Your code goes here
