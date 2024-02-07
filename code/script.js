211f856a41c094449de5e452f8a61ebe833827dd
schouenkes <m.schouenke@gmail.com>
2024-02-07 14:34:14 +0100

    added step 2, an if statement

--- code/script.js
+++ code/script.js
@@ -1,16 +1,36 @@
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+// Step 1 - Welcome and introduction
+alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`);
 
-const name = prompt (`Please enter your name:`)
-console.log (name)
+const name = prompt(`Please enter your name:`);
+console.log(name);
 
-alert(
-  `Hi ${name}, it's nice to see you here!`
-)
+alert(`Hi ${name}, it's nice to see you here!`);
 
 // Step 2 - Food choice
-// Your code goes here
+
+const foodOption = prompt(`What would you like to order?
+
+Enter the number of your choice.
+1 - Pizza Margherita
+2 - Pizza Kebab
+3 - Caesar Salad
+`);
+
+console.log(foodOption);
+
+let foodSelected = "";
+if (foodOption === "1") {
+   foodSelected = "Pizza Margherita";
+} else if (foodOption === "2") {
+   foodSelected = "Pizza Kebab";
+} else if (foodOption === "3") {
+   foodSelected = "Caesar Salad";
+} else {
+   alert(`Couldn't you decide what to choose? 
+    Please, start over and choose what you want to eat.`);
+}
+
+alert(`Good choice, you chosed to order ${foodSelected}`);
 
 // Step 3 - Subtype choice
 // Your code goes here
@@ -20,4 +40,3 @@ alert(
 
 // Step 5 - Order confirmation
 // Your code goes here
-
