4f81c08e2751c2b228a58ce4100049c9eb7249fa
Tejpex <equis_angel@hotmail.com>
2024-02-07 10:49:50 +0100

    Wrote welcome message with user's name included

--- code/script.js
+++ code/script.js
@@ -1,36 +1,35 @@
-// Start here
-let pizzaMenu = ("Margerita (tomato, cheese)", "Al Funghi (tomato, cheese, mushrooms)" )
-
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+);
 
 // Ask for name.
+let userName = prompt(`What is your name?`);
+console.log(userName);
 
-prompt (
-  "What is your name?"
-)
-// Welcome with name
+//Welcome with name
+alert(`Welcome ${userName}! Let's order some food`);
 
-alert ("Welcome" + "name")
 // Step 2 - Food choice
-// Your code goes here
+let foodChoice = prompt(
+  `What do you want to eat? 
+  Choose between pizza, burger or salad and write your answer.`
+);
 
-prompt ("What do you want to eat? Choose between pizza, burger or salad")
+console.log(foodChoice);
 
-let foodChoice=("pizza")
+let pizzaMenu =
+  ("Margerita (tomato, cheese)", "Al Funghi (tomato, cheese, mushrooms)");
 
 // Step 3 - Subtype choice
 // Your code goes here
 
-if foodChoice === pizza {
+/*if foodChoice === pizza {
   prompt ("What type of pizza do you want? Write the number on the menu:")
   table (pizzaMenu)
 } else {
   prompt ("What type of salad do you want? Write the number on the menu:")
-}
+}*/
 
 // Step 4 - Age
 // Your code goes here
