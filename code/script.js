c3d137cdaf46815b6fb206ebcc8d7eb1870acfde
AHPIXI <148851757+AHPIXI@users.noreply.github.com>
2024-02-08 21:32:13 +0100

    Added let and if(else if) for step 2

--- code/script.js
+++ code/script.js
@@ -13,7 +13,31 @@ alert(`Hi ${clientName}! Let's order some delicious food! Are you ready?`)
 
 // Step 2 - Food choice
 // Your code goes here
+const foodChoice = prompt(`
+Select your food:
+1 - Pizza
+2 - Pasta
+3 - Salad
+Please enter the number of your choice.
+`)
 
+console.log(foodChoice)
+
+let selectedFood = ""
+
+if (foodChoice === 1) {
+  selectedFood = "Pizza"
+} else if (foodChoice === 2) {
+  selectedFood = "Pasta"
+} else if (foodChoice === 3) {
+  selectedFood = "Salad"
+} else {
+  alert("Sorry - that's not a valid option. Please start over and choose a valid option. Thank you!")
+  exit(1)
+}
+
+console.log(selectedFood)
+alert(`You choose ${selectedFood} Excellent choice!!`)
 
 // Step 3 - Subtype choice
 // Your code goes here
