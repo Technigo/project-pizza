5d565ee82f1c304a2ad8e11e2e4d3cb44c3730bf
unknown <linda.frischknecht@bluewin.ch>
2024-02-08 15:33:44 +0100

    fixed problem with name and deleted the old code

--- code/switch.js
+++ code/switch.js
@@ -6,38 +6,36 @@ alert(
     "Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin."
   )
   
-  //Asking person for name with prompt
+  //Asking person for name
   
   let person = prompt("Please enter your name")
   
   switch (person) {
-    case person:
-        alert (
-            `Hi and welcome ${person}`
-        )
+    case "Anna":
+      alert (
+        `Hi and welcome Anna.`
+      )
+    break
+    case "Charlie":
+      alert (
+        `Hi and welcome Charlie.`
+      )
+    break
+    case "Tom":
+      alert (
+        `Hi and welcome Tom.`
+      )
     break
     default:
         alert (
-            `Hi and welcome anonymous.`
+            `I am sorry, I don't know your name. I am gonna call you Bob. Welcome Bob.`
         )
-    break
   } 
-
-  /*if (person) {
-    alert (
-      `Hi and welcome ${person}`
-      )
-    } else {
-    alert (
-    `Hi and welcome anonymous.`
-    )
-    }*/
   
   
   // Step 2 - Food choice
   // Your code goes here
   
-// We can use parseInt or use "1"
 
   let food = parseInt (
     prompt(`Please select your choice of Food by entering the number.
@@ -70,25 +68,6 @@ alert(
   }
     
   
-  /*if (food == 1) {
-    alert (
-      `You choose Pizza.`
-    )
-  } else if (food == 2) {
-    alert (
-      `You choose Pasta.`
-    )
-  } else if (food == 3) {
-    alert (
-      `You choose Lasagne.`
-    )
-  } else {
-    alert (
-      `You haven't choosen anything.`
-    )
-  }*/
-  
-  
   // Step 3 - Subtype choice
   // Your code goes here
   
@@ -107,17 +86,6 @@ alert(
       subtype = ["nothing"]
       exit (1)
   }
-
-
-  /*if (food == 1) {
-    subtype = ["1 - Margherita","2 - Pepperoni","3 - Hawaiian"]
-  } else if (food == 2) {
-    subtype = ["1 - Carbonara","2 - Bolognese","3 - Alfredo"]
-  } else if (food == 3) {
-    subtype = ["1 - With Meet","2 - Vegetarian","3 - Vegan"]
-  } else {
-    subtype = ["nothing"]
-  }*/
   
   let choice = parseInt 
     (prompt (`Please select your meal from the following by entering the number\n ${subtype.join("\n")}`))
@@ -177,48 +145,6 @@ alert(
   } 
 
   console.log (choice)
-  
-  /*if (food == 1 && choice == 1) {
-    alert (
-      `You choose Margherita.`
-    )
-  } else if (food == 1 && choice == 2) {
-    alert (
-      `You choose Pepperoni.`
-    )
-  } else if (food == 1 && choice == 3) {
-    alert (
-      `You choose Hawaiian.`
-    )
-  } else if (food == 2 && choice == 1) {
-    alert (
-      `You choose Carbonara.`
-    )
-  } else if (food == 2 && choice == 2) {
-    alert (
-      `You choose Bolognese.`
-    )
-  } else if (food == 2 && choice == 3) {
-    alert (
-      `You choose Alfredo.`
-    )
-  } else if (food == 3 && choice == 1) {
-    alert (
-      `You choose with meet.`
-    )
-  } else if (food == 3 && choice == 2) {
-    alert (
-      `You choose vegetarian.`
-    )
-  } else if (food == 3 && choice == 3) {
-    alert (
-      `You choose vegan.`
-    )
-  } else {
-    alert (
-      `You choose nothing.`
-    )
-  }*/
   
   
   // Step 4 - Age
@@ -352,27 +278,20 @@ alert(
       exit (1)
   }
 
-
-  /*
-  if (age >= 18) {
-    alert (`We will prepare one adult size ${foodResult} ${subtypeResult} for you. That'll cost 20 CHF in total. `)
-  }
-  else {
-    alert (`We will prepare one child size ${foodResult} ${subtypeResult} for you. That'll cost 15 CHF in total.`)
-  }
-  */
   
   // Step 5 - Order confirmation
   // Your code goes here
   
-  let confirmation = prompt (`Are you sure you want to order this?
+  let confirmation = parseInt (prompt (`Are you sure you want to order this?
   Please select a number to confirm:
   1 - Yes
-  2 - No`)
+  2 - No`))
   
-  if (confirmation == 1) {
-    alert (`Thank you for your order. We will prepare it for you!`)
-  } else {
-    alert (`You cancelled your order. Please start from the beginning.`)
+  switch (confirmation) {
+    case 1:
+      alert (`Thank you for your order. We will prepare it for you!`)
+    break
+    default:
+      alert (`You cancelled your order. Please start from the beginning.`)
+      exit(1)
   }
-  
