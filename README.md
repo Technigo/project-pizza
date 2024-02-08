cfe3aebffb24132beb97ccddfa0e2255799341d8
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-02-08 14:00:44 +0100

    some updates

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The task was to familiarise ouselves with variables, conditionals, and native me
 
 I started out writing out parts of the code on a piece of paper, to get a feel for what to do. I have done some programming before, so I was already somewhat familiar with variables and if/else/else if statements. However, I had not seen the switch and ternary operators before. The description and examples on the Week 1 Notion page were very helpful and easy to understand. After finishing the assignment using if statments, I therefore decided to try out the stretch goalas in switchc.js, which was fun. 
 
-With more time, I would have tried to make a version that used arrays in some form.
+With more time, I would have tried to make a version that used arrays.
 
 ## View it live
 
--- code/script.js
+++ code/script.js
@@ -12,27 +12,27 @@ alert(
 
 const customer = prompt("Please enter your name:"); 
 
-if (!customer) {
+if (customer) {
+  alert(`Happy to meet you, ${customer}!`);
+} else {
   alert("That's not a name. Please refresh site and try again.");
   exit(1);
-} else {
-  alert(`Happy to meet you, ${customer}!`);
 }
 
 // Step 2 - Food choice
 
-const foodType = prompt(`
+const foodType = parseInt(prompt(`
   What type of food would you like to order? 
   Please enter a number: 
   1 - Pizza 
   2 - Pasta 
-  3 - Salad`)
+  3 - Salad`));
 
-if (foodType === "1") {
+if (foodType === 1) {
   alert("You've chosen Pizza!");
-} else if (foodType === "2") {
+} else if (foodType === 2) {
   alert("You've chosen Pasta!");
-} else if (foodType === "3") {
+} else if (foodType === 3) {
   alert("You've chosen Salad!");
 } else {
   alert("Invalid food type. Please refresh site and try again.");
@@ -42,19 +42,19 @@ if (foodType === "1") {
 // Step 3 - Subtype choice
 
 // Pizza 
-if (foodType === "1") {
-  const pizzaType = prompt(
+if (foodType === 1) {
+  const pizzaType = parseInt(prompt(
     `Please select a type of Pizza. 
     Enter a number:  
     1 - Vesuvius  
     2 - Hawaii 
-    3 - Capricciosa`)
+    3 - Capricciosa`));
         
-  if (pizzaType === "1") {
+  if (pizzaType === 1) {
     orderedFood = "Vesuvius";
-  } else if (pizzaType === "2") {
+  } else if (pizzaType === 2) {
     orderedFood = "Hawaii";
-  } else if (pizzaType === "3") {
+  } else if (pizzaType === 3) {
     orderedFood = "Capricciosa";
   } else {
     alert("Invalid pizza type. Please refresh site and try again");
@@ -62,19 +62,19 @@ if (foodType === "1") {
   }
 
 // Pasta 
-} else if (foodType === "2") {
-  const pastaType = prompt(
+} else if (foodType === 2) {
+  const pastaType = parseInt(prompt(
     `Please select a Pasta dish. 
     Enter a number:  
     1 - Spaghetti Bolognese 
     2 - Penne all’Arrabbiata 
-    3 - Frutti de Mare`)
+    3 - Frutti de Mare`));
 
-  if (pastaType === "1") {
+  if (pastaType === 1) {
     orderedFood = "Spaghetti Bolognese";
-  } else if (pastaType === "2") {
+  } else if (pastaType === 2) {
     orderedFood = "Penne all’Arrabbiata";
-  } else if (pastaType === "3") {
+  } else if (pastaType === 3) {
     orderedFood = "Frutti de Mare";
   } else {
     alert("Invalid pasta type. Please refresh site and try again");
@@ -82,19 +82,19 @@ if (foodType === "1") {
   }
 
   // Salad 
-} else if (foodType === "3") {
-  const saladType = prompt(
+} else if (foodType === 3) {
+  const saladType = parseInt(prompt(
     `Please select a Salad. 
     Enter a number: 
     1 - Caesar Salad
     2 - Salade Niçoise 
-    3 - Waldorf Salad`)
+    3 - Waldorf Salad`));
 
-  if (saladType === "1") {
+  if (saladType === 1) {
     orderedFood = "Caesar Salad";
-  } else if (saladType === "2") {
+  } else if (saladType === 2) {
     orderedFood = "Salade Niçoise";
-  } else if (saladType === "3") {
+  } else if (saladType === 3) {
     orderedFood = "Waldorf Salad";
   } else {
     alert("Invalid salad type. Please refresh site and try again");
@@ -109,26 +109,32 @@ alert(`You've chosen ${orderedFood}!`);
 
 const age = (prompt("Is this food for a child or an adult? Type your age:"));
 
-if (age <= 17) {
-  price = "€10";
-  portionSize = "child-sized";
+if (age) {  
+  if (age <= 17) {
+    price = "€10";
+    portionSize = "child-sized";
+  } else { 
+    price = "€15";
+    portionSize = "adult-sized";
+  } 
 } else {
-  price = "€15";
-  portionSize = "adult-sized";
-} 
+  alert("Invalid age entry. Please refresh site and try again.");
+  exit(1);
+}
+
 
 // Step 5 - Order confirmation
 
-const confirmOrder = prompt(
+const confirmOrder = parseInt(prompt(
   `One ${portionSize} ${orderedFood} will be prepared for you. That'll be ${price}. Are you sure you want to order this? 
   Enter a number to confirm: 
   1 - Yes 
-  2 - No`);
+  2 - No`));
 
-if (confirmOrder === "1") {
+if (confirmOrder === 1) {
   alert("Thank you for your order! Your meal will be prepared."); 
-} else if (confirmOrder === "2") {
+} else if (confirmOrder === 2) {
   alert("No worries, we'll be here if you change your mind.");
 } else {
-  alert("Apologies, but that was neither a 'yes' nor a 'no'. Please try again.")
+  alert("Apologies, but that was neither a 'yes' nor a 'no'. Please try again.");
 }
