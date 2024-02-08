b6ea5d4c97694dd9a5e806e98e398ecf53a21c37
Lina Roos <linaroos@MBPsomtlhorLina.home>
2024-02-08 10:43:14 +0100

    first push, didnt know we where supposed to do often

--- code/script.js
+++ code/script.js
@@ -1,19 +1,47 @@
-// Start here
 
-// Step 1 - Welcome and introduction
-// Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+  `Welcome to Pizzeria Estockholmo. Ready to order? - Click 'OK' to begin.`)
 
-// Step 2 - Food choice
-// Your code goes here
+  let userName = prompt("Please enter your name:")
+alert("Hi, " + userName + "!")
 
-// Step 3 - Subtype choice
-// Your code goes here
+--> byt ut let till const 
 
-// Step 4 - Age
-// Your code goes here
+let userChoice = prompt("Please enter the number corresponding to your choice:\n1. Pizza\n2. Pasta\n3. Salad")
+switch(userChoice) {
+    case "1":
+        alert("You have chosen Pizza!")
+        break;
+    case "2":
+        alert("You have chosen Pasta!")
+        break;
+    case "3":
+        alert("You have chosen Salad!")
+        break;
+    default:
+        alert("Invalid choice. Please enter 1, 2, or 3.")
+}
 
-// Step 5 - Order confirmation
-// Your code goes here
+let customerType = prompt ("Is the order for a child or an adult?")
+
+if (customerType === "child") {
+  alert("Your price will be 5€")
+} else if (customerType ==="adult") {
+  alert("Your price will be 10€")
+} else {
+  alert ("Not valid, please try again")
+}
+
+let order = "You have chosen " + (userChoice === "1" ? "Pizza" : userChoice === "2" ? "Pasta" : "Salad") +
+  " ,your price will be  " + (customerType === "child" ? "5€" : "10€") +
+  ". Please let us know if you want to place this order? Type 'yes' to confirm, 'no' to cancel.";
+
+let confirmOrder = prompt(order);
+
+if (confirmOrder.toLowerCase() === "yes") {
+    alert("Thank you " + userName + "! Your " + (userChoice === "1" ? "Pizza" : userChoice === "2" ? "Pasta" : "Salad") + " will be made.");
+} else if (confirmOrder.toLowerCase() === "no") {
+    alert("Thanks for visiting Pizzeria Estockholmo. See you soon!");
+} else {
+    alert("Please try again.");
+}
