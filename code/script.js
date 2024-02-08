9daaf7661463fdf332a95fcb511b3f1fdf765bd6
Maria-source-cell <manuelabirnat@gmail.com>
2024-02-08 14:14:49 +0100

    step 4, complete

--- code/script.js
+++ code/script.js
@@ -6,81 +6,99 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+const userName = prompt ("Please enter your name")
+
+console.log(userName)
+alert (`Hello ${userName}! Let´s feed you!`)
 // Step 2 - Food choice
 // Your code goes here
 
-const foodChoice = prompt (`Select your favorite dish:
+const foodChoice = parseInt(
+  prompt (`Select your favorite dish:
   1.Burger
   2.Pizza 
   3.Salad
 
-  Please enter the number of your choice:`);
-  
-  console.log(foodChoice);
+  Please enter the number of your choice:`)
+)
+  console.log("food choice", foodChoice, typeof "foodChoice");
 
+let selectedFood = "";
+
+if(foodChoice === 1) {
+  selectedFood = "Burger";
+} else if (foodChoice === 2) {
+  selectedFood = "Pizza";
+} else if (foodChoice === 3) {
+  selectedFood = "Salad";
+} else {
+  alert("Invalid choice, please start again.")
+}
+
+console.log ("selected food", selectedFood);
+
+alert (`You chose ${selectedFood}`);
+
+//exit(1)
 
 // Step 3 - Subtype choice
 // Your code goes here
-let size;
-
-function burgerType() {
-  const burgers = prompt (`Please enter your preference
-  1.Cheeseburger
-  2.Chicken Burger
-  3.Vegan Burger
-
-  Enter a number for your choice:`);
-
-  return burgers;
-}
-
-function pizzaType() {
-  const pizza = prompt (`Please enter your preference
-  1.Peperoni
-  2.Greek Pizza
-  3.New haven Pizza
-
-  Enter a number for your choice:`);
-
-  return pizza;
-}
-
-function saladType() {
-  const salad = prompt (`Please enter your preference
-  1.Crunchy Munchy Salad
-  2.Leafy Green Giggle Salad
-  3.Zesty Zoodle Zinger Salad
-
-  Enter a number for your choice:`);
-
-  return salad;
-}
 
 
-let selectedFood = "";
-if (foodChoice === "1") {
- const burgerTyper = burgerType();
- console.log(burgerTyper);
+let choiceSlot =""
+let foodNames = []
 
-} else if (foodChoice === "2") {
-const pizzaTyper = pizzaType();
-console.log(pizzaTyper);
+switch (selectedFood) {
+  case "Burger":
+    choiceSlot = parseInt (
+      prompt(`Please enter your burger preference 
+        1.Cheeseburger
+        2.Chicken Burger
+        3.Vegan Burger 
+        Please chose a number:`)
+    )
+      foodNames = ["Cheesburger", "Chicken Burger", "Vegan Burger"]
+     break
 
-} else if (foodChoice === "3") {
- const saladTyper = saladType();
- console.log(saladTyper);
+  case "Pizza":
+   choiceSlot = parseInt (
+    prompt(`Please enter your pizza preference
+      1.Peperoni Pizza
+      2.Greek Pizza
+      3.New Haven Pizza
+      Please chose a number:`)
+  )
+    foodNames = ["Peperoni Pizza", "Greek Pizza", "New Haven Pizza"]
+    break
 
-} else {
- alert ("Invalid choice, please start again.");
- exit(1);
- alert (`You chose ${selectedFood}`)
-}
+    case "Salad":
+    choiceSlot = parseInt (
+      prompt(`Please enter your salad preference
+      1. Crunchy Munchy Salad
+      2. Leafy Green Salad
+      3. Zesty Zoodle Zinger Salad
+          Please chose a number:`)
+      )
+      foodNames = ["Crunchy Munchy Salad", "Leafy Green Salad", "Zezty Zoodle Zinger Salad"]
+      break
+      default:
+      alert("You chose ${foodNames}")
+      exit(1)
+    }
+
+console.log ("choiceSlot", choiceSlot)
 
 
 // Step 4 - Age
 // Your code goes here
 
-const age = prompt("Find out your size by typing your age:");
+//Define age
+let age = parseInt(prompt("Find out your size by typing your age:"));
+
+//Declare size variable
+let size;
+
+//Determine size based on age
 
   if (age < 18) {
     size = "Small"
@@ -88,36 +106,25 @@ const age = prompt("Find out your size by typing your age:");
     size = "Large";
   }
 
+  //Define burger variable
   let burgerName;
-  switch (burgers) {
-    case "1":
+
+  //Switch statement to determine burger name
+  switch (choiceSlot) {
+    case 1:
       burgerName = "Cheesburger";
       break;
-    case "2":
+    case 2:
       burgerName =  "Chicken Burger";
       break;
-    case "3":
+    case 3:
       burgerName = "Vegan Burger";
-      break;
-    default:
-      burgerName = "Unknown Burger";
       break;
   }
 
-  return { burger: burgerName, size: size };
-
-const { burger, size } = burgerType();;
-console.log(`You chose a ${size} ${burger}`)
+ alert (`You chose a ${size} ${burgerName}`);
 
 
-
-
- /*return { burger: "Cheeseburger", size: size };
-} else {
-  return { burger: "Other", size: null };
-}
-
-console.log(`You chose a ${size} ${burger}`);
 
 // Step 5 - Order confirmation
-// Your code goes here
+// Your code goes here*/
