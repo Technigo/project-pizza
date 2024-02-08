3bda440eb08c9ea3e8ed819b782c51ba3707a1b2
Lina Roos <linaroos@MBPsomtlhorLina.home>
2024-02-08 17:30:09 +0100

    added last code

--- code/script.js
+++ code/script.js
@@ -1,15 +1,12 @@
-
-console.log("Pizzeria order")
-
 alert(
-  "Welcome to Pizzeria. Ready to order? - Click 'OK' to begin.")
+  "Welcome to Pizzeria Penguen. Ready to order? - Click 'OK' to begin.")
 
+//user name//
 const userName = prompt("Please enter your name:")
-
 console.log(userName)
-
 alert (`Hi ${userName}! Get ready to order! `)
 
+//User selection of food//
 const userChoice = parseInt(
   prompt(`
 Select your choice:
@@ -19,7 +16,6 @@ Select your choice:
 Please enter the number of your choice
 `)
 )
-
 console.log("user choice", userChoice, typeof userChoice)
 
 let selectedMeal = ""
@@ -35,6 +31,33 @@ if (userChoice === 1) {
 }
 
 console.log("selected meal", selectedMeal)
-alert(`You chose ${selectedMeal}`)
+alert(`You chose ${selectedMeal} ,exellent choice!`)
 
+//user selection of age//
+let customerType = prompt ("Is the order for a child or an adult?")
+
+if (customerType === "child") {
+  alert("Your price will be 5€")
+} else if (customerType ==="adult") {
+  alert("Your price will be 10€")
+} else {
+  alert ("Not valid, please try again")
+}
+
+//order confirmation//
+let orderConfirmation = "";
+
+if (customerType === "child" || customerType === "adult") {
+  orderConfirmation = prompt(`You have ordered ${selectedMeal} for a ${customerType}. Your total price is ${customerType === "child" ? "5€" : "10€"}. Would you like to confirm your order? Yes or No`);
+} else {
+  alert("Invalid customer type. Please restart your order.");
+}
+
+if (orderConfirmation.toLowerCase() === "yes") {
+  alert(`Thank you, ${userName}! Your order for ${selectedMeal} has been confirmed.`);
+} else if (orderConfirmation.toLowerCase() === "no") {
+  alert("No problem, your order has been canceled. Welcome back any time!");
+} else {
+  alert("Invalid response. Please restart your order.");
+}
 
