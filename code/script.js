3d41c414445266120ca13ed9738ba0b97bdd45bd
Your Name <you@example.com>
2024-02-08 21:59:50 +0100

    Fixed bugs and saving script.js

--- code/script.js
+++ code/script.js
@@ -15,138 +15,163 @@ I tryied to create an array but then I couldn't figure out how I could use it:
 
 // Step 1 - Welcome and introduction
 
-alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`)
+alert (`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`)
 userName = prompt(`What's your name?`)
-alert(`Hi ${userName}! What a preatty name you have!
+alert (`Hi ${userName}! What a preatty name you have!
 Welcome into our pizzeria,
 
 Press Ok to continue.`)
 
 // Step 2 - Food choice
-mainDishNumber = parseInt(prompt(`What are you going to eat? You have 3 options:
- 1 - Pizza 🍕
- 2 - Pasta 🍝
- 3 - Salad 🥗
- (answer by typing 1, 2 or 3)`))
 
-//"connect" mainDishNumber to mainDish
+mainDishNumber = parseInt(prompt(`What are you going to eat? You have 3 options:
+
+  1 - Pizza 🍕
+  2 - Pasta 🍝
+  3 - Salad 🥗
+
+(answer by typing 1, 2 or 3)`))
 
 if (mainDishNumber===1){
   mainDish = "pizza"
   alert (`Pizza 🍕 Youmy! 
   
-  Press OK to continue.`)}
-
-  else if (mainDishNumber===2){ 
+  Press OK to continue.`)
+} else if (mainDishNumber===2) { 
   mainDish = "pasta"
   alert (`The HGI option:D Pasta!! 🍝
 
-  Press OK to continue.`)}
-
-  else if ( mainDishNumber===3){ 
+  Press OK to continue.`)
+} else if ( mainDishNumber===3) { 
   mainDish = "salad"
   alert (`Salad 🥗, the healthy choise!
  
-  Press OK to continue.`)}
-
-  else {alert (`Make sure you are typing one the options available.)
+  Press OK to continue.`)
+} else {
+  alert (`Make sure you are typing one the options available.)
  
   You have to start again.`)
   exit(1)}
 
-console.log(typeof mainDish)
-
+/* Some testing in the console here: console.log(typeof mainDish) */
 // Step 3 - Subtype choice
-if (mainDish===1){
-    subtypeDishNumber = prompt(`Which pizza do you want?
 
-    1 - Margherita
-    2 - Cheese 
-    3 - Caprichosa
-    4 - Pepperoni
-    5 - Meatballs
-    
-    Type the number`)
-  } else if (mainDishNumber===2){   
-   subtypeDishNumber = prompt(`Which kind of pasta do you want?
+if (mainDishNumber===1) {
+  subtypeDishNumber = parseInt(prompt(`Which pizza do you want?
 
-    1 - Spaghetti Carbonara
-    2 - Mac & Cheese
-    3 - Pesto
-    4 - Spaghetti and Meatballs
-    5 - Beef & Cheese Lasagna
-    
-    Type the number`)
-  } else if (mainDishNumber===3) { 
-  subtypeDishNumber = parseInt(prompt(`What kind of sallad do you want?
+  1 - Margherita
+  2 - Cheese 
+  3 - Caprichosa
+  4 - Pepperoni
+  5 - Meatballs
+  
+  Type the number`))
+} else if (mainDishNumber===2) {   
+  subtypeDishNumber = parseInt(prompt(`Which kind of pasta do you want?
 
-   1 - Mediterranian
-   2 - Cesar 
-   3 - Vegana
-   4 - Salmon
-   5 - Chèvre
-   
-   Type the number`))
-  } else {
-  alert (`Make sure you are typing one the options available.)
+  1 - Spaghetti Carbonara
+  2 - Mac & Cheese
+  3 - Pesto
+  4 - Spaghetti and Meatballs
+  5 - Beef & Cheese Lasagna
+  
+  Type the number`))
+} else if (mainDishNumber===3) { 
+subtypeDishNumber = parseInt(prompt(`What kind of sallad do you want?
 
-  You have to start again.`)
-  exit(1)}
+  1 - Mediterranian
+  2 - Cesar 
+  3 - Vegana
+  4 - Salmon
+  5 - Chèvre
+  
+  Type the number`))
+} else {
+alert (`Make sure you are typing one the options available.)
 
-/* This was some trial and error 
+You have to start again.`)
+exit(1)}
+
+// This was some testing in the console
 console.log("mainDish", mainDishNumber)
 console.log(typeof mainDishNumber)
 console.log("subtypeDish",subtypeDishNumber)
-console.log(typeof subtypeDishNumber) */
+console.log(typeof subtypeDishNumber)
 
 
 if (mainDishNumber === 1 && subtypeDishNumber === 1) {
-    subtypeDish = "Margherita"; 
-  } else if (mainDishNumber === 1 && subtypeDishNumber === 2) {
-    subtypeDish = "Cheese";
-  } else if (mainDishNumber === 1 && subtypeDishNumber === 3) {
-    subtypeDish = "Caprichosa";
-  } else if (mainDishNumber === 1 && subtypeDishNumber === 4) {
-    subtypeDish = "Pepperoni";
-  } else if (mainDishNumber === 1 && subtypeDishNumber === 5) {
-    subtypeDish = "Meatballs";
-  } else if (mainDishNumber === 2 && subtypeDishNumber === 1) {
-    subtypeDish = "Spaghetti Carbonara";
-  } else if (mainDishNumber === 2 && subtypeDishNumber === 2) {
-    subtypeDish = "Mac & Cheese";
-  } else if (mainDishNumber === 2 && subtypeDishNumber === 3) {
-    subtypeDish = "Pesto";
-  } else if (mainDishNumber === 2 && subtypeDishNumber === 4) {
-    subtypeDish = "Spaghetti and Meatballs";
-  } else if (mainDishNumber === 2 && subtypeDishNumber === 5) {
-    subtypeDish = "Beef & Cheese Lasagna";
-  } else if (mainDishNumber === 3 && subtypeDishNumber === 1) {
-    subtypeDish = "Mediterranian";
-  } else if (mainDishNumber === 3 && subtypeDishNumber === 2) {
-    subtypeDish = "Cesar";
-  } else if (mainDishNumber === 3 && subtypeDishNumber === 3) {
-    subtypeDish = "Vegana";
-  } else if (mainDishNumber === 3 && subtypeDishNumber === 4) {
-    subtypeDish = "Salmon";
-  } else if (mainDishNumber === 3 && subtypeDishNumber === 5) {
-    subtypeDish = "Chèvre";
-  } else {}
+  subtypeDish = "Margherita"; 
+} else if (mainDishNumber === 1 && subtypeDishNumber === 2) {
+  subtypeDish = "Cheese";
+} else if (mainDishNumber === 1 && subtypeDishNumber === 3) {
+  subtypeDish = "Caprichosa";
+} else if (mainDishNumber === 1 && subtypeDishNumber === 4) {
+  subtypeDish = "Pepperoni";
+} else if (mainDishNumber === 1 && subtypeDishNumber === 5) {
+  subtypeDish = "Meatballs";
+} else if (mainDishNumber === 2 && subtypeDishNumber === 1) {
+  subtypeDish = "Spaghetti Carbonara";
+} else if (mainDishNumber === 2 && subtypeDishNumber === 2) {
+  subtypeDish = "Mac & Cheese";
+} else if (mainDishNumber === 2 && subtypeDishNumber === 3) {
+  subtypeDish = "Pesto";
+} else if (mainDishNumber === 2 && subtypeDishNumber === 4) {
+  subtypeDish = "Spaghetti and Meatballs";
+} else if (mainDishNumber === 2 && subtypeDishNumber === 5) {
+  subtypeDish = "Beef & Cheese Lasagna";
+} else if (mainDishNumber === 3 && subtypeDishNumber === 1) {
+  subtypeDish = "Mediterranian";
+} else if (mainDishNumber === 3 && subtypeDishNumber === 2) {
+  subtypeDish = "Cesar";
+} else if (mainDishNumber === 3 && subtypeDishNumber === 3) {
+  subtypeDish = "Vegana";
+} else if (mainDishNumber === 3 && subtypeDishNumber === 4) {
+  subtypeDish = "Salmon";
+} else if (mainDishNumber === 3 && subtypeDishNumber === 5) {
+  subtypeDish = "Chèvre";
+} else {
+  alert (`Make sure you are typing one the options available.)
+  
+  You have to start again.`)
+  exit(1)}
 
 alert (`Your order is ${subtypeDish} ${mainDish}!
 
-We will make sure you enjoy your meal`)
+OK to continue.`)
 
 
 // Step 4 - Age
 let age = parseInt (prompt(`How old are you?`))
+let ageSize 
 
-if (age >= 15) {
-  alert (`You will get an adult size ${subtypeDish}.`)
-} else if (age < 15){
-  alert(`You will get a child size ${subtypeDish}.`)
-} else {
+if (age >= 15) { ageSize = ("ADULT SIZE")
+  alert (`You will get an ADULT SIZE ${subtypeDish} ${mainDish}.`)
+} else if (age < 15){ageSize = ("CHILD SIZE")
+  alert(`You will get cheaper CHILD SIZE ${subtypeDish} ${mainDish}.`)
+} else { 
   alert (`this is an invalid input, you will need to start again`)
 exit(1)}
 
 // Step 5 - Order confirmation
-// Your code goes here
+orderConfirmation = prompt (`To accept your delicious ${subtypeDish} ${mainDish} - ${ageSize}
+
+  Press 1 - to confirm
+
+  Press 0 - to Cancel`
+  
+  )
+
+if (orderConfirmation === "1") {
+  (alert (`Your order is in the way. 
+  
+We will make sure you enjoy your meal😋
+
+Thank you! We hope to see you again soon, have a nice day`))
+} else if (orderConfirmation === "0"){
+  alert (`Order Canceled, start again if you would like to order some thing else.`)
+} else {
+  alert (`This options isn't available.
+
+  We are sorry but. You will have to start again.`)
+  exit(1)}
+
