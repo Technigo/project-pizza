8079582635f789d9e2ca4002da21eb2905be348a
Maria-source-cell <manuelabirnat@gmail.com>
2024-02-09 19:42:02 +0100

    done

--- code/script.js
+++ code/script.js
@@ -9,7 +9,9 @@ alert(
 const userName = prompt ("Please enter your name")
 
 console.log(userName)
-alert (`Hello ${userName}! Let´s feed you!`)
+
+//Display the welcome message after getting the user´s name
+alert (`Hello ${userName}! Let´s feed you!`);
 
 // Step 2 - Food choice
 // Your code goes here
@@ -17,13 +19,14 @@ alert (`Hello ${userName}! Let´s feed you!`)
 const foodChoice = parseInt(
   prompt (`Select your favorite dish:
 
-  1.Burger
+  1.Burger 
   2.Pizza 
   3.Salad
 
   Please enter the number of your choice:`)
 )
-  console.log("food choice", foodChoice, typeof "foodChoice");
+
+console.log("food choice", foodChoice, typeof "foodChoice");
 
 let selectedFood = "";
 
@@ -46,8 +49,8 @@ alert (`You choose ${selectedFood}`);
 // Your code goes here
 
 
-let choiceSlot =""
-let foodNames = []
+let choiceSlot ="";
+let foodNames = [];
 
 switch (selectedFood) {
   case "Burger":
@@ -58,20 +61,20 @@ switch (selectedFood) {
         2.Chicken Burger
         3.Vegan Burger 
 
-        Please choose a number:`)
+        Please choose a number`)
     )
       foodNames = ["Cheesburger", "Chicken Burger", "Vegan Burger"]
      break
 
   case "Pizza":
    choiceSlot = parseInt (
-    prompt(`Enter your pizza preference
+    prompt(`Enter your pizza preference:
 
       1.Peperoni Pizza
       2.Greek Pizza
       3.New Haven Pizza
 
-      Please choose a number:`)
+      Please choose a number`)
   )
     foodNames = ["Peperoni Pizza", "Greek Pizza", "New Haven Pizza"]
     break
@@ -84,15 +87,16 @@ switch (selectedFood) {
       2. Leafy Green Salad
       3. Zesty Zoodle Zinger Salad
 
-          Please choose a number:`)
+      Please choose a number:`)
       )
+
       foodNames = ["Crunchy Munchy Salad", "Leafy Green Salad", "Zezty Zoodle Zinger Salad"]
       break
 
-      default:
+  default:
       alert("That item is not in our list. Please try again. :)")
       exit(1)
-    }
+}
 
 console.log ("choiceSlot", choiceSlot)
 
@@ -101,11 +105,10 @@ console.log ("choiceSlot", choiceSlot)
 // Your code goes here
 
 //Define age
+
 let age = parseInt(prompt("Find out your size by typing your age:"));
 let size = age < 18 ? "Small" : "Large";
 
-//Declare size variable
-//let size;
 
 //Determine size based on age
 
@@ -135,7 +138,7 @@ let size = age < 18 ? "Small" : "Large";
         exit(1)
   }
 
- alert (`You chose a ${size} ${foodItem}`);
+ alert (`Thnak you for your oderr.You ordered a ${size} ${foodItem}`);
 
 /*let pizzaName;
 switch (choiceSlot) {
@@ -160,3 +163,4 @@ Your food is beeing prepared by humans.
 Now you can relax while the food is being prepared.
 
 Enjooooy!🤗 `)
+
