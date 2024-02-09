29f29d4b5cc03ba3685ca2708348c1473a1611f8
SofieFerrari <sofiestrahl@gmail.com>
2024-02-09 11:02:15 +0100

    working on the switch.js

--- code/switch.js
+++ code/switch.js
@@ -1,137 +1,166 @@
-// Step 1 - Welcome and introduction
+// Welcome and introduction
 alert(
     `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
   );
   
-  const userName = prompt("Please enter your name", "Hungry Hugo");
+  const userName = prompt(
+    "Please enter your name", "Hungry Hugo"
+  );
   console.log("user name", userName)
   
   alert(
     `Hello ${userName}! Are you ready to order som yummy food?`
   );
   
-  // Step 2 - Food choice
-  let menu = prompt(`What would you like to order
+  // Food choice
+  let foodChoice = parseInt(
+    prompt(
+      `What would you like to order
+
   1 = Pizza
   2 = Pasta
   3 = Salad
   
-  Type the number of your choice here please!`);
+  Type the number of your choice here please!`
+  )
+  );
   
-  let foodName;
-  
-  if (menu === "1") {
-    foodName = "Pizza";
-  } else if (menu === "2"){
-    foodName = "Pasta";
-  } else if (menu === "3") {
-    foodName = "Salad"
-  } else {
+  switch(foodChoice) {
+    case 1:
+      foodChoice = "Pizza";
+      alert(`${foodChoice}, what a great choice!`)
+      break;
+    case 2:
+      foodChoice = "Pasta";
+      alert(`${foodChoice}, what a great choice!`)
+      break;
+    case 3:
+      foodChoice = "Salad"
+      alert(`${foodChoice}, what a great choice!`)
+      break;
+    default: 
     alert("We're sorry, we don't have that, please start over!")
     exit(1);
   }
-  console.log("food name", foodName)
-  
-  alert(`${foodName}! What a lovely choice!`)
-  
+  console.log("first choice", foodChoice)
   
   // Step 3 - Subtype choice
-  let subChoice;
-  
-  if (foodName === "Pizza") {
-   subChoice = prompt(`Select the number of your type of pizza! 
-   1 = Vegetarian
-   2 = Salami
-   3 = Calzone`)
-  } else if (foodName === "Pasta") {
-    subChoice = prompt(`Select the number of your type of pasta!
-    1 = Vegetarian
-    2 = Meat
-    3 = Seafood`)
-  } else if (foodName === "Salad") {
-    subChoice = prompt(`Select the number of your type of salad! 
-    1 = Vegetarian
-    2 = Cheese
-    3 = Shrimp`)
-  } else {
-    alert("We're sorry, we don't have that, please start over!");
-  }
-  
-  let lastChoice;
-  
-  if (foodName === "Pizza"){
-    if (subChoice === "1") {
-      lastChoice = "Vegetarian pizza"
-      alert(`You chose the ${lastChoice}!`)
-    } else if (subChoice === "2") {
-      lastChoice = "Salami pizza"
-      alert(`You chose the ${lastChoice}!`)
-    } else if (subChoice === "3") {
-      lastChoice = "Calzone"
-      alert(`You chose ${lastChoice}!`)
-    } else {
-        alert("We're sorry, we don't have that, please start over!");
+  const pizzaSubchoice = [1 = "Vegetarian", 2= "Salami", 3= "Calzone"];
+  const pastaSubchoice = ["Vegetarian", "Meat", "Seafood"];
+  const saladSubchoice = ["Vegetarian", "Cheese", "Shrimp"];
+  let foodSubchoice = [""];
+
+  switch(foodChoice) {
+    case "Pizza":
+      foodSubchoice = pizzaSubchoice;
+      prompt(`What kind of ${foodChoice} would you like to order? ${pizzaSubchoice} `)
+      break;
+    case "Pasta":
+      foodSubchoice = pastaSubchoice;
+      break;
+    case "Salad":
+      foodSubchoice = saladSubchoice;
+      break;
+    default: 
+      alert("We're sorry, we don't have that, please start over!")
+      exit(1);
     }
-  }
-  if (foodName === "Pasta"){
-    if (subChoice === "1") {
-      lastChoice = "Vegetarian pasta"
-      alert(`You chose the ${lastChoice}!`)
-    } else if (subChoice === "2") {
-      lastChoice = "pasta with meat"
-      alert(`You chose the ${lastChoice}!`)
-    } else if (subChoice === "3") {
-      lastChoice = "Seafood pasta"
-      alert(`You chose ${lastChoice}!`)
-    } else {
-        alert("We're sorry, we don't have that, please start over!");
-    }
-  }
+
   
-  if (foodName === "Salad"){
-    if (subChoice === "1") {
-      lastChoice = "Vegetarian salad"
-      alert(`You chose the ${lastChoice}!`)
-    } else if (subChoice === "2") {
-      lastChoice = "Cheese salad"
-      alert(`You chose the ${lastChoice}!`)
-    } else if (subChoice === "3") {
-      lastChoice = "Shrimp salad"
-      alert(`You chose ${lastChoice}!`)
-    } else {
-        alert("We're sorry, we don't have that, please start over!");
-    }
-  }
-  console.log("last choice", lastChoice)
+  // if (foodName === "Pizza") {
+  //  subChoice = prompt(`Select the number of your type of pizza! 
+  //  1 = Vegetarian
+  //  2 = Salami
+  //  3 = Calzone`)
+  // } else if (foodName === "Pasta") {
+  //   subChoice = prompt(`Select the number of your type of pasta!
+  //   1 = Vegetarian
+  //   2 = Meat
+  //   3 = Seafood`)
+  // } else if (foodName === "Salad") {
+  //   subChoice = prompt(`Select the number of your type of salad! 
+  //   1 = Vegetarian
+  //   2 = Cheese
+  //   3 = Shrimp`)
+  // } else {
+  //   alert("We're sorry, we don't have that, please start over!");
+  // }
   
-  // Step 4 - Age // Step 5 - Order confirmation
+  // let lastChoice;
   
-  let age = prompt("Is the food for a child or an adult? Please enter your age?")
-  let confirmation;
+  // if (foodName === "Pizza"){
+  //   if (subChoice === "1") {
+  //     lastChoice = "Vegetarian pizza"
+  //     alert(`You chose the ${lastChoice}!`)
+  //   } else if (subChoice === "2") {
+  //     lastChoice = "Salami pizza"
+  //     alert(`You chose the ${lastChoice}!`)
+  //   } else if (subChoice === "3") {
+  //     lastChoice = "Calzone"
+  //     alert(`You chose ${lastChoice}!`)
+  //   } else {
+  //       alert("We're sorry, we don't have that, please start over!");
+  //   }
+  // }
+  // if (foodName === "Pasta"){
+  //   if (subChoice === "1") {
+  //     lastChoice = "Vegetarian pasta"
+  //     alert(`You chose the ${lastChoice}!`)
+  //   } else if (subChoice === "2") {
+  //     lastChoice = "pasta with meat"
+  //     alert(`You chose the ${lastChoice}!`)
+  //   } else if (subChoice === "3") {
+  //     lastChoice = "Seafood pasta"
+  //     alert(`You chose ${lastChoice}!`)
+  //   } else {
+  //       alert("We're sorry, we don't have that, please start over!");
+  //   }
+  // }
   
-  if (age >= 13) {
-    confirmation = prompt(`One adult size ${lastChoice} will be prepared for you and that will be 15$!
-    Do you want to confirm your order?
-    1 - Yes
-    2 - No
-    `)
-  } else if (age <= 13) {
-    confirmation = prompt(`One child size ${lastChoice} will be prepared for you and that will be 10$!
-    Do you want to confirm your order?
-    1 - Yes
-    2 - No
-    `)
-  } else {
-    prompt ("Invalid choice, please start over!")
-  }
-  console.log("age", age)
+  // if (foodName === "Salad"){
+  //   if (subChoice === "1") {
+  //     lastChoice = "Vegetarian salad"
+  //     alert(`You chose the ${lastChoice}!`)
+  //   } else if (subChoice === "2") {
+  //     lastChoice = "Cheese salad"
+  //     alert(`You chose the ${lastChoice}!`)
+  //   } else if (subChoice === "3") {
+  //     lastChoice = "Shrimp salad"
+  //     alert(`You chose ${lastChoice}!`)
+  //   } else {
+  //       alert("We're sorry, we don't have that, please start over!");
+  //   }
+  // }
+  // console.log("last choice", lastChoice)
+  
+  // // Step 4 - Age // Step 5 - Order confirmation
+  
+  // let age = prompt("Is the food for a child or an adult? Please enter your age?")
+  // let confirmation;
+  
+  // if (age >= 13) {
+  //   confirmation = prompt(`One adult size ${lastChoice} will be prepared for you and that will be 15$!
+  //   Do you want to confirm your order?
+  //   1 - Yes
+  //   2 - No
+  //   `)
+  // } else if (age <= 13) {
+  //   confirmation = prompt(`One child size ${lastChoice} will be prepared for you and that will be 10$!
+  //   Do you want to confirm your order?
+  //   1 - Yes
+  //   2 - No
+  //   `)
+  // } else {
+  //   prompt ("Invalid choice, please start over!")
+  // }
+  // console.log("age", age)
   
   
-  if (confirmation === "1") {
-    alert(`Your ${lastChoice} is being prepared! Thank you for ordering food from the Technigo Pizzeria!`)
-  } else if (confirmation === "2") {
-    alert(`Ok! Please come back for some yummy food some other time! Have a great day!`)
-  } else {
-    exit(1)
-  }
+  // if (confirmation === "1") {
+  //   alert(`Your ${lastChoice} is being prepared! Thank you for ordering food from the Technigo Pizzeria!`)
+  // } else if (confirmation === "2") {
+  //   alert(`Ok! Please come back for some yummy food some other time! Have a great day!`)
+  // } else {
+  //   exit(1)
+  // }
   
