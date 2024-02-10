03dfdd39933b58f87d22d6bc5c1ae98a8c1d96ad
schouenkes <m.schouenke@gmail.com>
2024-02-10 20:32:24 +0100

    added a switch.js file and Step 1

---
+++ code/switch.js
@@ -0,0 +1,12 @@
+// Step 1 - Welcome and introduction
+
+alert(`Welcome to our Virtual Pizzeria. Ready to order? - Click 'OK' to begin.`);
+
+//A statement which variable value will be the name the user enters.
+const name = prompt(`Please enter your name:`);
+
+//will log the users name in the console.
+console.log(`Customer: ${name}`);
+
+//this will return an alert message with the name that the user typed in.
+alert(`Hi ${name}, it's nice to see you here!`);
