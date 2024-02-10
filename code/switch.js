d8c20c31a106fd274f145a42163cad94e01920ab
schouenkes <m.schouenke@gmail.com>
2024-02-10 22:50:46 +0100

    added step 4 in switch.js - an age switch statement

--- code/switch.js
+++ code/switch.js
@@ -160,3 +160,51 @@ switch (foodSelected) {
 }
 
 alert(`Good choice, you chosed to order a ${pizzaSelected || hamburgerSelected || saladSelected}`);
+
+// Step 4 - Age
+
+const age = parseInt(
+   prompt(
+      `Is this meal for a child or an adult? 
+       Type your age:`
+   )
+);
+
+console.log(`${age} years`);
+
+//The switch expression compares strictly to the expression in each case it can't be <= or >=, so to be able to decide whether it is a child or an adult sized meal I set the expression as true and then it checks if the input value matched strictly to the case of the case expression.
+
+switch (true) {
+   case age >= 15:
+      alert(
+         `One adult sized ${pizzaSelected || hamburgerSelected || saladSelected} will be prepared for you.
+         That will be €10`
+      );
+      break;
+
+   case age <= 14:
+      alert(
+         `One child sized ${pizzaSelected || hamburgerSelected || saladSelected} will be prepared for you.
+         That will be €8`
+      );
+      break;
+
+   default:
+      alert(
+         `Oops, we didn't get your age!
+        We need to know your age to prepare an appropiate meal size. 
+        Please start over and remember to fill in all of the input fields`
+      );
+      exit(1);
+}
+
+const orderConfirmation = parseInt(
+   prompt(
+      `Do you want to place the order?
+       Enter the number that represents your choice:
+       1 - Yes
+       2 - No`
+   )
+);
+
+console.log(`Want to finish the order (1=Yes 2=No): ${orderConfirmation}`);
