f0d5f888acd58ba24a144fdccf615a1bf300c061
AHPIXI <148851757+AHPIXI@users.noreply.github.com>
2024-02-11 12:51:31 +0100

    added subtype for the rest of food choices

--- code/script.js
+++ code/script.js
@@ -46,17 +46,59 @@ let foodName = []
 
 switch (selectedFood) {
   case "Pizza":
-    subFoodtype = parseInt(prompt
-      `Select what pasta you want 
-   1 - Aribiata
-   2 - Carbonara
-   3 - Bolgonese
+    subFoodtype = parseInt
+      (prompt(
+        `Select what pasta you want 
+   1 - Margarita
+   2 - Vesuvio
+   3 - Quattro formaggi
    Please enter the number of your choice:
    `
-    )
+      )
+      )
+    foodName = ["Margarita", "Vesuvio", "Quattro formaggi"]
+    break
+
+  case "Pasta":
+    subFoodtype = parseInt
+      (prompt(
+        `Select what pasta you want 
+ 1 - Aribiata
+ 2 - Carbonara
+ 3 - Bolgonese
+ Please enter the number of your choice:
+ `
+      )
+      )
+    foodName = ["Aribiata", "Carbonara", "Bolognese"]
+    break
+
+  case "Salad":
+    subFoodtype = parseInt
+      (prompt(
+        `Select what pasta you want 
+ 1 - Chicken salad
+ 2 - Greek salad
+ 3 - Salomon salad
+ Please enter the number of your choice:
+ `
+      )
+      )
+    foodName = ["Chicken salad", "Greek salad", "Salomon salad"]
+    break
+
+  default:
+    alert("Sorry - you need to make a choice in order to continue. Please start over.")
 }
+
 // Step 4 - Age
 // Your code goes here
 
 // Step 5 - Order confirmation
 // Your code goes here
+console.log("food name", foodName)
+
+if (foodName === 1) {
+  alert
+    `Excellent choise! You have ordered ${foodName[0]} and it will be ready in 10-15 minutes. Welcome!`
+}
