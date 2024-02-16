f21cd3a4724eb9983a339fbbd51b1e0110c4e32c
Tejpex <equis_angel@hotmail.com>
2024-02-16 09:57:36 +0100

    Added loop to menu-item choice and removed unnecessary console.logs

--- code/script.js
+++ code/script.js
@@ -5,7 +5,6 @@ alert(
 
 // Ask for name
 let userName = prompt(`What is your name?`);
-console.log("Name:", userName);
 
 //Welcome with name
 alert(`Welcome ${userName}! Let's order some food`);
@@ -20,7 +19,6 @@ while (foodChoice === "apple") {
     `What do you want to eat? 
   Choose between pizza, burger or salad and write your answer.`
   );
-  console.log("Answer:", foodAnswer);
 
   switch (foodAnswer.toLowerCase()) {
     case "pizza":
@@ -36,8 +34,6 @@ while (foodChoice === "apple") {
       alert(`I'm sorry. We don't have ${foodAnswer}. Choose again!`);
   }
 }
-
-console.log("Choice:", foodChoice);
 
 // Step 3 - Subtype choice
 
@@ -102,83 +98,73 @@ let menuAnswer = null;
 let menuChoice = null;
 let menuPrice = null;
 
-//Choose within pizza menu
-if (foodChoice === "pizza") {
-  menuAnswer = parseInt(
-    prompt(
-      `What type of pizza do you want? Write the number on the menu: 
-    1. Margerita (tomato, cheese) 100:-
-    2. Al Funghi (tomato, cheese, mushrooms) 110:-
-    3. Vegetariana (tomato, cheese, onion, bell pepper, mushrooms, olives, artichoke) 120:-
-    4. Chicken No Chicken (tomato, cheese, banana, curry, peanuts) 110:-
-    (Children's size -10:-)`
-    )
-  );
-  console.log("Answer:", menuAnswer);
-  //Log chosen item and price
-  if (menuAnswer >= 1 && menuAnswer <= 4) {
-    menuChoice = pizzaMenu[menuAnswer - 1]["item"];
-    console.log("Option:", menuChoice);
-    menuPrice = pizzaMenu[menuAnswer - 1]["price"];
-    console.log("Price:", menuPrice);
-  } else {
-    alert(
-      `I'm sorry. Number ${menuAnswer} is not an option. Please start over.`
+while (menuChoice === null) {
+  //Choose within pizza menu
+  if (foodChoice === "pizza") {
+    menuAnswer = parseInt(
+      prompt(
+        `What type of pizza do you want? Write the number on the menu: 
+      1. Margerita (tomato, cheese) 100:-
+      2. Al Funghi (tomato, cheese, mushrooms) 110:-
+      3. Vegetariana (tomato, cheese, onion, bell pepper, mushrooms, olives, artichoke) 120:-
+      4. Chicken No Chicken (tomato, cheese, banana, curry, peanuts) 110:-
+      (Children's size -10:-)`
+      )
     );
-    exit(1);
-  }
+    //Save chosen item and price
+    if (menuAnswer >= 1 && menuAnswer <= 4) {
+      menuChoice = pizzaMenu[menuAnswer - 1]["item"];
+      menuPrice = pizzaMenu[menuAnswer - 1]["price"];
+    } else {
+      alert(
+        `I'm sorry. Number ${menuAnswer} is not an option. Please start over.`
+      );
+    }
 
-  //Choose within burger menu
-} else if (foodChoice === "burger") {
-  menuAnswer = parseInt(
-    prompt(
-      `Here is our burger menu:
-    1. Small burger 70:-
-    2. Big burger 100:-
-    3. Small cheeseburger 80:-
-    4. Big cheeseburger 110:-
-  Write the number of your choice of burger.
-  (Children's size -10:-)`
-    )
-  );
-  console.log("Answer:", menuAnswer);
-  //Log chosen item and price
-  if (menuAnswer >= 1 && menuAnswer <= 4) {
-    menuChoice = burgerMenu[menuAnswer - 1]["item"];
-    console.log("Option:", menuChoice);
-    menuPrice = burgerMenu[menuAnswer - 1]["price"];
-    console.log("Price:", menuPrice);
-  } else {
-    alert(
-      `I'm sorry. Number ${menuAnswer} is not an option. Please start over.`
-    );
-    exit(1);
-  }
-
-  //Choose within salad menu
-} else {
-  menuAnswer = parseInt(
-    prompt(
-      `What type of salad do you want? Write the number on the menu:
-    1. Greek salad 90:-
-    2. Ceasar salad 90:-
-    3. Pasta salad 90:-
-    4. Avocado salad 100:-
+    //Choose within burger menu
+  } else if (foodChoice === "burger") {
+    menuAnswer = parseInt(
+      prompt(
+        `Here is our burger menu:
+      1. Small burger 70:-
+      2. Big burger 100:-
+      3. Small cheeseburger 80:-
+      4. Big cheeseburger 110:-
+    Write the number of your choice of burger.
     (Children's size -10:-)`
-    )
-  );
-  console.log("Answer:", menuAnswer);
-  //Log chosen item and price
-  if (menuAnswer >= 1 && menuAnswer <= 4) {
-    menuChoice = saladMenu[menuAnswer - 1]["item"];
-    console.log("Option:", menuChoice);
-    menuPrice = saladMenu[menuAnswer - 1]["price"];
-    console.log("Price:", menuPrice);
-  } else {
-    alert(
-      `I'm sorry. Number ${menuAnswer} is not an option. Please start over.`
+      )
     );
-    exit(1);
+    //Save chosen item and price
+    if (menuAnswer >= 1 && menuAnswer <= 4) {
+      menuChoice = burgerMenu[menuAnswer - 1]["item"];
+      menuPrice = burgerMenu[menuAnswer - 1]["price"];
+    } else {
+      alert(
+        `I'm sorry. Number ${menuAnswer} is not an option. Please start over.`
+      );
+    }
+
+    //Choose within salad menu
+  } else {
+    menuAnswer = parseInt(
+      prompt(
+        `What type of salad do you want? Write the number on the menu:
+      1. Greek salad 90:-
+      2. Ceasar salad 90:-
+      3. Pasta salad 90:-
+      4. Avocado salad 100:-
+      (Children's size -10:-)`
+      )
+    );
+    //Save chosen item and price
+    if (menuAnswer >= 1 && menuAnswer <= 4) {
+      menuChoice = saladMenu[menuAnswer - 1]["item"];
+      menuPrice = saladMenu[menuAnswer - 1]["price"];
+    } else {
+      alert(
+        `I'm sorry. Number ${menuAnswer} is not an option. Please start over.`
+      );
+    }
   }
 }
 
@@ -189,7 +175,6 @@ alert(`You chose the ${menuChoice}. Great choice!`);
 let age = prompt(
   `We need to know if the food is for an adult or a child. Please write your age:`
 );
-console.log("Age:", age);
 //Decide if the size is child or adult
 let size = "";
 let finalPrice = null;
@@ -199,33 +184,29 @@ if (age < 18) {
     `Since you are younger than 18. We will make a child-size ${foodChoice}.`
   );
   size = "a child";
-  console.log("Size:", size);
   finalPrice = menuPrice - 10;
 } else {
   alert(`Since you are 18 or older. We will make an adult-size ${foodChoice}.`);
   size = "an adult";
-  console.log("Size:", size);
   finalPrice = menuPrice;
 }
-
-//Log order with size and correct price
-
-console.log("Order:", size, menuChoice);
-console.log("Final price:", finalPrice);
 
 // Step 5 - Order confirmation
 let confirmation = prompt(
   `You have ordered ${size}-size ${menuChoice} for the price of ${finalPrice}:-. Is this order correct? Write yes or no:`
 );
-console.log("Confirmed:", confirmation);
 
-if (confirmation === "yes") {
-  alert(
-    `Thank you for your order, ${userName}! Your ${menuChoice} is on its way.`
-  );
-} else {
-  alert(
-    `I'm sorry to hear that ${userName}. You're welcome to start over and place a new order. Otherwise - have a good one, and I hope to see you again soon!`
-  );
-  exit(1);
+let orderDone = false;
+
+switch (confirmation.toLowerCase()) {
+  case "yes":
+    alert(
+      `Thank you for your order, ${userName}! Your ${menuChoice} is on its way.`
+    );
+    orderDone = true;
+    break;
+  default:
+    alert(
+      `I'm sorry to hear that ${userName}. You're welcome to start over and place a new order. Otherwise - have a good one, and I hope to see you again soon!`
+    );
 }
--- pull_request_template.md
+++
@@ -1,4 +0,0 @@
-## Netlify link
-
-Add your Netlify link here.
-https://clever-cendol-83931b.netlify.app/
