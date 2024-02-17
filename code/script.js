cd85ff1401fa4d43c24bfd71e373f8c950673034
Lina Roos <linaroos@MBPsomtlhorLina.home>
2024-02-17 21:40:34 +0100

    updated

--- code/script.js
+++ code/script.js
@@ -29,30 +29,41 @@ if (userChoice === 1) {
 
 alert(`You chose ${selectedMeal} ,exellent choice!`)
 
-//user selection of age//
-let customerType = prompt ("Is the order for a child or an adult? Please type 'child' or 'adult'. ")
+let subtype = "";
 
-if (customerType === "child") {
-  alert("Your price will be 5€")
-} else if (customerType ==="adult") {
-  alert("Your price will be 10€")
-} else {
-  alert ("Not valid, please try again")
-}
-//order information//
-let orderConfirmation = "";
+if (selectedMeal) {
+  if (selectedMeal === "Pizza") {
+    subtype = prompt("Please choose a type of pizza: Margherita, Pepperoni, Hawaiian");
+  } else if (selectedMeal === "Pasta") {
+    subtype = prompt("Please choose a type of pasta: Spaghetti, Lasagna, Ravioli");
+  } else if (selectedMeal === "Salad") {
+    subtype = prompt("Please choose a type of salad: Caesar, Greek, Chicken");
+  }
 
-if (customerType === "child" || customerType === "adult") {
-  orderConfirmation = prompt(`You have ordered ${selectedMeal} for a ${customerType}. Your total price is ${customerType === "child" ? "5€" : "10€"}. Would you like to confirm your order? Yes or No`);
-} else {
-  alert("Invalid customer type. Please restart your order.");
+  if (!subtype) {
+    alert("You did not choose a subtype. Please start again.");
+ 
+  } else {
+    alert(`You chose ${subtype} ${selectedMeal}, a great choice!`);
+  }
 }
-//order confirmation//
+
+// User selection of age
+let customerType = prompt("Is the order for a child or an adult? Please type 'child' or 'adult'.");
+if (customerType !== "child" && customerType !== "adult") {
+  alert("Not valid, please try again");
+}
+
+let orderPrice = customerType === "child" ? "5€" : "10€";
+alert(`Your price will be ${orderPrice}`);
+
+// Order information and confirmation
+let orderConfirmation = prompt(`You have ordered a ${subtype} ${selectedMeal} for a ${customerType}. Your total price is ${orderPrice}. Would you like to confirm your order? Yes or No`);
+
 if (orderConfirmation.toLowerCase() === "yes") {
-  alert(`Thank you, ${userName}! Your order for ${selectedMeal} is being prepared.`);
+  alert(`Thank you, ${userName}! Your order for a ${subtype} ${selectedMeal} is being prepared.`);
 } else if (orderConfirmation.toLowerCase() === "no") {
   alert("No problem, your order has been canceled. Welcome back any time!");
 } else {
   alert("Invalid response. Please restart your order.");
 }
-
