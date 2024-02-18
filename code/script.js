4c1528ecd806a690e940531d542c20448ec50293
schouenkes <m.schouenke@gmail.com>
2024-02-18 21:01:01 +0100

    fixed indentation and deleted console.log

--- code/script.js
+++ code/script.js
@@ -4,9 +4,6 @@ alert(`Welcome to our Virtual Pizzeria. Ready to order? - Click 'OK' to begin.`)
 //A statement which variable value will be the name the user enters.
 const name = prompt(`Please enter your name:`);
 
-//will log the users name in the console.
-console.log(`Customer: ${name}`);
-
 //this will return an alert message with the name that the user typed in.
 alert(`Hi ${name}, it's nice to see you here!`);
 
@@ -15,14 +12,14 @@ alert(`Hi ${name}, it's nice to see you here!`);
 //the variable value parseInt is used to parse a string and return an integer. By using parseInt in this case we override the default data type (string) of a prompt and the user can only type a number to don't get an error.
 
 const foodType = parseInt(
-   prompt(
-      `What would you like to order?
+  prompt(
+    `What would you like to order?
 
       Enter the number of your choice.
       1 - Pizza 
       2 - Hamburger
       3 - Salad`
-   )
+  )
 );
 
 let foodSelected;
@@ -30,20 +27,18 @@ let foodSelected;
 //Depending on which number the user choose above, the if statement will check if any of the if, else if och else statements is true and "create" a new variable with a new value that represent the number. Giving the number a name.
 
 if (foodType === 1) {
-   foodSelected = "Pizza";
+  foodSelected = "Pizza";
 } else if (foodType === 2) {
-   foodSelected = "Hamburger";
+  foodSelected = "Hamburger";
 } else if (foodType === 3) {
-   foodSelected = "Salad";
+  foodSelected = "Salad";
 } else {
-   alert(
-      `Couldn't you decide what to choose? 
+  alert(
+    `Couldn't you decide what to choose? 
       Please, start over and type the number representing what you want to eat.`
-   );
-   exit(1); //Will exit the application with a failure code if used correctly.
+  );
+  exit(1); //Will exit the application with a failure code if used correctly.
 }
-
-console.log(`Selected food type: ${foodSelected}`);
 
 alert(`You chosed ${foodSelected}`);
 
@@ -54,92 +49,88 @@ let pizzaSelected;
 //presents a new menu depending on which food type that was selected, and a nested if-statement to declare the food options number with a name.
 
 if (foodType === 1) {
-   const foodOption = parseInt(
-      prompt(
-         `What type of pizza would you like to order?
+  const foodOption = parseInt(
+    prompt(
+      `What type of pizza would you like to order?
    
          Enter the number of your choice.
          1 - Napolitan
          2 - Margherita
          3 - Capricciosa`
-      )
-   );
+    )
+  );
 
-   if (foodOption === 1) {
-      pizzaSelected = "Napolitan";
-   } else if (foodOption === 2) {
-      pizzaSelected = "Margherita";
-   } else if (foodOption === 3) {
-      pizzaSelected = "Capricciosa";
-   } else {
-      alert(
-         `Couldn't you decide what to choose? 
+  if (foodOption === 1) {
+    pizzaSelected = "Napolitan";
+  } else if (foodOption === 2) {
+    pizzaSelected = "Margherita";
+  } else if (foodOption === 3) {
+    pizzaSelected = "Capricciosa";
+  } else {
+    alert(
+      `Couldn't you decide what to choose? 
          Please, start over and type the number representing your choice.`
-      );
-      exit(1);
-   }
-   console.log(`Selected pizza: ${pizzaSelected}`);
+    );
+    exit(1);
+  }
 }
 
 let hamburgerSelected;
 
 if (foodType === 2) {
-   const foodOption = parseInt(
-      prompt(
-         `What type of hamburger would you like to order?
+  const foodOption = parseInt(
+    prompt(
+      `What type of hamburger would you like to order?
   
          Enter the number of your choice.
          1 - Halloumi burger
          2 - Plant-based burger
          3 - Classic burger`
-      )
-   );
+    )
+  );
 
-   if (foodOption === 1) {
-      hamburgerSelected = "Halloumi burger";
-   } else if (foodOption === 2) {
-      hamburgerSelected = "Plant-based burger";
-   } else if (foodOption === 3) {
-      hamburgerSelected = "Classic burger";
-   } else {
-      alert(
-         `Couldn't you decide what to choose? 
+  if (foodOption === 1) {
+    hamburgerSelected = "Halloumi burger";
+  } else if (foodOption === 2) {
+    hamburgerSelected = "Plant-based burger";
+  } else if (foodOption === 3) {
+    hamburgerSelected = "Classic burger";
+  } else {
+    alert(
+      `Couldn't you decide what to choose? 
          Please, start over and type the number representing your choice.`
-      );
-      exit(1);
-   }
-
-   console.log(`Selected burger: ${hamburgerSelected}`);
+    );
+    exit(1);
+  }
 }
 
 let saladSelected;
 
 if (foodType === 3) {
-   const foodOption = parseInt(
-      prompt(
-         `What type of salad would you like to order?
+  const foodOption = parseInt(
+    prompt(
+      `What type of salad would you like to order?
  
          Enter the number of your choice.
          1 - Vegan salad
          2 - Caesar salad
          3 - Greek salad`
-      )
-   );
+    )
+  );
 
-   if (foodOption === 1) {
-      saladSelected = "Vegan salad";
-   } else if (foodOption === 2) {
-      saladSelected = "Caesar salad";
-   } else if (foodOption === 3) {
-      saladSelected = "Greek salad";
-   } else {
-      alert(
-         `Couldn't you decide what to choose? 
+  if (foodOption === 1) {
+    saladSelected = "Vegan salad";
+  } else if (foodOption === 2) {
+    saladSelected = "Caesar salad";
+  } else if (foodOption === 3) {
+    saladSelected = "Greek salad";
+  } else {
+    alert(
+      `Couldn't you decide what to choose? 
          Please, start over and type the number representing your choice.`
-      );
-      exit(1);
-   }
-   console.log(`Selected salad: ${saladSelected}`);
+    );
+    exit(1);
+  }
 }
 
 alert(`Good choice, you chosed to order a ${pizzaSelected || hamburgerSelected || saladSelected}`);
@@ -147,59 +138,56 @@ alert(`Good choice, you chosed to order a ${pizzaSelected || hamburgerSelected |
 // Step 4 - Age
 
 const age = parseInt(
-   prompt(
-      `Is this meal for a child or an adult? 
+  prompt(
+    `Is this meal for a child or an adult? 
       Type your age:`
-   )
+  )
 );
-console.log(`${age} years`);
 
 if (age >= 15) {
-   alert(
-      `One adult sized ${pizzaSelected || hamburgerSelected || saladSelected} will be prepared for you.
+  alert(
+    `One adult sized ${pizzaSelected || hamburgerSelected || saladSelected} will be prepared for you.
       That will be €10`
-   );
+  );
 } else if (age <= 14) {
-   alert(
-      `One child sized ${pizzaSelected || hamburgerSelected || saladSelected} will be prepared for you.
+  alert(
+    `One child sized ${pizzaSelected || hamburgerSelected || saladSelected} will be prepared for you.
       That will be €8`
-   );
+  );
 } else {
-   alert(
-      `Oops, we didn't get your age!
+  alert(
+    `Oops, we didn't get your age!
       We need to know your age to prepare an appropiate meal size. 
       Please start over and remember to fill in all of the input fields`
-   );
-   exit(1);
+  );
+  exit(1);
 }
 
 const orderConfirmation = parseInt(
-   prompt(
-      `Do you want to place the order?
+  prompt(
+    `Do you want to place the order?
       Enter the number that represents your choice:
       1 - Yes
       2 - No`
-   )
+  )
 );
-
-console.log(`Want to finish the order (1=Yes 2=No): ${orderConfirmation}`);
 
 // Step 5 - Order confirmation
 
 if (orderConfirmation === 1) {
-   alert(
-      `Thank you for your order, your order will be ready in 20 min. 
+  alert(
+    `Thank you for your order, your order will be ready in 20 min. 
       Bon appetit!`
-   );
+  );
 } else if (orderConfirmation === 2) {
-   alert(
-      `No problem, welcome back another time.
+  alert(
+    `No problem, welcome back another time.
       Have a good day!`
-   );
+  );
 } else {
-   alert(
-      `We didn't get any confirmation if you wanted to place the order.
+  alert(
+    `We didn't get any confirmation if you wanted to place the order.
       Please start over if you still want to order.`
-   );
-   exit(1);
+  );
+  exit(1);
 }
--- code/switch.js
+++ code/switch.js
@@ -4,20 +4,19 @@ alert(`Welcome to our Virtual Pizzeria. Ready to order? - Click 'OK' to begin.`)
 
 const name = prompt(`Please enter your name:`);
 
-console.log(`Customer: ${name}`);
 alert(`Hi ${name}, it's nice to see you here!`);
 
 // Step 2 - Food choice
 
 const foodType = parseInt(
-   prompt(
-      `What would you like to order?
+  prompt(
+    `What would you like to order?
  
        Enter the number of your choice.
        1 - Pizza 
        2 - Hamburger
        3 - Salad`
-   )
+  )
 );
 
 //Giving the food type number a name.
@@ -25,27 +24,26 @@ const foodType = parseInt(
 let foodSelected;
 
 switch (foodType) {
-   case 1:
-      foodSelected = "Pizza";
-      break;
+  case 1:
+    foodSelected = "Pizza";
+    break;
 
-   case 2:
-      foodSelected = "Hamburger";
-      break;
+  case 2:
+    foodSelected = "Hamburger";
+    break;
 
-   case 3:
-      foodSelected = "Salad";
-      break;
+  case 3:
+    foodSelected = "Salad";
+    break;
 
-   default:
-      alert(
-         `Couldn't you decide what to choose? 
+  default:
+    alert(
+      `Couldn't you decide what to choose? 
          Please, start over and type the number representing what you want to eat.`
-      );
-      exit(1);
+    );
+    exit(1);
 }
 
-console.log(`Selected food type: ${foodSelected}`);
 alert(`You chosed ${foodSelected}`);
 
 // Step 3 - Subtype choice
@@ -56,107 +54,104 @@ let foodChoice;
 //presents a new menu depending on which food type that was selected, and a nested switch statement to declare the food options number with a name.
 
 switch (foodSelected) {
-   case "Pizza":
-      foodOption = parseInt(
-         prompt(
-            `What type of pizza would you like to order?
+  case "Pizza":
+    foodOption = parseInt(
+      prompt(
+        `What type of pizza would you like to order?
      
            Enter the number of your choice.
            1 - Napolitan
            2 - Margherita
            3 - Capricciosa`
-         )
-      );
-      switch (foodOption) {
-         case 1:
-            foodChoice = "Napolitan";
-            break;
+      )
+    );
+    switch (foodOption) {
+      case 1:
+        foodChoice = "Napolitan";
+        break;
 
-         case 2:
-            foodChoice = "Margherita";
-            break;
+      case 2:
+        foodChoice = "Margherita";
+        break;
 
-         case 3:
-            foodChoice = "Capricciosa";
-            break;
+      case 3:
+        foodChoice = "Capricciosa";
+        break;
 
-         default:
-            alert(
-               `Couldn't you decide what to choose? 
+      default:
+        alert(
+          `Couldn't you decide what to choose? 
                 Please, start over and type the number representing your choice.`
-            );
-            exit(1);
-      }
-      console.log(`Selected pizza: ${foodChoice}`);
-      break;
+        );
+        exit(1);
+    }
+    break;
 
-   case "Hamburger":
-      foodOption = parseInt(
-         prompt(
-            `What type of hamburger would you like to order?
+  case "Hamburger":
+    foodOption = parseInt(
+      prompt(
+        `What type of hamburger would you like to order?
 
             Enter the number of your choice.
             1 - Halloumi burger
             2 - Plant-based burger
             3 - Classic burger`
-         )
-      );
-      switch (foodOption) {
-         case 1:
-            foodChoice = "Halloumi burger";
-            break;
+      )
+    );
+    switch (foodOption) {
+      case 1:
+        foodChoice = "Halloumi burger";
+        break;
 
-         case 2:
-            foodChoice = "Plant-based burger";
-            break;
+      case 2:
+        foodChoice = "Plant-based burger";
+        break;
 
-         case 3:
-            foodChoice = "Classic burger";
-            break;
+      case 3:
+        foodChoice = "Classic burger";
+        break;
 
-         default:
-            alert(
-               `Couldn't you decide what to choose? 
+      default:
+        alert(
+          `Couldn't you decide what to choose? 
                 Please, start over and type the number representing your choice.`
-            );
-            exit(1);
-      }
-      console.log(`Selected burger: ${foodChoice}`);
-      break;
+        );
+        exit(1);
+    }
+    break;
 
-   case "Salad":
-      foodOption = parseInt(
-         prompt(
-            `What type of salad would you like to order?
+  case "Salad":
+    foodOption = parseInt(
+      prompt(
+        `What type of salad would you like to order?
    
             Enter the number of your choice.
             1 - Vegan salad
             2 - Caesar salad
             3 - Greek salad`
-         )
-      );
-      switch (foodOption) {
-         case 1:
-            foodChoice = "Vegan salad";
-            break;
+      )
+    );
+    switch (foodOption) {
+      case 1:
+        foodChoice = "Vegan salad";
+        break;
 
-         case 2:
-            foodChoice = "Caesar salad";
-            break;
+      case 2:
+        foodChoice = "Caesar salad";
+        break;
 
-         case 3:
-            foodChoice = "Greek salad";
-            break;
+      case 3:
+        foodChoice = "Greek salad";
+        break;
 
-         default:
-            alert(
-               `Couldn't you decide what to choose? 
+      default:
+        alert(
+          `Couldn't you decide what to choose? 
                 Please, start over and type the number representing your choice.`
-            );
-            exit(1);
-      }
-      console.log(`Selected salad: ${foodChoice}`);
-      break;
+        );
+        exit(1);
+    }
+    break;
 }
 
 alert(`Good choice, you chosed to order a ${foodChoice}`);
@@ -164,72 +159,68 @@ alert(`Good choice, you chosed to order a ${foodChoice}`);
 // Step 4 - Age
 
 const age = parseInt(
-   prompt(
-      `Is this meal for a child or an adult? 
+  prompt(
+    `Is this meal for a child or an adult? 
        Type your age:`
-   )
+  )
 );
-
-console.log(`${age} years`);
 
 //The switch expression compares strictly to the expression in each case it can't be <= or >=, so to be able to decide whether it is a child or an adult sized meal I set the expression as true and then it checks if the input value matched strictly to the case of the case expression.
 
 switch (true) {
-   case age >= 15:
-      alert(
-         `One adult sized ${foodChoice} will be prepared for you.
+  case age >= 15:
+    alert(
+      `One adult sized ${foodChoice} will be prepared for you.
          That will be €10`
-      );
-      break;
+    );
+    break;
 
-   case age <= 14:
-      alert(
-         `One child sized ${foodChoice} will be prepared for you.
+  case age <= 14:
+    alert(
+      `One child sized ${foodChoice} will be prepared for you.
          That will be €8`
-      );
-      break;
+    );
+    break;
 
-   default:
-      alert(
-         `Oops, we didn't get your age!
+  default:
+    alert(
+      `Oops, we didn't get your age!
         We need to know your age to prepare an appropiate meal size. 
         Please start over and remember to fill in all of the input fields`
-      );
-      exit(1);
+    );
+    exit(1);
 }
 
 const orderConfirmation = parseInt(
-   prompt(
-      `Do you want to place the order?
+  prompt(
+    `Do you want to place the order?
        Enter the number that represents your choice:
        1 - Yes
        2 - No`
-   )
+  )
 );
-
-console.log(`Want to finish the order (1=Yes 2=No): ${orderConfirmation}`);
 
 // Step 5 - Order confirmation
 
 switch (orderConfirmation) {
-   case 1:
-      alert(
-         `Thank you for your order, your order will be ready in 20 min. 
+  case 1:
+    alert(
+      `Thank you for your order, your order will be ready in 20 min. 
          Bon appetit!`
-      );
-      break;
+    );
+    break;
 
-   case 2:
-      alert(
-         `No problem, welcome back another time.
+  case 2:
+    alert(
+      `No problem, welcome back another time.
          Have a good day!`
-      );
-      break;
+    );
+    break;
 
-   default:
-      alert(
-         `We didn't get any confirmation if you wanted to place the order.
+  default:
+    alert(
+      `We didn't get any confirmation if you wanted to place the order.
          Please start over if you still want to order.`
-      );
-      exit(1);
+    );
+    exit(1);
 }
