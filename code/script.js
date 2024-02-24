4952ea73830d11d273780d192572bc1bd3fd9d76
Katarina Sjölin <kat.sjolin@gmail.com>
2024-02-24 16:25:29 +0100

    added step 1 welcoming the customer

--- code/script.js
+++ code/script.js
@@ -6,6 +6,13 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+let customerName = prompt("Hello and welcome to our establishment! May I have your name?");
+
+alert(
+  `Are you hungry today, ${customerName}? We've got some delicious dishes here at Javascript Pizzeria!`
+)
+
+
 // Step 2 - Food choice
 // Your code goes here
 
