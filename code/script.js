ddb5a111f3aa1a8f5c015d6fff0d0329e8346357
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-03 16:30:07 +0200

    Finished mealchoices

--- code/script.js
+++ code/script.js
@@ -16,16 +16,26 @@ alert(
 
 // Step 2 - Food choice
 // Your code goes here
-const mealchoice = prompt(
-  'What would you like to order? 1. Pizza, 2. Pasta, 3. Salad. Please enter the number of your choice´
-  
-  if (mealchoice === 1)
-  alert(`Thanks, ${name}! You wish to order Pizza!`
-  else if (mealchoice === 2))
-alert(`Thanks, ${name}! You wish to order Pasta!`
-  else if (mealchoice === 3))
-alert(`Thanks, ${name}! You wish to order Salad!`)
+let mealchoice = prompt(
+  `What would you like to order? 1. Pizza, 2. Pasta, 3. Salad. Please enter the number of your choice`
 )
+if (mealchoice === `1`) {
+  alert(
+    `Thanks, ${name}! You wish to order Pizza!`
+  )
+} else if (mealchoice === `2`) {
+  alert(
+    `Thanks, ${name}! You wish to order Pasta!`
+  )
+} else if (mealchoice === `3`) {
+  alert(
+    `Thanks, ${name}! You wish to order Salad!`
+  )
+} else {
+  alert(
+    `Sorry, ${name}! That's not a valid choice!`
+  )
+}
 
 // Step 3 - Subtype choice
 // Your code goes here
