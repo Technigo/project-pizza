c877c285b7c9d4fcef28fa50b68c112b5bde1176
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-03 14:41:46 +0200

    Started the Javascript Pizzeria project

--- code/script.js
+++ code/script.js
@@ -6,8 +6,22 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+const name = prompt(
+  `Hi hungry fellow! What's your name?`
+)
+
+alert(
+  `Hello, ${name}! Hit the button below whenever you are ready to order!`
+)
+
 // Step 2 - Food choice
 // Your code goes here
+const mealchoice = prompt(
+  'What would you like to order?' 1. Pizza, 2. Pasta, 3. Salad.Please enter the number of your choice:
+)
+alert(
+  `Thanks, ${name}! You wish to order ${mealchoice}!`
+)
 
 // Step 3 - Subtype choice
 // Your code goes here
