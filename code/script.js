01ec7818c28f4ba5e341c3579a3b2444cad4ca37
Anna Hansen <annah80@gmail.com>
2024-09-04 14:18:32 +0200

    Changed title, h1 and added styling and the first promt

--- code/script.js
+++ code/script.js
@@ -3,14 +3,38 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  `Welcome to MamaMia Pizzeria. Ready to order? - Click 'OK' to begin.`
 )
 
-// Step 2 - Food choice
-// Your code goes here
+// Step 2 - What is your name? 
+
+const name = prompt("What's your name?")
+console.log('user name is:', name)
+
+alert(`Thank you for choosing our restaurant, ${name}! We’re here to make your experience memorable.`);
+
+ // Step 3 - What type of food
+
+//  const mealchoice = prompt (
+ 
+//  ("What would you like to eat? Choose the dish number 1, 2 or 3 according to below"
+
+//   1.Pizza
+//   2. Pasta
+//   3. Salad
+
+//  if (number === 1)
+//   console.log("Pizza");
+// )
+
+
+alert (
+  "You have choosen, ${food choice}"
+)
+
 
 // Step 3 - Subtype choice
-// Your code goes here
+
 
 // Step 4 - Age
 // Your code goes here
