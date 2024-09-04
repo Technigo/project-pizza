10d9fc0e6bdcabd5b260f81bf9b77e83e02e2e22
Estefanny <estefanny@Estefannys-MacBook-Air.local>
2024-09-04 22:45:02 +0200

    Add food choice, clean up code

--- code/script.js
+++ code/script.js
@@ -2,18 +2,25 @@
 
 // Step 1 - Welcome and introduction
 // Your code goes here
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`)
 
-let customerName = prompt("Please enter your name", "Your name");
+let customerName = prompt("Please enter your name", "Your name")
 
-alert("Hello: " + customerName + ". What are you craving today?")
-
-
-
+alert("Hi " + customerName + "! What are you craving today?")
 // Step 2 - Food choice
 // Your code goes here
+
+let foodChoice = prompt ("Please enter a number: 1 for pizza. 2 for pasta. 3 for sallad", "Please type your number here")
+
+if (foodChoice == 1) {
+  alert("You have chosen pizza!")
+} else if (foodChoice == 2) {
+  alert("You have chosen pasta!")
+} else if (foodChoice == 3) {
+  alert("You have chosen sallad!")
+} else {
+  alert("Incorrect number")
+}
 
 
 // Step 3 - Subtype choice
