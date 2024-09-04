1b116a842b38d61464cfe653471d97848ba2905b
Anna Hansen <annah80@gmail.com>
2024-09-04 15:04:14 +0200

    added more prompts and alerts for the different kinds of food

--- code/script.js
+++ code/script.js
@@ -9,29 +9,44 @@ alert(
 // Step 2 - What is your name? 
 
 const name = prompt("What's your name?")
-console.log('user name is:', name)
+console.log('name is:', name)
 
 alert(`Thank you for choosing our restaurant, ${name}! We’re here to make your experience memorable.`);
 
  // Step 3 - What type of food
 
-//  const mealchoice = prompt (
- 
-//  ("What would you like to eat? Choose the dish number 1, 2 or 3 according to below"
+const mealchoice = prompt (`What would you like to eat? Choose the dish number 1, 2 or 3 according to below"
 
-//   1.Pizza
-//   2. Pasta
-//   3. Salad
-
-//  if (number === 1)
-//   console.log("Pizza");
-// )
-
-
-alert (
-  "You have choosen, ${food choice}"
+1. Pizza
+2. Pasta
+3. Salad
+`
 )
 
+if (mealchoice === "1") {
+
+alert (
+ `Thank you ${name},you have chosen nr 1, Pizza` 
+)
+
+} else if (mealchoice === "2") {
+  alert (
+  `Thank you ${name} you have chosen nr 2, Pasta`
+  )
+
+} else if (mealchoice === "3") {
+    alert (
+    `Thank you ${name} you have chosen nr 3, Salad`
+  )
+    
+} else {
+    alert (
+    `I´m sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3`
+    )
+}
+
+
+  
 
 // Step 3 - Subtype choice
 
