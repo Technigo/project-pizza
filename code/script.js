6dc4328461c469cc8c98a5f233800f99892e14ec
Johanna Eriksson <johanna.eriksson@schibsted.com>
2024-09-04 15:45:25 +0200

    Step 4 - Age. Is the food for a child or an adult

--- code/script.js
+++ code/script.js
@@ -22,59 +22,107 @@ const mealchoice = prompt(
   3. Salad
   `
 )
+
 if (mealchoice === "1") {
   alert(
     `Thanks, ${userName}! You wish to order Pizza!`
   )
+
+  //Pizza
+  const pizzachoice = prompt(
+    `What type of Pizza you like to order? Please enter the number of your choice.
+    
+    1. Capricciosa 
+    2. Hawaii
+    3. Vesuvius
+    `
+  )
+
+  if (pizzachoice === "1") {
+    orderedFood = "Capricciosa"
+  } else if (pizzachoice === "2") {
+    orderedFood = "Hawaii"
+  } else if (pizzachoice === "3") {
+    orderedFood = "Vesuvius"
+  } else {
+    alert("Sorry, we don't have that kind of pizza.")
+  }
+  if (orderedFood) {
+    alert(`You've chosen ${orderedFood}`)
+  }
+
 } else if (mealchoice === "2") {
   alert(
     `Thanks, ${userName}! You wish to order Pasta!`
   )
+
+  //Pasta
+  const pastachoice = prompt(
+    `What type of Pasta you like to order ? Please enter the number of your choice.
+     
+     1. Spagetti Bolognese
+     2. Pasta Carbonara
+     3. Fettuccine Alfredo
+  `
+  )
+  
+  if (pastachoice === "1") {
+    orderedFood = "Spagetti Bolognese"
+  } else if (pastachoice === "2") {
+    orderedFood = "Pasta Carbonara"
+  } else if (pastachoice === "3") {
+    orderedFood = "Fettuccine Alfredo"
+  } else {
+    alert("Sorry, we don't have that kind of pasta.")
+  }
+  if (orderedFood) {
+    alert(`You've chosen ${orderedFood}`)
+  }
+
 } else if (mealchoice === "3") {
   alert(
     `Thanks, ${userName}! You wish to order Salad!`
   )
+
+  //Salad
+  const saladchoice = prompt(
+    `What type of Salad you like to order ? Please enter the number of your choice.
+      
+      1. Pasta salad 
+      2. Noodle salad
+      3. Ceaser salad 
+    `
+  )
+
+  if (saladchoice === "1") {
+    orderedFood = "Pasta salad"
+  } else if (saladchoice === "2") {
+    orderedFood = "Noodle salad"
+  } else if (saladchoice === "3") {
+    orderedFood = "Ceaser salad"
+  } else {
+    alert("Sorry, we don't have that kind of salad.")
+  }
+  if (orderedFood) {
+    alert(`You've chosen ${orderedFood}`)
+  }
+
 } else {
   alert(
     `Sorry, ${userName}! That's not a valid choice!`
   )
 }
-// Step 3 - Subtype choice
-//Pizza
-if (mealchoice === "1") {
-  prompt(
-    `What type of Pizza you like to order? Please enter the number of your choice.
-    
-    1. Capricciosa
-    2. Hawaii
-    3. Vesuvius
-    `
-  )
-} else if (mealchoice === "2") {
-  prompt(
-    `What type of Pasta you like to order? Please enter the number of your choice.
-    
-    1. Spagetti Bolognese
-    2. Pasta Carbonara
-    3. Fettuccine Alfredo
-    `
-  )
-} else if (mealchoice === "3") {
-  prompt(
-    `What type of Salad you like to order? Please enter the number of your choice.
-      
-      1. Pasta salad 
-      2. Noodle salad
-      3. Something
-      `
-  )
-}
-
-// Your code goes here
-
 
 // Step 4 - Age
-// Your code goes here
+const age = prompt (
+  `Is this food for a child or an adult? Type your age:`
+)
+if (age >= 18) 
+   alert("The food is for an adult")
+  else {
+    alert ("The food is for a child")
+  }
 
 // Step 5 - Order confirmation
 // Your code goes here
+
