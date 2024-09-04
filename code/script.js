6f448e1a7a9cf3b64a71bbe6db77b1cd569cf957
Estefanny <estefanny@Estefannys-MacBook-Air.local>
2024-09-04 21:59:56 +0200

    welcome and introduction

--- code/script.js
+++ code/script.js
@@ -6,8 +6,15 @@ alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+let customerName = prompt("Please enter your name", "Your name");
+
+alert("Hello: " + customerName + ". What are you craving today?")
+
+
+
 // Step 2 - Food choice
 // Your code goes here
+
 
 // Step 3 - Subtype choice
 // Your code goes here
