ff3b95dadd294986e46d463984901bc5a452a969
iamfannyhenriques <fanny.henriques@hotmail.com>
2024-09-05 14:39:26 +0200

    added a loop

--- code/script.js
+++ code/script.js
@@ -5,7 +5,7 @@
 alert(
   `Hey there, pizza lover! 🍕 Welcome to Pizzeria Fresco! Click 'OK' to get started!`
 )
-let userName = prompt("What is your name?")
+const userName = prompt(`What is your name?`)
 console.log("User's name is:", userName)
 
 alert(
@@ -14,138 +14,187 @@ alert(
 
 // Step 2 - Food choice
 // Selection between Pizza, Pasta, Salad
-let foodSelection = prompt(`Please select your food of choice: 
-  Write: 1 - for Pizza 🍕
-  Write: 2 - for Pasta 🍝
-  Write: 3 - for Salad 🥗`
-)
-//convert the variabel "foodselection" to be a number
-foodSelection = Number(foodSelection)
-
+// while loop to ensure that the user selects a valid option if not, the user will be prompted to select again. Will loop until it hits break. 
 let foodType
 
-if (foodSelection === 1) {
-  console.log("Pizza")
-  foodType = "Pizza"
-} else if (foodSelection === 2) {
-  console.log("Pasta")
-  foodType = "Pasta"
-} else if (foodSelection === 3) {
-  console.log("Salad")
-  foodType = "Salad"
-} else {
-  alert(`Oops! It looks like that's not an option. Please choose from 1, 2, or 3`)
-  console.log("Invalid selection. Please choose 1, 2, or 3.")
+while (true) {
 
+  let foodSelection = prompt(`Please select your food of choice: 
+      Write: 1 - for Pizza 🍕
+      Write: 2 - for Pasta 🍝
+      Write: 3 - for Salad 🥗`)
+
+  // convert the variabel "foodSelection" to be a number instead of a string
+  foodSelection = Number(foodSelection)
+
+
+  if (foodSelection === 1) {
+    console.log("Pizza")
+    foodType = "Pizza"
+    break
+  } else if (foodSelection === 2) {
+    console.log("Pasta")
+    foodType = "Pasta"
+    break
+  } else if (foodSelection === 3) {
+    console.log("Salad")
+    foodType = "Salad"
+    break
+  } else {
+    alert(
+      `Oops! It looks like that's not an option. Please choose from 1, 2, or 3`
+    )
+    console.log("Invalid selection. Please choose 1, 2, or 3.")
+  }
 }
-
 //message with the food that has been chosen 
 alert(`Great choice, ${foodType}! Click 'OK' to select the specific type of ${foodType} you'd like.`)
 
 // Step 3 - Subtype choice
 // Variables to store subtype choices
-let pizzaType, pastaType, saladType
+let pizzaType
+let pastaType
+let saladType
 let subType
 
 //If choice is Pizza -> select type of pizza
 if (foodType === "Pizza") {
-  pizzaType = prompt(`Please select your type of Pizza 🍕: 
+  while (true) {
+    pizzaType = prompt(`Please select your type of Pizza 🍕: 
     Write: 1 - for Margherita Pizza 
     Write: 2 - for Pepperoni Pizza
     Write: 3 - for Veggie Pizza`
-  )
+    )
 
-  pizzaType = Number(pizzaType)
+    pizzaType = Number(pizzaType)
 
-  if (pizzaType === 1) {
-    console.log("Margherita")
-    subType = "Margherita Pizza"
-  } else if (pizzaType === 2) {
-    console.log("Pepperoni Pizza")
-    subType = "Pepperoni"
-  } else if (pizzaType === 3) {
-    console.log("Veggie")
-    subType = "Veggie Pizza"
-  } else {
-    alert(`Oops! It looks like that's not an option. Please choose from 1, 2, or 3`)
-    console.log("Invalid selection. Please choose 1, 2, or 3.")
+    if (pizzaType === 1) {
+      console.log("Margherita")
+      subType = "Margherita Pizza"
+      break
+    } else if (pizzaType === 2) {
+      console.log("Pepperoni Pizza")
+      subType = "Pepperoni"
+      break
+    } else if (pizzaType === 3) {
+      console.log("Veggie")
+      subType = "Veggie Pizza"
+      break
+    } else {
+      alert(`Oops! It looks like that's not an option. Please choose from 1, 2, or 3`)
+      console.log("Invalid selection. Please choose 1, 2, or 3.")
+    }
   }
 }
+
+
 // If choice is pasta -> select type of pasta
 if (foodType === "Pasta") {
-  pastaType = prompt(`Please select your type of Pasta 🍝: 
+  while (true) {
+    pastaType = prompt(`Please select your type of Pasta 🍝: 
     Write: 1 - for Spaghetti Carbonara  
     Write: 2 - for Spaghetti Bolognese
     Write: 3 - for Pasta Primavera`
-  )
+    )
 
-  pastaType = Number(pastaType)
+    pastaType = Number(pastaType)
 
-  if (pastaType === 1) {
-    console.log("Carbonara")
-    subType = "Spaghetti Carbonara"
-  } else if (pastaType === 2) {
-    console.log("Bolognese")
-    subType = "Spaghetti Bolognese"
-  } else if (pastaType === 3) {
-    console.log("Primavera")
-    subType = "Pasta Primavera"
-  } else {
-    alert(`Oops! It looks like that's not an option. Please choose from 1, 2, or 3`)
-    console.log("Invalid selection. Please choose 1, 2, or 3.")
+    if (pastaType === 1) {
+      console.log("Carbonara")
+      subType = "Spaghetti Carbonara"
+      break
+    } else if (pastaType === 2) {
+      console.log("Bolognese")
+      subType = "Spaghetti Bolognese"
+      break
+    } else if (pastaType === 3) {
+      console.log("Primavera")
+      subType = "Pasta Primavera"
+      break
+    } else {
+      alert(`Oops! It looks like that's not an option. Please choose from 1, 2, or 3`)
+      console.log("Invalid selection. Please choose 1, 2, or 3.")
+    }
   }
 }
-// If choice is salad -> select type of salad
 
+// If choice is salad -> select type of salad
 if (foodType === "Salad") {
-  saladType = prompt(`Please select your type of Salad 🥗: 
+  while (true) {
+    saladType = prompt(`Please select your type of Salad 🥗: 
     Write: 1 - for Ceacar Salad  
     Write: 2 - for Shrimp Salad
     Write: 3 - for Greek Salad`
-  )
+    )
 
-  saladType = Number(saladType)
+    saladType = Number(saladType)
 
-  if (saladType === 1) {
-    console.log("Ceacar Salad")
-    subType = "Ceacar Salad"
-  } else if (saladType === 2) {
-    console.log("Shrimp Salad")
-    subType = "Shrimp Salad"
-  } else if (saladType === 3) {
-    console.log("Greek Salad")
-    subType = "Greek Salad"
-  } else {
-    alert(`Oops! It looks like that's not an option. Please choose from 1, 2, or 3`)
-    console.log("Invalid selection. Please choose 1, 2, or 3.")
+    if (saladType === 1) {
+      console.log("Ceacar Salad")
+      subType = "Ceacar Salad"
+      break
+    } else if (saladType === 2) {
+      console.log("Shrimp Salad")
+      subType = "Shrimp Salad"
+      break
+    } else if (saladType === 3) {
+      console.log("Greek Salad")
+      subType = "Greek Salad"
+      break
+    } else {
+      alert(`Oops! It looks like that's not an option. Please choose from 1, 2, or 3`)
+      console.log("Invalid selection. Please choose 1, 2, or 3.")
+    }
   }
 }
 //message with the subtype that has been chosen 
 alert(`Yum! You've selected ${subType}! Excellent choice! Click 'OK' to start placing your order.`)
 
+
 // Step 4 - Age
-let userAge = prompt("Is this order intended for a child or an adult? Please respond with 'adult' or 'child' in the field below:")
-console.log("User's age is:", userAge)
+let userAge;
+while (true) {
+  userAge = prompt(
+    `Is this order intended for a child or an adult? Please respond with 'adult' or 'child' in the field below:`
+  )
+  console.log("User's age is:", userAge)
 
-let confirmation
-
-if (userAge === "adult") {
-  confirmation = prompt(`One Adult-sized ${subType} coming straight up! Do you accept?
-    Please enter 'yes' to confirm or 'no' to decline`)
-} else if (userAge === "child") {
-  confirmation = prompt(`One Child-sized ${subType} coming straight up! 
-    Do you accept?
-    Please enter: "yes" to confirm "no" to decline`)
-} else {
-  console.log("Invalid selection. Please choose:", "child", "adult")
-}
-// Step 5 - Order confirmation
-if (confirmation === "yes") {
-  alert(`Woho! 🎉 Your ${subType} is on it's way! Thanks for dining with us at Pizzeria Fresco! 🍕`)
-} else if (confirmation === "no") {
-  alert(`Your order has been cancelled. Hope to see you here again soon!`)
-} else {
-  console.log("Invalid selection. Please choose:", "yes", "no")
+  if (userAge === "adult" || userAge === "child") {
+    break
+  } else {
+    alert(
+      `Oops! It looks like that's not an option. Please choose 'adult' or 'child'`
+    )
+    console.log("Invalid selection. Please choose:", "child", "adult")
+  }
 }
 
+let confirmation;
+while (true) {
+  if (userAge === "adult") {
+    confirmation =
+      prompt(
+        `One Adult-sized ${subType} coming straight up!🍕 
+        Do you accept? 
+        Please enter 'yes' to confirm or 'no' to decline:`)
+  } else if (userAge === "child") {
+    confirmation = prompt(
+      `One Child-sized ${subType} coming straight up! 🍕
+      Do you accept? 
+      Please enter 'yes' to confirm or 'no' to decline:`)
+  }
 
+  // Step 5 - Order confirmation
+  if (confirmation === "yes") {
+    alert(
+      `Woho! 🎉 Your ${subType} is on its way! Thanks for dining with us at Pizzeria Fresco! 🍕`
+    );
+    break
+  } else if (confirmation === "no") {
+    alert(`Your order has been cancelled. Hope to see you here again soon!`)
+    break
+  } else {
+    alert(`Invalid selection. Please choose 'yes' or 'no'.`)
+    console.log("Invalid selection. Please choose:", "yes", "no")
+  }
+}
