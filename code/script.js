b9062d7b393e255ea05cc712d50b0098881b635c
Jonash189 <jonas.hellstrom89@gmail.com>
2024-09-05 18:25:46 +0200

    Added propmt and alert for userName +  Added a prompt for the food options.

--- code/script.js
+++ code/script.js
@@ -1,19 +1,44 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
 
+let userName = prompt("Hi there, whats your name?");
+alert(`Hi ${userName}! Order your food in the next step`)
+
+
 // Step 2 - Food choice
-// Your code goes here
+
+let foodChoice = prompt(" What kind of food do you want?  1. Pizza 2. Pasta 3. Sallad ")
+
+
+let selectedFood
+let subChoice
+
+if (foodChoice === "1") {
+  selectedFood = "Pizza";
+}
+else if (foodChoice === "2") {
+  selectedFood = "Pasta";
+}
+else if (foodChoice === "3") {
+  selectedFood = "Sallad";
+} else {
+  selectedFood = "unknown";
+  alert("Error");
+}
 
 // Step 3 - Subtype choice
-// Your code goes here
+
+
 
 // Step 4 - Age
 // Your code goes here
 
 // Step 5 - Order confirmation
-// Your code goes here
+
+
+
+
