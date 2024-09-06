724559e4d181e229b27c819cc9a009d6bb4ff16d
Anna Hansen <annah80@gmail.com>
2024-09-06 09:30:54 +0200

    added wile loops with strict equality operators, added rest of the JS input such as age prompt and confirmorder

--- code/script.js
+++ code/script.js
@@ -1,172 +1,404 @@
-// Start here
+// I have used while loops and let before const, so the variable can change in the loop. I have also assigned empty strings. 
 
-// Step 1 - Welcome and introduction
-// Your code goes here
+// A while loop is a control flow structure in programming that repeatedly executes a block of code as long as a given condition evaluates to true. The condition is checked before each iteration of the loop, and if the condition is false, the loop terminates.
 
-alert (
-  `Welcome to Mama Mia Pizzeria. Ready to order? - Click 'OK' to begin.`
-)
-const name = prompt(`What's your name?`)
 
-alert(
-  
-  `Thank you for choosing our restaurant, ${name}! We’re here to make your experience memorable.`
-);
+alert(`Welcome to Mama Mia Pizzeria. Ready to order? - Click 'OK' to begin.`)
+
+let userName = prompt(`What's your name?`) 
+
+if (!userName) {
+  userName = "anonymous"
+}
+alert(`Thank you for choosing our restaurant, ${userName}! We're here to make your experience memorable.`)
+
 
 // Step 2 - What type of food
-const mealchoice = prompt(
-  
-  `What would you like to eat? Choose the dish number 1, 2 or 3 according to below:
 
-  1. Pizza
-  2. Pasta
-  3. Salad
-`
-)
+// Added while loops to check if the user's input is not one of the valid options by using === (equals)
+
+let mealchoice = ''
+while (!(mealchoice === "1" || mealchoice === "2" || mealchoice === "3")) {
+  mealchoice = prompt(
+
+    `What would you like to order? Choose the dish number 1, 2 or 3 according to below:
+
+    1. Pizza
+    2. Pasta
+    3. Salad
+  `
+  )
+// If none of the valid options (!) are chosen the alert below will show)
+
+  if (!(mealchoice === "1" || mealchoice === "2" || mealchoice === "3")) {
+    alert(`I'm sorry ${userName}, that is not a valid option. Please choose number 1, 2 or 3.`)
+  }
+}
+
+let mealType = ''
+let subType = ''
 
 if (mealchoice === "1") {
-  
-  alert(
+  mealType = "Pizza"
+  alert(`Thank you ${userName}, you have chosen no 1, Pizza`)
+
+  // Step 3 - Subtype choice for Pizza. // Added while loops to check if the user's input is not one of the valid options by using === (equals)
+  let pizzatype = ''
+
+  while (!(pizzatype === "1" || pizzatype === "2" || pizzatype === "3")) {
+    pizzatype = prompt(
+
+      `What kind of Pizza would you like? Choose between no 1, 2 or 3 according to below:
+
+      1. Capricciosa 
+      2. Hawaii
+      3. Kebab pizza
+    `
+    )
+
+    if (pizzatype === "1") {
+
+      subType = "Capricciosa"
+      alert(`You have chosen no 1, Capricciosa. Love your choice!`)
     
-    `Thank you ${name}, you have chosen no 1, Pizza`
-  
-)
+    } else if (pizzatype === "2") {
+      subType = "Hawaii"
 
-  // Step 3 - Subtype choice for Pizza
-  const pizzatype = prompt(
-    
-    `What kind of Pizza would you like? Choose between no 1, 2 or 3 according to below:
+      alert(`You have chosen no 2, Hawaii. Aloha!`)
+    } else if (pizzatype === "3") {
 
-    1. Capricciosa 
-    2. Hawaii
-    3. Kebab pizza
-  `
-)
+      subType = "Kebab pizza"
+      alert(`You have chosen no 3, Kebab pizza. Great choice!`)
+    } else {
 
-  if (pizzatype === "1") {
-
-    alert(
-      
-      `Thank you ${name}, you have chosen no 1, Capricciosa. Love your choice!`
-  
-)
-  } else if (pizzatype === "2") {
-
-    alert(
-      
-      `Thank you ${name}, you have chosen no 2, Hawaii. Aloha!`
-)
-  } else if (pizzatype === "3") {
-
-    alert(
-      
-      `Thank you ${name}, you have chosen no 3, Kebab pizza. Great choice!`
-    
-)
-  } else {
-    
-    alert(
-      
-      `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`
-)
+      alert(`I'm sorry ${userName}, that is not a valid option. Please choose number 1, 2 or 3.`)
+    }
   }
 
 } else if (mealchoice === "2") {
-  alert(
+  mealType = "Pasta"
+  alert(`Thank you ${userName}, you have chosen no 2, Pasta`)
+
+  // Step 3 - Subtype choice for Pasta. // Added while loops to check if the user's input is not one of the valid options by using === (equals)
+
+  let pastatype = ""
+
+  while (!(pastatype === "1" || pastatype === "2" || pastatype === "3")) {
     
-    `Thank you ${name}, you have chosen no 2, Pasta`
-  
-)
+    pastatype = prompt(
+      `What kind of pasta would you like? Choose between no 1, 2 or 3 according to below:
 
-  // Step 3 - Subtype choice for Pasta
-  const pastatype = prompt(
-    
-    `What kind of pasta would you like? Choose between no 1, 2 or 3 according to below:
+      1. Bolognese 
+      2. Carbonara
+      3. Lasagna
+    `
+    )
 
-    1. Bolognese 
-    2. Carbonara
-    3. Lasagna
-  `
-)
+    if (pastatype === "1") 
+    {
+      subType = 'Bolognese'
 
-  if (pastatype === "1") {
-    alert(
-      
-      `Thank you ${name}, you have chosen no 1, Bolognese. Great choice!`
-)
-  } else if (pastatype === "2") {
+      alert(`You have chosen no 1, Bolognese. Great choice!`)
 
-    alert(
-      
-      `Thank you ${name}, you have chosen no 2, Carbonara. Splendido!`
-)
-  } else if (pastatype === "3") {
-    alert(
-      
-      `Thank you ${name}, you have chosen no 3, Lasagna. Delicious choice!`
-)
-  } else {
+    } else if (pastatype === "2") {
+      subType = 'Carbonara'
 
-    alert(
-      
-      `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`
-)
+      alert(`You have chosen no 2, Carbonara. Splendido!`)
+
+    } else if (pastatype === "3") {
+      subType = 'Lasagna'
+
+      alert(`You have chosen no 3, Lasagna. Delicious choice!`)
+
+    } else {
+      alert(`I'm sorry ${userName}, that is not a valid option. Please choose number 1, 2 or 3.`)
+    }
   }
 
 } else if (mealchoice === "3") {
+  mealType = "Salad"
 
-  alert(
-    
-    `Thank you ${name}, you have chosen no 3, Salad`
-)
+  alert(`Thank you ${userName}, you have chosen no 3, Salad`)
 
   // Step 3 - Subtype choice for Salad
-  const saladtype = prompt(
-    
-    ` What kind of salad would you like? Choose between no 1, 2 or 3 according to below:
+  let saladtype = ''
 
-    1. Caesar salad
-    2. Greek salad
-    3. Ham & cheese salad
-  `
-)
+  while (!(saladtype === "1" || saladtype === "2" || saladtype === "3")) {
 
-  if (saladtype === "1") {
+    saladtype = prompt(
 
-    alert(
-      
-      `Thank you ${name}, you have chosen no 1, Caesar salad. Fresh choice!`
-    
-)
-  } else if (saladtype === "2") {
+      ` What kind of salad would you like? Choose between no 1, 2 or 3 according to below:
 
-    alert(
-      
-      `Thank you ${name}, you have chosen no 2, Greek salad. Healthy choice!`
-)
-  } else if (saladtype === "3") {
-    alert(
-      
-      `Thank you ${name}, you have chosen no 3, Ham & cheese salad. Great choice!`
-    
-)
-  } else {
+      1. Caesar 
+      2. Greek 
+      3. Ham & cheese 
+    `
+    )
 
-    alert(
-      
-      `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`
-    
-)
+    if (saladtype === "1") {
+      subType = "Caesar"
+
+      alert (`You have chosen no 1, Caesar salad. Fresh choice!`)
+
+    } else if (saladtype === "2") {
+
+      subType = "Greek"
+      alert (`You have chosen no 2, Greek salad. Healthy choice!`)
+
+    } else if (saladtype === "3") {
+      subType = "Ham & cheese"
+
+      alert (`You have chosen no 3, Ham & cheese salad. Great choice!`)
+
+    } else {
+      alert (`I'm sorry ${userName}, that is not a valid option. Please choose number 1, 2 or 3.`)
+    }
   }
-
-} else {
-  alert(
-    
-      `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`)
 }
 
-// Step 4 - Age
-// Your code goes here
+// Step 4 - Portion size   // Added while loops to check if the user's input is not one of the valid options by using === (equals)
+let age = ''
+let portionSize = ''   
 
-// Step 5 - Order confirmation
-// Your code goes here/ // // 
+while (!(age === "1" || age === "2")) {
+  age = prompt(
+
+    `Is this order for an adult or child? Please choose corresponding number below.
+
+    1. Adult
+    2. Child
+  `
+  )
+
+  if (age === "1") {
+    portionSize = 'adult'
+
+    alert(`You have chosen an adult portion.`)
+
+  } else if (age === "2") {
+
+    portionSize = 'child'
+
+    alert(`You have chosen a child portion.`)
+
+  } else {
+
+    alert (`I'm sorry ${userName}, that is not a valid option. Please choose number 1 or 2.`)
+  }
+}
+
+// Step 5 - Order confirmation  I used confirmOrder.toLowercase to make sure that the user input is not case sensitive. In other words, they can write YES, yes or No, no and it will be valid. 
+
+let confirmOrder = prompt (`Do you confirm the order of ${portionSize} portion, ${subType} ${mealType}? Yes or No`)
+
+if (confirmOrder.toLowerCase() === "yes") {
+
+  alert(`Thank you for your order, ${userName}! We will start preparing your meal now and we hope you will enjoy the food!`)
+
+} else {
+
+  alert(`You have declined the order, ${userName}. Please come back again if you want to order another time!`)
+}
+
+
+
+
+
+
+
+
+
+
+// // Start here
+
+// // Step 1 - Welcome and introduction
+// // Your code goes here
+
+// alert (
+//   `Welcome to Mama Mia Pizzeria. Ready to order? - Click 'OK' to begin.`
+// )
+// const name = prompt(`What's your name?`)
+
+// alert(
+  
+//   `Thank you for choosing our restaurant, ${name}! We’re here to make your experience memorable.`
+// )
+
+// // Step 2 - What type of food
+// const mealchoice = prompt(
+  
+//   `What would you like to order? Choose the dish number 1, 2 or 3 according to below:
+
+//   1. Pizza
+//   2. Pasta
+//   3. Salad
+// `
+// )
+
+// if (mealchoice === "1") {
+  
+//   alert(
+    
+//     `Thank you ${name}, you have chosen no 1, Pizza`
+  
+// )
+
+//   // Step 3 - Subtype choice for Pizza
+//   const pizzatype = prompt(
+    
+//     `What kind of Pizza would you like? Choose between no 1, 2 or 3 according to below:
+
+//     1. Capricciosa 
+//     2. Hawaii
+//     3. Kebab pizza
+//   `
+// )
+
+//   if (pizzatype === "1") {
+
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 1, Capricciosa. Love your choice!`
+  
+// )
+//   } else if (pizzatype === "2") {
+
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 2, Hawaii. Aloha!`
+// )
+//   } else if (pizzatype === "3") {
+
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 3, Kebab pizza. Great choice!`
+    
+// )
+//   } else {
+    
+//     alert(
+      
+//       `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`
+// )
+//   }
+
+// } else if (mealchoice === "2") {
+//   alert(
+    
+//     `Thank you ${name}, you have chosen no 2, Pasta`
+  
+// )
+
+//   // Step 3 - Subtype choice for Pasta
+//   const pastatype = prompt(
+    
+//     `What kind of pasta would you like? Choose between no 1, 2 or 3 according to below:
+
+//     1. Bolognese 
+//     2. Carbonara
+//     3. Lasagna
+//   `
+// )
+
+//   if (pastatype === "1") {
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 1, Bolognese. Great choice!`
+// )
+//   } else if (pastatype === "2") {
+
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 2, Carbonara. Splendido!`
+// )
+//   } else if (pastatype === "3") {
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 3, Lasagna. Delicious choice!`
+// )
+//   } else {
+
+//     alert(
+      
+//       `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`
+// )
+//   }
+
+// } else if (mealchoice === "3") {
+
+//   alert(
+    
+//     `Thank you ${name}, you have chosen no 3, Salad`
+// )
+
+//   // Step 3 - Subtype choice for Salad
+//   const saladtype = prompt(
+    
+//     ` What kind of salad would you like? Choose between no 1, 2 or 3 according to below:
+
+//     1. Caesar salad
+//     2. Greek salad
+//     3. Ham & cheese salad
+//   `
+// )
+
+//   if (saladtype === "1") {
+
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 1, Caesar salad. Fresh choice!`
+    
+// )
+//   } else if (saladtype === "2") {
+
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 2, Greek salad. Healthy choice!`
+// )
+//   } else if (saladtype === "3") {
+//     alert(
+      
+//       `Thank you ${name}, you have chosen no 3, Ham & cheese salad. Great choice!`
+    
+// )
+//   } else {
+
+//     alert(
+      
+//       `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`
+    
+// )
+//   }
+
+// } else {
+//   alert(
+    
+//       `I'm sorry ${name}, that is not a valid option. Please choose number 1, 2 or 3.`)
+// }
+
+// // Step 4 - Age
+// // Your code goes here
+// const age = prompt(
+// `Is this order for an adult or child? Please choose corresponding number below.
+
+// 1. Adult
+// 2. Child
+//  ` 
+// )
+
+// if (age === "1") {
+
+//  alert(
+      
+// `Thank you ${name}, you have ordered an adult portion.`
+    
+// )
+
+// } else if (age === "2") {
+
+//  alert(
+      
+//  `Thank you ${name}, you have ordered a child portion.`
+// )   
+
+// }
