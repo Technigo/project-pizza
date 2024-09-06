9b746aaa17060996af9ff1f0169bac1438a5495a
xingyin2024 <xingyin.thu@gmail.com>
2024-09-06 23:38:00 +0200

    uppdate css stylesheet

--- code/script.js
+++ code/script.js
@@ -3,8 +3,12 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+  "Welcome to my Javascript Pizzeria. Ready to Start? - Click 'OK' to begin."
 )
+
+const userName = prompt("What's your name?")
+console.log("name is ", userName)
+
 
 // Step 2 - Food choice
 // Your code goes here
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,6 +1,6 @@
 ## Netlify link
 Add your Netlify link here like this (update with the correct one):
 
-https://my-netlify-link.netlify.app
+https://xingspizzeria.netlify.app
 
 PS. Don't forget to add it in your readme as well.
