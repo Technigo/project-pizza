f8ea5c318b1e665abf262e4b90a191f73edf88fc
Estefanny <estefanny@Estefannys-MacBook-Air.local>
2024-09-07 23:48:23 +0200

    refactored step 4 to adhere to be in line with step 5

--- code/script.js
+++ code/script.js
@@ -73,26 +73,34 @@ alert("You have selected: " + foodType + ", great choice!")
 // Step 4 - Age
 // Your code goes here
 
-let confirm = ""
-while (confirm != "1"){
-  let age 
-  while (isNaN(age) == true ||  age < 1 || 100 < age) {
-    age = prompt("How old are you?", "Please type your age here")
-    if (isNaN(age) == true || age < 1 || 100 < age) {
-      alert("Please enter a valid age.")
-    }
+let age 
+while (isNaN(age) == true ||  age < 1 || 100 < age) {
+  age = prompt("How old are you?", "Please type your age here")
+  if (isNaN(age) == true || age < 1 || 100 < age) {
+    alert("Please enter a valid age.")
   }
-  let price, size
-  if (age > 15 ){
-    size = "an adult size "
-    price = 150
-  } else {
-    size = "a kid size "
-    price = 75
-  }
-
-  confirm = prompt("You have chosen " + size + foodType + " " + foodChoice + ". This will cost you " + price + " kr. \nIs this correct? Please enter 1 - for Yes or 2 - for No")
-    
 }
+let price, size
+if (age > 15 ){
+  size = "an adult size "
+  price = 150
+} else {
+  size = "a kid size "
+  price = 75
+}
+
+let confirm = ""
+while (confirm == ""){
+  choice = prompt("You have chosen " + size + foodType + " " + foodChoice + ". This will cost you " + price + " kr. \nIs this correct? Please enter 1 - for Yes or 2 - for No")
+  if (choice == 1) {
+    confirm = 1
+  } else if (choice == 2){
+    confirm = 2
+  } else {
+    alert("Please enter 1 or 2")
+  }
+}  
+
 // Step 5 - Order confirmation
 // Your code goes here
+
