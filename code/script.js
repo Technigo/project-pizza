ae97d5ce277abb9b05fcd00dcf7f0fe9a594ec68
Estefanny <estefanny@Estefannys-MacBook-Air.local>
2024-09-08 00:09:09 +0200

    add step 5 and clean up code

--- code/script.js
+++ code/script.js
@@ -3,7 +3,7 @@
 // Step 1 - Welcome and introduction
 // Your code goes here
 
-alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`)
+alert("Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.")
 let customerName = prompt("Please enter your name", "Your name")
 alert("Hi " + customerName + "! What are you craving today? \nPlease press \"ok\” to continue.")
 const errMsg = "Incorrect number, please enter 1, 2 or 3."
@@ -12,13 +12,13 @@ const errMsg = "Incorrect number, please enter 1, 2 or 3."
 // Your code goes here
 
 let foodChoice = ""
-while (foodChoice == ""){
+while (foodChoice == "") {
   let choice = prompt("Please enter a number: \n 1 - Pizza \n 2 - Pasta \n 3 - Salad", "Please type you number of choice here")
   if (choice == 1) {
     foodChoice = "Pizza"
   } else if (choice == 2) {
     foodChoice = "Pasta"
-  } else if (choice == 3){
+  } else if (choice == 3) {
     foodChoice = "Salad"
   } else {
     alert(errMsg)
@@ -31,14 +31,14 @@ alert("You have selected: " + foodChoice + ". Yummy!")
 // Your code goes here
 
 let foodType = ""
-while (foodType == ""){
+while (foodType == "") {
   if (foodChoice == "Pizza") {
     choice = prompt("Please enter a number: \n 1 - Napolitana \n 2 - Margherita \n 3 - Sicilian", "Please type you number of choice here") 
     if (choice == 1) {
       foodType = "Napolitana"          
     } else if (choice == 2) {
       foodType = "Margherita"  
-    } else if (choice == 3){
+    } else if (choice == 3) {
       foodType = "Sicilian"  
     } else {
       alert(errMsg)
@@ -49,18 +49,18 @@ while (foodType == ""){
       foodType = "Fungui"          
     } else if (choice == 2) {
       foodType = "Ragú"  
-    } else if (choice == 3){
+    } else if (choice == 3) {
       foodType = "Carbonara"  
     } else {
       alert(errMsg)
     }
-  } else if (foodChoice == "Salad"){
+  } else if (foodChoice == "Salad") {
     choice= prompt("Please enter a number: \n 1 - Caesar \n 2 - Greek \n 3 - Caprese", "Please type you number of choice here") 
     if (choice == 1) {
       foodType = "Caesar"          
     } else if (choice == 2) {
       foodType = "Greek"  
-    } else if (choice == 3){
+    } else if (choice == 3) {
       foodType = "Caprese"  
     } else {
       alert(errMsg)
@@ -81,7 +81,7 @@ while (isNaN(age) == true ||  age < 1 || 100 < age) {
   }
 }
 let price, size
-if (age > 15 ){
+if (age > 15) {
   size = "an adult size "
   price = 150
 } else {
@@ -90,11 +90,11 @@ if (age > 15 ){
 }
 
 let confirm = ""
-while (confirm == ""){
+while (confirm == "") {
   choice = prompt("You have chosen " + size + foodType + " " + foodChoice + ". This will cost you " + price + " kr. \nIs this correct? Please enter 1 - for Yes or 2 - for No")
   if (choice == 1) {
     confirm = 1
-  } else if (choice == 2){
+  } else if (choice == 2) {
     confirm = 2
   } else {
     alert("Please enter 1 or 2")
@@ -104,3 +104,8 @@ while (confirm == ""){
 // Step 5 - Order confirmation
 // Your code goes here
 
+if (confirm == 1) {
+  alert("Thank you for your order! Your delicious meal will be prepared. See you soon " + String.fromCodePoint(0x1F920))
+} else if (confirm == 2) {
+  alert("No problem, come back and order anytime you want!");
+}
