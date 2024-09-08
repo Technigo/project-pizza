f7ef980e79c2c8c5151126691d3fd689c6182e0e
Jenny <jennyandersen182@gmail.com>
2024-09-08 20:38:07 +0200

    fixed whatKind in order summary

--- code/script.js
+++ code/script.js
@@ -1,25 +1,21 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
-alert(
-  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
-)
+alert(`Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`);
 // Ask the user for their name
 const name = prompt("What is your name?");
 
 // Display a greeting with their name
 alert("Hello " + name + "!");
 
-
 // Step 2 - Food choice
-
 const foodChoice = prompt(
   "What would you like to order? Please choose a number:\n1. Pizza\n2. Pasta\n3. Salad"
 );
 
-// Display their food of choice
+let whatKind = ""; // This will store the specific food item
 
+// Display their food of choice
 if (foodChoice === "1") {
   alert("You've ordered a pizza!");
 } else if (foodChoice === "2") {
@@ -31,62 +27,87 @@ if (foodChoice === "1") {
 }
 
 // Step 3 - Subtype choice
-
 if (foodChoice === "1") {
-// if the user chooses Pizza
-  whatKind = promt(
-    "What kind of Pizza would you like? Please choose a number:\n1. Margherita\n2Pepperoni\n3. Hawaii"
+  // if the user chooses Pizza
+  whatKind = prompt(
+    "What kind of Pizza would you like? Please choose a number:\n1. Margherita\n2. Pepperoni\n3. Hawaii"
   );
 
   if (whatKind === "1") {
-  alert ("You ordered Margherita pizza.");
+    whatKind = "Margherita pizza";
   } else if (whatKind === "2") {
-  alert ("You ordered Pepperoni pizza.");
+    whatKind = "Pepperoni pizza";
   } else if (whatKind === "3") {
-  alert ("You ordered Hawaii pizza.");
+    whatKind = "Hawaii pizza";
   } else {
-  alert ("Unvalid choice")
+    alert("Invalid choice");
   }
 
 } else if (foodChoice === "2") {
   // if the user chooses Pasta
   whatKind = prompt(
-    "What kind of pasta would you like? Please choose a number:\n1. Vodka pasta\n2Carbonara\n3. Bolognese"
+    "What kind of pasta would you like? Please choose a number:\n1. Vodka pasta\n2. Carbonara\n3. Bolognese"
   );
 
   if (whatKind === "1") {
-    alert ("You ordered Vodka pasta.");
-    } else if (whatKind === "2") {
-    alert ("You ordered Carbonara.");
-    } else if (whatKind === "3") {
-    alert ("You ordered Bolognese.");
-    } else {
-    alert ("Unvalid choice")
-    }  
+    whatKind = "Vodka pasta";
+  } else if (whatKind === "2") {
+    whatKind = "Carbonara";
+  } else if (whatKind === "3") {
+    whatKind = "Bolognese";
+  } else {
+    alert("Invalid choice");
+  }
 
 } else if (foodChoice === "3") {
-  // if the user chooses salad
+  // if the user chooses Salad
   whatKind = prompt(
-    "What kind of salad would you like? Please choose a number:\n1. Ceasar salad\n2Greek salad\n3. Tuna salad"
+    "What kind of salad would you like? Please choose a number:\n1. Ceasar salad\n2. Greek salad\n3. Tuna salad"
   );
 
   if (whatKind === "1") {
-    alert ("You ordered Ceasar salad.");
-    } else if (whatKind === "2") {
-    alert ("You ordered Greek salad.");
-    } else if (whatKind === "3") {
-    alert ("You ordered Tuna salad.");
-    } else {
-    alert ("Unvalid choice")
-    }
-
-} else {
-  alert("Invalid choice. Please use a number between 1 and 3");
+    whatKind = "Ceasar salad";
+  } else if (whatKind === "2") {
+    whatKind = "Greek salad";
+  } else if (whatKind === "3") {
+    whatKind = "Tuna salad";
+  } else {
+    alert("Invalid choice");
+  }
 }
 
-
 // Step 4 - Age
-// Your code goes here
+// Asks the user for their age
+const age = parseInt(
+  prompt("Is this food for a child or an adult? Type your age:")
+);
+
+let price = "";
+let portionSize = "";
+
+if (age) {
+  if (age <= 10) {
+    price = "50kr";
+    portionSize = "child-sized";
+  } else {
+    price = "100kr";
+    portionSize = "adult-sized";
+  }
+} else {
+  alert("Invalid entry. Refresh the page and try again");
+}
 
 // Step 5 - Order confirmation
-// Your code goes here
+const orderConfirm = parseInt(
+  prompt(
+    `Got it ${name}! One ${portionSize} ${whatKind} is on the way. That will be ${price}.\nPlease confirm your order:\n1. Yes \n2. No`
+  )
+);
+
+if (orderConfirm === 1) {
+  alert(`Thank you ${name} for your order! We are preparing your ${whatKind} now.`);
+} else if (orderConfirm === 2) {
+  alert("That's cool! You are welcome back anytime.");
+} else {
+  alert("Sorry - invalid answer. Please try again!");
+}
