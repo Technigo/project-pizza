8cb93813acecb6ef452a3eda715f7eef7da78aee
gabster94 <iofe.gabriella@gmail.com>
2024-09-08 20:18:01 +0200

    Created two alerts, one to welcome and one to comfirn the clients name. I also stored the input in a variable

--- code/script.js
+++ code/script.js
@@ -2,12 +2,20 @@
 
 // Step 1 - Welcome and introduction
 // Your code goes here
+// Step 1a
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
+// Step 1b
+const name = prompt('What is your name?')
+// Step 1c
+alert(`Nice to meet you ${name.toUpperCase()} let's pick your dinner`)
 
 // Step 2 - Food choice
 // Your code goes here
+// Step 2a
+
+// Step 2b
 
 // Step 3 - Subtype choice
 // Your code goes here
